b945cb6057453c7963df6b5cf01c682d9c5dda7d
bryanpizzillo <bryanpizzillo@users.noreply.github.com>
2018-11-19 00:12:22 -0500

    Works for Mac

--- scripts/composer/CGovScriptHandler.php
+++ scripts/composer/CGovScriptHandler.php
@@ -26,13 +26,13 @@ class CGovScriptHandler {
     $project_root = join([ __DIR__, "../.." ], "/");
 
     $bltFile = array(
-        "path" => join([$project_root, "blt"]),
+        "path" => join([$project_root, "blt"], "/"),
         "sample" => "example.local.blt.yml",
         "real" => "local.blt.yml"
     );
 
     $dockerenvFile = array(
-      "path" => join([$project_root, "docker"]),
+      "path" => join([$project_root, "docker"], "/"),
       "sample" => "docker.env.sample",
       "real" => "docker.env"
     );
@@ -66,11 +66,22 @@ class CGovScriptHandler {
       exit(1);
     }
 
+    //Copy the files
+    foreach($filesToCopy as $fileInfo) {
+      CGovScriptHandler::copyLocalFile($fileInfo);
+    }
+
     $event->getIO()->write("CGov Project Initialization: Complete");
   }
 
+  /**
+   * Checks to see if a init file does not already exist
+   *
+   * @param [type] $fileInfo Information about the file to check
+   * @return boolean
+   */
   private static function isLocalFileValid($fileInfo) {
-    return true;
+    return !file_exists(join([ $fileInfo["path"], $fileInfo["real"] ],"/"));
   }
 
   /**
@@ -80,55 +91,9 @@ class CGovScriptHandler {
    * @return void
    */
   private static function copyLocalFile($fileInfo) {
-
+    copy(
+      join([ $fileInfo["path"], $fileInfo["sample"] ], "/"),
+      join([ $fileInfo["path"], $fileInfo["real"] ], "/")
+    );
   }
-
-  /*
-  public static function createRequiredFiles(Event $event) {
-    $fs = new Filesystem();
-    $drupalFinder = new DrupalFinder();
-    $drupalFinder->locateRoot(getcwd());
-    $drupalRoot = $drupalFinder->getDrupalRoot();
-
-    $dirs = [
-      'modules',
-      'profiles',
-      'themes',
-    ];
-
-    // Required for unit testing
-    foreach ($dirs as $dir) {
-      if (!$fs->exists($drupalRoot . '/'. $dir)) {
-        $fs->mkdir($drupalRoot . '/'. $dir);
-        $fs->touch($drupalRoot . '/'. $dir . '/.gitkeep');
-      }
-    }
-
-    // Prepare the settings file for installation
-    if (!$fs->exists($drupalRoot . '/sites/default/settings.php') and $fs->exists($drupalRoot . '/sites/default/default.settings.php')) {
-      $fs->copy($drupalRoot . '/sites/default/default.settings.php', $drupalRoot . '/sites/default/settings.php');
-      require_once $drupalRoot . '/core/includes/bootstrap.inc';
-      require_once $drupalRoot . '/core/includes/install.inc';
-      $settings['config_directories'] = [
-        CONFIG_SYNC_DIRECTORY => (object) [
-          'value' => Path::makeRelative($drupalFinder->getComposerRoot() . '/config/sync', $drupalRoot),
-          'required' => TRUE,
-        ],
-      ];
-      drupal_rewrite_settings($settings, $drupalRoot . '/sites/default/settings.php');
-      $fs->chmod($drupalRoot . '/sites/default/settings.php', 0666);
-      $event->getIO()->write("Create a sites/default/settings.php file with chmod 0666");
-    }
-
-    // Create the files directory with chmod 0777
-    if (!$fs->exists($drupalRoot . '/sites/default/files')) {
-      $oldmask = umask(0);
-      $fs->mkdir($drupalRoot . '/sites/default/files', 0777);
-      umask($oldmask);
-      $event->getIO()->write("Create a sites/default/files directory with chmod 0777");
-    }
-  }
-  **/
-
-
 }
