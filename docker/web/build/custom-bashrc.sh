b9a92ebce9d4ffc02b91ac8c901b264183842bc5
Brian Gallagher <brian.gallagher@nih.gov>
2018-11-26 14:06:39 -0500

    Add environment variables for phpUnit Testing

--- docker/web/build/custom-bashrc.sh
+++ docker/web/build/custom-bashrc.sh
@@ -1,3 +1,7 @@
 
 # Set obvious docker shell prompt
 PS1='🐳 \[\033[1;36m\]\h \[\033[1;34m\]\W\[\033[0;35m\] \[\033[1;36m\]# \[\033[0m\]'
+
+# Set Simpletest options
+export SIMPLETEST_DB=mysql://drupal:drupal@db:3306/drupal
+export SIMPLETEST_BASE_URL=http://cgov.loc/
