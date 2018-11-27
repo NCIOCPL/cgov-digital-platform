9a56740dbbf8d4f8f52c9fcd9d58a406a5d8a011
Brendan Beltz <bbeltz@sapient.com>
2018-11-27 09:20:29 -0500

    Create base theme webopack build with custom entries

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/webpack.config.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/webpack.config.js
@@ -1,6 +1,19 @@
 const path = require('path');
-const webpack = require('webpack');
-const baseConfig = require('../webpack.base.config');
-const themeConfig = require('./webpack.entries.js');
+const baseConfig = require('../webpack.base.config.js');
+const themeEntries = require('./webpack.entries.js');
+
+const themeConfig = {
+    entry: themeEntries,
+    resolve: {
+        modules: [path.resolve(__dirname, '../src'), 'node_modules'],
+        alias: {
+            core: path.resolve(__dirname, '..'),
+        }
+    },
+    output: {
+		path: path.join(__dirname, "dist/js")
+	},
+
+}
 
 module.exports = Object.assign(baseConfig, themeConfig);
