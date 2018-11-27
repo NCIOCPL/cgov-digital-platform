9b14fa2a381df10aafb966c380870f90274bd710
Brendan Beltz <bbeltz@sapient.com>
2018-11-27 11:08:35 -0500

    Test code in core global entry for babel transforming added

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/entrypoints/global/Common.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/entrypoints/global/Common.js
@@ -1,4 +1,8 @@
 import { newWindow } from 'core/src/utilities';
 import './Common.scss';
 
+const a = { A: 'AAA' };
+const b = { B: 'BBB' };
+const ab = { ...a, ...b };
+
 newWindow('https://www.cancer.gov');
