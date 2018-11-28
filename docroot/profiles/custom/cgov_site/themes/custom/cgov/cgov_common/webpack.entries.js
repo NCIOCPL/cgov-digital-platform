edd403b26f05a4feaadd8f5148ad51b4322edbdb
BrendanBeltzNIH <34315527+BrendanBeltzNIH@users.noreply.github.com>
2018-11-28 16:43:20 -0500

    Feature/78 add article node placeholder (#79)
    Resolves #78 
    * Register Global and Article libraries
    * Create Article entry point
    * Add in template placeholders
    * Add tester css to Article asset

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/webpack.entries.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/webpack.entries.js
@@ -2,4 +2,5 @@ const path = require('path');
 
 module.exports = {
     "Common": path.resolve(__dirname, "src/entrypoints/global/Common.js"),
+    "Article": path.resolve(__dirname, "src/entrypoints/article/Article.js"),
 }
