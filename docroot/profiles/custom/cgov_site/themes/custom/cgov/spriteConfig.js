51d9e055741cb99d790938611ce1bc9b5cceb048
Brendan Beltz <bbeltz@sapient.com>
2018-12-03 14:26:31 -0500

    Move core sprite folder

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/spriteConfig.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/spriteConfig.js
@@ -18,7 +18,7 @@ module.exports = {
             render: {
                 scss: {
                     dest: "../src/styles/sprites/_svg-sprite-map.scss",
-                    template: 'src/sprites/sprite-template.scss'
+                    template: 'src/styles/sprites/sprite-template.scss'
                 }
             },
             layout:"vertical",
