5e83f95ce4e0b41cbab1b9dc3ad8276f741382ad
Brendan Beltz <bbeltz@sapient.com>
2018-12-04 11:35:41 -0500

    Remove gulpfile test line

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/gulpfile.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/gulpfile.js
@@ -7,7 +7,6 @@ const themes = require('./registeredThemes.js');
 
 gulp.task('sprites', () => {
     return merge(themes.map(themePath => {
-        console.log('*****', themePath)
         return gulp.src('**/*.svg', { cwd: `${ themePath }/assets/sprites`})
             .pipe(svgSprite(spriteConfig))
             .pipe(gulp.dest(`${ themePath }/dist`))
