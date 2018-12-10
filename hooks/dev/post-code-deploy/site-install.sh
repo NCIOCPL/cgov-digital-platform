dcb32474c636120bc66890db4aecd9e55ffe81e9
Bryan Pizzillo <bryanp@mail.nih.gov>
2018-12-09 23:21:06 -0500

    Fixed issue with blt path

--- hooks/dev/post-code-deploy/site-install.sh
+++ hooks/dev/post-code-deploy/site-install.sh
@@ -7,6 +7,7 @@
 #
 # This is for initial development of our profile. At a certain point,
 # there should be an import of example web site content.
+set -ev
 
 # Map the script inputs to convenient names.
 site="$1"
@@ -16,5 +17,12 @@ deployed_tag="$4"
 repo_url="$5"
 repo_type="$6"
 
+# Prep for BLT commands.
+repo_root="/var/www/html/$site.$target_env"
+export PATH=$repo_root/vendor/bin:$PATH
+cd $repo_root
+
 blt setup:drupal:install --environment=$target_env -v --yes --no-interaction
 
+
+set +v
