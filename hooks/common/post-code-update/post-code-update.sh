af680a8994ac80ef176ad196dac8a87bb751d6a3
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2018-12-09 21:37:08 -0500

    Update post-code-update.sh

--- hooks/common/post-code-update/post-code-update.sh
+++ hooks/common/post-code-update/post-code-update.sh
@@ -22,11 +22,15 @@ deployed_tag="$4"
 repo_url="$5"
 repo_type="$6"
 
+echo "Common post-code-update"
+
 # Prep for BLT commands.
 repo_root="/var/www/html/$site.$target_env"
 export PATH=$repo_root/vendor/bin:$PATH
 cd $repo_root
 
 blt artifact:ac-hooks:post-code-update $site $target_env $source_branch $deployed_tag $repo_url $repo_type --environment=$target_env -v --yes --no-interaction
+
+echo "Completed Common post-code-update"
 
 set +v
