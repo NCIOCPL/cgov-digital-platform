020edbffa577f19afe6779f4867d127474a8667a
Brendan Beltz <bbeltz@sapient.com>
2018-12-11 14:10:36 -0500

    Fleshed out example PageOptions (not placeable yet)

--- docroot/profiles/custom/cgov_site/modules/custom/cgov_core/src/Plugin/Block/PageOptions.php
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_core/src/Plugin/Block/PageOptions.php
@@ -3,23 +3,69 @@
 namespace Drupal\cgov_core\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Routing\CurrentRouteMatch;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Cgov page options block.
+ * Provides a block with page options.
  *
  * @Block(
  *  id = "page_options",
- *  admin_label = "Page Options Block for Cgov",
+ *  admin_label = "Page Options",
  * )
  */
-class PageOptionsBlock extends BlockBase {
+class PageOptions extends BlockBase implements ContainerFactoryPluginInterface {
+
+  protected $routeMatch;
+
+  public $currentNodeType = 'test';
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_route_match')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, CurrentRouteMatch $route_match) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->routeMatch = $route_match;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getNodeType() {
+    $nid = '';
+    // $node = $this->routeMatch()->getParameter('node');
+    // if ($node instanceof NodeInterface) {
+    // $nid = $node->getType();
+    // }
+    return $nid;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPageOptionsForPageType($nodeType) {
+    return $nodeType;
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
     return [
-      'example' => 'example',
+      '#options' => $this->getPageOptionsForPageType($this->currentNodeType),
     ];
   }
 
