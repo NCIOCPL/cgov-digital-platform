68822f49eb59a6333aa3c8ef99922db71acb0530
Brendan Beltz <bbeltz@sapient.com>
2018-12-12 14:21:31 -0500

    DI Route Match service into page options service

--- docroot/profiles/custom/cgov_site/modules/custom/cgov_core/src/Services/PageOptionsManager.php
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_core/src/Services/PageOptionsManager.php
@@ -2,17 +2,30 @@
 
 namespace Drupal\cgov_core\Services;
 
+use Drupal\Core\Routing\RouteMatchInterface;
+
 /**
  * Page Options Manager Service.
  */
 class PageOptionsManager {
 
+  /**
+   * The route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
   protected $nodeType = '';
 
   /**
-   * {@inheritdoc}
+   * Constructs a new Page Options Manager Service class.
+   *
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The route match.
    */
-  public function __construct() {
+  public function __construct(RouteMatchInterface $route_match) {
+    $this->routeMatch = $route_match;
     $this->nodeType = 'Test';
   }
 
