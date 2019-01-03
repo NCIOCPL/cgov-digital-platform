a21e2d83efef8eb67fabbedd3867feb7ec8c3365
Brendan Beltz <bbeltz@sapient.com>
2019-01-03 17:04:49 -0500

    Accept CLI arg for module name

--- blt/src/Commands/YamlContentCommand.php
+++ blt/src/Commands/YamlContentCommand.php
@@ -12,12 +12,18 @@ class YamlContentCommand extends BltTasks {
   /**
    * Enable and install yaml content from custom module.
    *
+   * Pass the name of the module contain default yaml content.
+   * Eg, "blt custom:instal_default_yaml_content cgov_yaml_content".
+   *
+   * @param string $module
+   *   Name of module with yaml content.
+   *
    * @command custom:install_default_yaml_content
    */
-  public function installDefaultYamlContent() {
+  public function installDefaultYamlContent($module) {
     $commands = [
-      'custom:enable_custom_yaml_content',
-      'custom:import_custom_yaml_content',
+      'custom:enable_custom_yaml_content' => ['module' => $module],
+      'custom:import_custom_yaml_content' => ['module' => $module],
     ];
     $this->invokeCommands($commands);
   }
@@ -25,14 +31,19 @@ class YamlContentCommand extends BltTasks {
   /**
    * Enable custom yaml content module.
    *
+   * @param string $module
+   *   Name of module to enable.
+   *
    * @command custom:enable_custom_yaml_content
    */
-  public function enableCustomYamlContent() {
-    $this->say("Installing cgov_yaml_content module!");
+  public function enableCustomYamlContent($module) {
+    $this->say("************************************************");
+    $this->say("***** Installing $module module! *****");
+    $this->say("************************************************");
     /** @var \Acquia\Blt\Robo\Tasks\DrushTask $task */
     $task = $this->taskDrush()
       ->drush('en')
-      ->rawArg('cgov_yaml_content')
+      ->rawArg($module)
       ->printOutput(TRUE);
     $result = $task->interactive($this->input()->isInteractive())->run();
     return $result;
@@ -41,14 +52,19 @@ class YamlContentCommand extends BltTasks {
   /**
    * Import default yaml content from module.
    *
+   * @param string $module
+   *   Name of module to import from.
+   *
    * @command custom:import_custom_yaml_content
    */
-  public function importCustomYamlContent() {
-    $this->say("Importing cgov_yaml_content module default content!");
+  public function importCustomYamlContent($module) {
+    $this->say("***************************************************************");
+    $this->say("***** Importing $module module default content! *****");
+    $this->say("***************************************************************");
     /** @var \Acquia\Blt\Robo\Tasks\DrushTask $task */
     $task = $this->taskDrush()
       ->drush('ycim')
-      ->rawArg('cgov_yaml_content')
+      ->rawArg($module)
       ->printOutput(TRUE);
     $result = $task->interactive($this->input()->isInteractive())->run();
     return $result;
