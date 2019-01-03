defe820039976042d1e514ad5ee2acea2bce9d0c
Brendan Beltz <bbeltz@sapient.com>
2019-01-03 12:33:24 -0500

    Add in custom blt task to enable and install yaml content

---
+++ blt/src/Commands/YamlContentCommand.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Acquia\Blt\Custom\Commands;
+
+use Acquia\Blt\Robo\BltTasks;
+
+/**
+ * Handle importing default yaml content.
+ *
+ * Future iterations should allow for command line
+ * specification of the specific module to install.
+ */
+class YamlContentCommand extends BltTasks {
+
+  /**
+   * Print "Hello world!" to the console.
+   *
+   * @command custom:install_default_yaml_content
+   * @description This is an example command.
+   */
+  public function installDefaultYamlContent() {
+    $commands = [
+      'custom:enable_custom_yaml_content',
+      'custom:import_custom_yaml_content',
+    ];
+    $this->invokeCommands($commands);
+  }
+
+  /**
+   * Enable custom yaml content module.
+   *
+   * @command custom:enable_custom_yaml_content
+   * @description Do the thing.
+   */
+  public function enableCustomYamlContent() {
+    $this->say("Installing cgov_yaml_content module!");
+    /** @var \Acquia\Blt\Robo\Tasks\DrushTask $task */
+    $task = $this->taskDrush()
+      ->drush('en')
+      ->rawArg('cgov_yaml_content')
+      ->printOutput(TRUE);
+    $result = $task->interactive($this->input()->isInteractive())->run();
+    return $result;
+  }
+
+  /**
+   * Import default yaml content from module.
+   *
+   * @command custom:import_custom_yaml_content
+   * @description Do the thing.
+   */
+  public function importCustomYamlContent() {
+    $this->say("Importing cgov_yaml_content module default content!");
+    /** @var \Acquia\Blt\Robo\Tasks\DrushTask $task */
+    $task = $this->taskDrush()
+      ->drush('ycim')
+      ->rawArg('cgov_yaml_content')
+      ->printOutput(TRUE);
+    $result = $task->interactive($this->input()->isInteractive())->run();
+    return $result;
+  }
+
+}
