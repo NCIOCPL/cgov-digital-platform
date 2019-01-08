44d7859731bac8b4da1090d56f34dfa38ff5ac82
BrendanBeltzNIH <34315527+BrendanBeltzNIH@users.noreply.github.com>
2019-01-08 16:39:43 -0500

    Insert default content in regions on install (#376)
    Closes #353 
    * Created the cgov_yaml_content module to load demo content for a www site.
       * Add default regions to cgov_yaml_content raw html blocks for install
    * Added yaml_content event listeners to:
       * Add blocks to the correct page regions
       * Programmatically add spanish translations to default content for  nodes, block_content, and paragraphs
    * Enable cgov_yaml_content module for local environments
    * Created robo commands to install demo content
    * Added code deploy and update cloud hooks to install demo content on an environment code switch.
       * NOTE: Cloud hooks are currently hardcoded to just cgov_yaml

--- hooks/common/post-code-update/post-code-update.sh
+++ hooks/common/post-code-update/post-code-update.sh
@@ -33,5 +33,6 @@ cd $repo_root
 
 ## Perform a fresh install
 blt artifact:install:drupal --environment=$target_env -v --yes --no-interaction
+blt custom:install_cgov_yaml_content_by_module cgov_yaml_content
 
 set +v
