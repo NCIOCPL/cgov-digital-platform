af2e987602e59a56a62ffc08d155acbd7fa24dfa
Brendan Beltz <bbeltz@sapient.com>
2019-01-10 15:49:44 -0500

    Logic to filter dates by field_date_display_mode settings

--- docroot/profiles/custom/cgov_site/modules/custom/cgov_article/src/Plugin/Field/FieldFormatter/DateFilterFieldFormatter.php
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_article/src/Plugin/Field/FieldFormatter/DateFilterFieldFormatter.php
@@ -49,15 +49,106 @@ class DateFilterFieldFormatter extends FormatterBase {
   }
 
   /**
+   * Filter Date.
+   *
+   * If a date display mode field exists on the same entity as this field
+   * and if that field contains a reference to this field, further logic
+   * will be executed to determine if this field should be displayed.
+   * If the date display mode field exists and doesn't reference this
+   * field, then this field should not be rendered.
+   * In all other cases it should be displayed as normal.
+   *
+   * @param \Drupal\Core\Field\FieldItemListInterface $items
+   *   The current field's List interface.
+   */
+  public function filterDate(FieldItemListInterface $items) {
+    $node = $items->getEntity();
+    if (!$node) {
+      return TRUE;
+    }
+
+    $displayModesList = $node->toArray()["field_date_display_mode"];
+
+    // We aren't filtering if there isn't a date display mode.
+    // (This is a guard, but shouldn't be an issue if this filter
+    // is only used on fields on content types that have a
+    // field_date_display_mode).
+    if ($displayModesList === NULL || count($displayModesList) === 0) {
+      return TRUE;
+    }
+
+    // Clean up displayModes so it's a simple array to work with
+    // and so that it references dates by machine name.
+    $filteredDates = array_map(function ($arr) {
+      return 'field_date_' . $arr['value'];
+    }, $displayModesList);
+
+    $currentField = $this->fieldDefinition->getName();
+    // If this date field is not selected for display, we don't
+    // want to render it, obviously.
+    if (!in_array($currentField, $filteredDates)) {
+      return FALSE;
+    }
+
+    // The date entities to pass to the render array.
+    // (They are not renderable in the twig template in their current state).
+    $dateEntities = [];
+    foreach ($filteredDates as $machineName) {
+      $dateEntities[$machineName] = $node->get($machineName);
+    }
+
+    // NOTE: This is where we can set logic path, instead of using the node type
+    // we can pull that off of another field set by the user that determines the
+    // display mode (all, latest, oldest...).
+    $nodeType = $node->getType();
+    $isArticlePage = $nodeType === 'cgov_article';
+    if ($isArticlePage) {
+      // --------------------------------------------------------------------
+      // TODO: if() code should be an external function, taking and returning
+      // $dateEntities array, for cleanness.
+      // --------------------------------------------------------------------
+      // 1. Create a multidimensional array with the date value for sorting.
+      $dateEntitiesWithValues = [];
+      foreach ($dateEntities as $machineName => $dateEntity) {
+        $dateEntitiesWithValues[] = [
+          $machineName,
+          $dateEntity,
+          // The following retrieves the DateTimeItem fromt he DateTimeItemList
+          // and then pulls it's datetime_iso8601 time for sorting, rather than
+          // the formatted value.
+          $dateEntity->get(0)->value,
+        ];
+      };
+
+      // 2. Sort (in place) according to datetime_iso8601 value ascending.
+      usort($dateEntitiesWithValues, function ($entity1, $entity2) {
+        $entity1UnixTime = strtotime($entity1[2]);
+        $entity2UnixTime = strtotime($entity2[2]);
+        if ($entity1UnixTime == $entity2UnixTime) {
+          return 0;
+        }
+        return ($entity1UnixTime > $entity2UnixTime) ? 1 : -1;
+      });
+
+      // 3. Retrieve most recent value after sort.
+      $mostRecentDate = array_pop($dateEntitiesWithValues);
+      $mostRecentDateMachineName = $mostRecentDate[0];
+      if ($mostRecentDateMachineName !== $currentField) {
+        return FALSE;
+      }
+    }
+
+    return TRUE;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = [];
-
-    foreach ($items as $delta => $item) {
-      $entity = $item->getEntity();
-      var_dump($entity->toArray()["field_date_display_mode"]);
-      $elements[$delta] = ['#markup' => $this->viewValue($item)];
+    $shouldDisplayDate = $this->filterDate($items);
+    if ($shouldDisplayDate) {
+      $elements[0] = ['#markup' => $this->viewValue($items[0])];
     }
 
     return $elements;
