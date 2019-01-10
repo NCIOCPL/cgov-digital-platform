41fc8d3f907e84109a63fcd56d700c7e8da0e891
Brendan Beltz <bbeltz@sapient.com>
2019-01-10 13:39:52 -0500

    Boilerplate Field Formatter base

---
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_article/src/Plugin/Field/FieldFormatter/DateFilterFieldFormatter.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Drupal\cgov_article\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'date_filter_field_formatter' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "date_filter_field_formatter",
+ *   label = @Translation("Date filter field formatter"),
+ *   field_types = {
+ *     "datetime"
+ *   }
+ * )
+ */
+class DateFilterFieldFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      // Implement default settings.
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    return [
+      // Implement settings form.
+    ] + parent::settingsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    // Implement settings summary.
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+
+    foreach ($items as $delta => $item) {
+      $entity = $item->getEntity();
+      var_dump($entity->toArray()["field_date_display_mode"]);
+      $elements[$delta] = ['#markup' => $this->viewValue($item)];
+    }
+
+    return $elements;
+  }
+
+  /**
+   * Generate the output appropriate for one field item.
+   *
+   * @param \Drupal\Core\Field\FieldItemInterface $item
+   *   One field item.
+   *
+   * @return string
+   *   The textual output generated.
+   */
+  protected function viewValue(FieldItemInterface $item) {
+    // The text value has no text format assigned to it, so the user input
+    // should equal the output, including newlines.
+    return nl2br(Html::escape($item->value));
+  }
+
+}
