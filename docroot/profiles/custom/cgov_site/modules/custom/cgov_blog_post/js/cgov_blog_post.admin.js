da4c443c96ba6db446c84af7d64116e6f4b15be4
D'Aquino <daquinohd@nci-02133271-ml.nih.gov>
2019-02-13 17:14:25 -0500

    Cleanup and comments

--- docroot/profiles/custom/cgov_site/modules/custom/cgov_blog_post/js/cgov_blog_post.admin.js
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_blog_post/js/cgov_blog_post.admin.js
@@ -1,6 +1,18 @@
 (function($) {
   $.fn.drawBlogTopics = function(data) {
-    console.log('I cant dance');
+    // Hide all checkboxes by default
+    $('#edit-field-blog-topics .js-form-item').addClass('visually-hidden');
+
+    // Get the seleccted series ID
+
+    var seriesId = $('select#edit-field-blog-series')[0].value;
+    // What do we want from the data params?
+    // 1. Selected Blog Series (number)
+    // 2. ?
+    // 3. Profit.
+    // TODO: hide toics wrapper if not categories
+    console.log(data);
+    console.log('series ID: ' + seriesId);
   };
   //TODO: clean up and add guts
 })(jQuery);
