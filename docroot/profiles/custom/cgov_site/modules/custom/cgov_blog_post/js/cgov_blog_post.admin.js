cda5e3c0f4e065f86e1b2a6453ff52f1721e5642
D'Aquino <daquinohd@nci-02133271-ml.home>
2019-02-13 20:31:29 -0500

    Build out option filter logic

--- docroot/profiles/custom/cgov_site/modules/custom/cgov_blog_post/js/cgov_blog_post.admin.js
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_blog_post/js/cgov_blog_post.admin.js
@@ -1,17 +1,23 @@
 (function($) {
   $.fn.drawBlogTopics = function(data) {
     // Hide all checkboxes by default
-    $('#edit-field-blog-topics .js-form-item').addClass('visually-hidden');
+    $('#edit-field-blog-topics .js-form-item').addClass('to-be-hidden');
 
     // Get the seleccted series ID
-
     var seriesId = $('select#edit-field-blog-series')[0].value;
+
     // What do we want from the data params?
     // 1. Selected Blog Series (number)
     // 2. ?
     // 3. Profit.
     // TODO: hide toics wrapper if not categories
-    console.log(data);
+    for (var key in data) {
+      if(data[key] === seriesId) {
+        console.log(key + ', ' + data[key]);
+      }
+    }
+
+    //console.log(data);
     console.log('series ID: ' + seriesId);
   };
   //TODO: clean up and add guts
