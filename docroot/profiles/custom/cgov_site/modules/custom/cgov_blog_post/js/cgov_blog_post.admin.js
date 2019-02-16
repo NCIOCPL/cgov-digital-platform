0ed021cb8c5a76c93aa2100371a4fb4122ed7329
D'Aquino <daquinohd@nci-02133271-ml.home>
2019-02-15 23:01:05 -0500

    JS and module cleanup

--- docroot/profiles/custom/cgov_site/modules/custom/cgov_blog_post/js/cgov_blog_post.admin.js
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_blog_post/js/cgov_blog_post.admin.js
@@ -1,6 +1,8 @@
 /**
  * @file
  * Placeholder file for custom blog post form behaviors.
+ * TODO: make selectors settings 
+ * 
  */
 (function($, Drupal) {
 
@@ -12,37 +14,44 @@
   Drupal.behaviors.blogPostForm = {
     attach: function (context, settings) {
 
-      console.log(settings.adminJS.isDraft);
-      console.log(settings.adminJS.idPairs);
+      //console.log(settings.adminJS.selectable);
+      //console.log(settings.adminJS.idPairs);
 
+      /** Initialize correct selections on ready(). */
+      $(function () {
+        filterTopicOptions(settings);
+      });
 
-
-
+      /** Clear checks and show correct selections on dropdown change. */
       $( "#edit-field-blog-series" ).change(function() {
-
-        $('#edit-field-blog-topics .js-form-item').addClass('visually-hidden');
         $('#edit-field-blog-topics .form-checkbox').prop('checked', false);
-    
+        filterTopicOptions(settings);
+      });
+
+      /**
+       * If the topic ID matches the series ID, remove the hidden class and show the checkboxes.
+       * 
+       * @param {*} settings 
+       *    Drupalsettings
+       */
+      function filterTopicOptions(settings) {
+        // Hide checkboxes to start.
+        $('#edit-field-blog-topics .js-form-item').addClass('visually-hidden');
+
         // Get the seleccted series ID.
         var sid = $('select#edit-field-blog-series')[0].value;
-    
-        // If the topic ID matches the series ID, remove the hidden class and show the checkboxes.
-        var topics = settings.adminJS.idPairs;
 
+        // Show every checkbox item that matches the owner series.
+        var topics = settings.adminJS.idPairs;
         for (var tid in topics) {
           if(topics[tid] === sid) {
             jQuery('input[value=' + tid + ']').parent().removeClass('visually-hidden');
           }
         }
+      } // filterTopicOptions()
 
-      });
-
-
-
-
-
-    }
-  };
-
+    } // attach()
+    
+  }; // Drupal.behaviors.blogPostForm {}
 
 })(jQuery, Drupal);
