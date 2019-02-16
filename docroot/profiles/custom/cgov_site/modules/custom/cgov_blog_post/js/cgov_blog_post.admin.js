24d431854e9b66e430d5acd84beecff3260544c1
D'Aquino <daquinohd@nci-02133271-ml.home>
2019-02-15 21:18:57 -0500

    Reworking category display JS

--- docroot/profiles/custom/cgov_site/modules/custom/cgov_blog_post/js/cgov_blog_post.admin.js
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_blog_post/js/cgov_blog_post.admin.js
@@ -11,36 +11,38 @@
    */
   Drupal.behaviors.blogPostForm = {
     attach: function (context, settings) {
-      if(context.querySelector('html')) {
-        // Do page load actions per state...
-      } else {
-        // console.log('ajax form debug');
-      }
+
+      console.log(settings.adminJS.isDraft);
+      console.log(settings.adminJS.idPairs);
+
+
+
+
+      $( "#edit-field-blog-series" ).change(function() {
+
+        $('#edit-field-blog-topics .js-form-item').addClass('visually-hidden');
+        $('#edit-field-blog-topics .form-checkbox').prop('checked', false);
+    
+        // Get the seleccted series ID.
+        var sid = $('select#edit-field-blog-series')[0].value;
+    
+        // If the topic ID matches the series ID, remove the hidden class and show the checkboxes.
+        var topics = settings.adminJS.idPairs;
+
+        for (var tid in topics) {
+          if(topics[tid] === sid) {
+            jQuery('input[value=' + tid + ']').parent().removeClass('visually-hidden');
+          }
+        }
+
+      });
+
+
+
+
+
     }
   };
 
-  /**
-   * Provides the ability to drag to manipulate a table and its fields.
-   * 
-   * @param {object} topics
-   *   Collection of Blog Topics data.
-   */
-  $.fn.drawBlogTopics = function(topics) {
-    // Uncheck and hide all checkboxes when this function is called.
-    $('#edit-field-blog-topics .js-form-item').addClass('visually-hidden');
-    $('#edit-field-blog-topics .form-checkbox').prop('checked', false);
-
-    // Get the seleccted series ID.
-    var sid = $('select#edit-field-blog-series')[0].value;
-
-    // If the topic ID matches the series ID, remove the hidden class and show the checkboxes.
-    for (var tid in topics) {
-      if(topics[tid] === sid) {
-        jQuery('input[value=' + tid + ']').parent().removeClass('visually-hidden');
-      }
-    }
-  };
-  // TODO: hide topics wrapper if not in categories
-  // TODO: make visually-hidden a wrapper
 
 })(jQuery, Drupal);
