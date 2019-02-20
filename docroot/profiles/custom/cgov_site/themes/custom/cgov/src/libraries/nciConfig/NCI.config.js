ccdfa7aabec2654ef32561075f7134c1067ae660
Frank Ali <frank.ali@nih.gov>
2019-02-20 14:50:41 -0500

    fixing indents

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/src/libraries/nciConfig/NCI.config.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/src/libraries/nciConfig/NCI.config.js
@@ -8,68 +8,68 @@ export const breakpoints = {
 
 export const lang = {
   Collapse: {
-      en: 'Collapse',
-      es: 'Reducir'
+    en: 'Collapse',
+    es: 'Reducir'
   },
   Expand: {
-      en: 'Expand',
-      es: 'Ampliar'
+    en: 'Expand',
+    es: 'Ampliar'
   },
   Section_Menu: {
-      en: 'Section Menu',
-      es: 'Menú de secciones'
+    en: 'Section Menu',
+    es: 'Menú de secciones'
   },
   Dictionary_More_Information: {
-      en: 'More information on dictionary page',
-      es: 'Más información'
+    en: 'More information on dictionary page',
+    es: 'Más información'
   },
   Definition_Title: {
-      en: 'Definition',
-      es: 'Definición'
+    en: 'Definition',
+    es: 'Definición'
   },
   Definition_Show_Full: {
-      en: 'full definition',
-      es: 'toda la definición'
+    en: 'full definition',
+    es: 'toda la definición'
   },
   Show: {
-      en: 'Show',
-      es: 'Mostrar'
+    en: 'Show',
+    es: 'Mostrar'
   },
   Hide: {
-      en: 'Hide',
-      es: 'Ocultar'
+    en: 'Hide',
+    es: 'Ocultar'
   },
   OnThisPage: {
-      en: 'On This Page',
-      es: 'En Esta Página'
+    en: 'On This Page',
+    es: 'En Esta Página'
   },
   InThisSection: {
-      en: 'In This Section',
-      es: 'En esta sección'
+    en: 'In This Section',
+    es: 'En esta sección'
   },
   Search: {
-      en: 'Search',
-      es: 'Buscar'
+    en: 'Search',
+    es: 'Buscar'
   },
   Find_Trials: {
-      en: 'Find Trials',
-      es: 'Encontrar Pruebas'
+    en: 'Find Trials',
+    es: 'Encontrar Pruebas'
   },
   CTS_Search_Hint: {
-      en: 'Start your search at any time.',
-      es: 'Comienzo su búsqueda en cualquier momento.'
+    en: 'Start your search at any time.',
+    es: 'Comienzo su búsqueda en cualquier momento.'
   },
   CTHPDropdown_Label: {
-      en: 'View more information',
-      es: 'Vea más información'
+    en: 'View more information',
+    es: 'Vea más información'
   },
   More_Information: {
-      en: 'More Information',
-      es: 'Más Información'
+    en: 'More Information',
+    es: 'Más Información'
   },
   View_On_Youtube: {
-      en: 'View this video on YouTube.',
-      es: 'Ver este video en YouTube.'
+    en: 'View this video on YouTube.',
+    es: 'Ver este video en YouTube.'
   }
 }
 
