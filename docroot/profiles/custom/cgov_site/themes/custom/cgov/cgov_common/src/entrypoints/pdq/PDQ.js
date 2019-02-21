b9334e6e10951f9645c932c04131bae9bf48bca8
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2019-02-20 23:11:57 -0500

    - added script to move toggle to head of article - toggle styling

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/entrypoints/pdq/PDQ.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/entrypoints/pdq/PDQ.js
@@ -1,1 +1,10 @@
+import $ from 'jquery';
 import './PDQ.scss';
+
+const onDOMContentLoaded = () => {
+    
+    // move health professional/patient toggle up to article head
+    $('.pdq-hp-patient-toggle').detach().insertAfter('h1');
+}
+
+document.addEventListener('DOMContentLoaded',onDOMContentLoaded);
