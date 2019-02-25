6d1f614d596175e2af99ca4a38f05da1a662d096
Frank Ali <frank.ali@nih.gov>
2019-02-25 16:31:29 -0500

    fix nested ul list opening and closing tag location

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/entrypoints/pdq/PDQ.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/entrypoints/pdq/PDQ.js
@@ -5,7 +5,7 @@ import './PDQ.scss';
 const onDOMContentLoaded = () => {
   // move health professional/patient toggle up to article head
   moveToggle();
-  
+
   buildInThisSection(getNodeArray('#cgvBody .accordion > section'));
 }
 
@@ -26,16 +26,25 @@ const buildInThisSection = (sections) => {
     if(section.id !== '_AboutThis_1' && headers.length > 0 && !headers[0].id.match(/kpBox/)) {
       let nav = `<nav class="in-this-section role="navigation"><h6>In This Section</h6><ul>`;
       headers.map((header,i) => {
+        // check to see if we need to nest a new unordered list for h4's
         if( i !== 0 && headers[i-1].nodeName.toLowerCase() === 'h3' && header.nodeName.toLowerCase() === 'h4' ){
-          nav += `<ul><li><a href="#${header.id}">${header.innerHTML}</a></li>`;
+          nav += `<ul><li><a href="#${header.id}">${header.innerHTML}</a>`;
         } else {
-          nav += `<li><a href="#${header.id}">${header.innerHTML}</a></li>`;
+          if(i === 0) {
+            // this is the first item in the list
+            nav += `<li><a href="#${header.id}">${header.innerHTML}</a>`;
+          } else {
+            // this is an h3 with a previous sibling of h3 or
+            // this is an h4 with a previous sibling of h4
+            nav += `</li><li><a href="#${header.id}">${header.innerHTML}</a>`;
+          }
         }
-        if (headers[i+1] && headers[i+1].nodeName === 'h3' && header.nodeName === 'h4') {
-          nav += `</ul>`;
+        // next header is an h3 so close this nested list
+        if (headers[i+1] && headers[i+1].nodeName.toLowerCase() === 'h3' && header.nodeName.toLowerCase() === 'h4') {
+          nav += `</li></ul>`;
         }
       });
-      nav += `</ul></nav>`;
+      nav += `</li></ul></nav>`;
 
       section.firstElementChild.insertAdjacentHTML('afterend',nav);
     }
