3746118d31ee2650bc4334159ec5f84909a92bda
daquinohd <daquinohd@noreploy.github.com>
2019-03-08 12:18:00 -0500

    Adding find-parent logic & debugging

--- docroot/profiles/custom/cgov_site/modules/custom/cgov_blog/src/Plugin/Block/CgovDisqus.php
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_blog/src/Plugin/Block/CgovDisqus.php
@@ -3,9 +3,15 @@
 namespace Drupal\cgov_blog\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\Query\QueryFactory;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Provides a Blog Categories Block.
+ * Provides a Disqus JS snippet Block.
  *
  * @Block(
  *   id = "cgov_disqus",
@@ -13,12 +19,126 @@ use Drupal\Core\Block\BlockBase;
  *   category = @Translation("Cgov Digital Platform"),
  * )
  */
-class CgovDisqus extends BlockBase {
+class CgovDisqus extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The route matcher.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatcher;
+
+  /**
+   * An entity query.
+   *
+   * @var Drupal\Core\Entity\Query\QueryFactory
+   */
+  protected $entityQuery;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a CgovPager object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_matcher
+   *   The route matcher.
+   * @param \Drupal\Core\Entity\Query\QueryFactory $entity_query
+   *   An entity query.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager service.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    RouteMatchInterface $route_matcher,
+    QueryFactory $entity_query,
+    EntityTypeManagerInterface $entity_type_manager
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->routeMatcher = $route_matcher;
+    $this->entityQuery = $entity_query;
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_route_match'),
+      $container->get('entity.query'),
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * Gets the current entity if there is one.
+   *
+   * @return Drupal\Core\Entity\ContentEntityInterface
+   *   The retrieved entity, or FALSE if none found.
+   */
+  private function getCurrEntity() {
+    $params = $this->routeMatcher->getParameters()->all();
+    foreach ($params as $param) {
+      if ($param instanceof ContentEntityInterface) {
+        // If you find a ContentEntityInterface stop iterating and return it.
+        return $param;
+      }
+    }
+    return FALSE;
+  }
+
+  /**
+   * Create a new node storage instance.
+   *
+   * @return Drupal\Core\Entity\EntityStorageInterface
+   *   The node storage or NULL.
+   */
+  private function getNodeStorage() {
+    $node_storage = $this->entityTypeManager->getStorage('node');
+    return isset($node_storage) ? $node_storage : NULL;
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
+
+    // Verify that this is an entity object.
+    if ($curr_entity = $this->getCurrEntity()) {
+      $bpid = $curr_entity->id();
+      $bsid = $curr_entity->get('field_blog_series')->target_id;
+      $owner = $this->getNodeStorage()->load($bsid)->get('field_pretty_url')->value;
+    }
+    // If not an entity object, exit build():
+    else {
+      return;
+    }
+
+    $debug = [
+      'debug' => '~~ Debugging array ~~',
+      'owner' => $owner,
+      'post-nid' => $bpid,
+      'series-nid' => $bsid,
+      'its-a-me' => 'Mario!',
+    ];
+    ksm($debug);
+
     $build = [
       '#markup' => $this->t('
         <div id="disqus_thread"></div>
