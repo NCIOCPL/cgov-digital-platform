4e6921fb3b5668255068f4b0a784bcaed9138981
Brendan Beltz <bbeltz@sapient.com>
2019-03-11 15:30:25 -0400

    Add in methods with dummy api and insert

--- docroot/profiles/custom/cgov_site/modules/custom/cgov_ckeditor/js/plugins/cgov_glossifier/dialogs/glossify.js
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_ckeditor/js/plugins/cgov_glossifier/dialogs/glossify.js
@@ -1,6 +1,29 @@
+function requestGlossification() {
+  fetch('http://api.open-notify.org/iss-now.json')
+    .then(res => res.json())
+    .then(res => {
+      const { latitude, longitude } = res.iss_position;
+      const htmlBody = `
+        <h2>ISS Current Position</h2>
+        <p>Latitude: ${ latitude }</p>
+        <p>Longitude: ${ longitude }</p>
+      `;
+      const htmlArea = this.getElement().getDocument().getById('dialog_container');
+      htmlArea.setHtml(htmlBody);
+    })
+  }
+
+function saveGlossificationChoices() {
+  const htmlArea = this.getElement().getDocument().getById('dialog_container').getHtml();
+  this._.editor.insertHtml(htmlArea);
+}
+
 CKEDITOR.dialog.add('glossifyDialog', function(editor) {
   return {
     title: 'Glossify Page',
+    buttons: [ CKEDITOR.dialog.cancelButton, CKEDITOR.dialog.okButton ],
+    onLoad: requestGlossification,
+    onOk: saveGlossificationChoices,
     minWidth: 600,
     minHeight: 400,
     contents: [
@@ -14,7 +37,7 @@ CKEDITOR.dialog.add('glossifyDialog', function(editor) {
             id: 'html',
             type: 'html',
             label: 'Select Elements to Glossify',
-            html: 'Jazz Hands!!!'
+            html: '<div id="dialog_container"><div id="spinner">Loading...</div></div>'
           }
         ],
       }
