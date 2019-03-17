040012e7f673ef9bca16835c4f8499ee5eac9527
daquinohd <daquinohd@noreploy.github.com>
2019-03-16 20:38:11 -0400

    Test run plugin manager

--- docroot/profiles/custom/cgov_site/modules/custom/cgov_blog/src/Plugin/Block/BlogCategories.php
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_blog/src/Plugin/Block/BlogCategories.php
@@ -2,7 +2,10 @@
 
 namespace Drupal\cgov_blog\Plugin\Block;
 
+use Drupal\cgov_blog\Services\BlogManagerInterface;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /*
  * NOTE: this is a dummy plugin for front-end templating only.
@@ -18,18 +21,48 @@ use Drupal\Core\Block\BlockBase;
  *   category = @Translation("Cgov Digital Platform"),
  * )
  */
-class BlogCategories extends BlockBase {
+class BlogCategories extends BlockBase implements ContainerFactoryPluginInterface {
+
+  public $isSeries = FALSE;
+  public $series = [];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(
+    // Constructor with args.
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    BlogManagerInterface $blog_manager
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->isSeries = $blog_manager->isSeries();
+    $this->series = $blog_manager->getSeries();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    // Construct a new instance of this class.
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('cgov_blog.blog_manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
+    $isSeries = '';
+    $working = (isset($isSeries)) ? 'true' : 'false';
     $build = [
-      '#markup' => $this->t('
-
-        <b>foo bar baz</b>
-
-      '),
+      '#markup' => '<b><i>Debug BlogCategories.php->build(): </i></b>
+                    <ol><li>' . $working . '</li></ol>',
     ];
     return $build;
   }
