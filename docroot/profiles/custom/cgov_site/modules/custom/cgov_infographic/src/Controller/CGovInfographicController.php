3932f0683a0cc2ffcdac0fbfa8c5d20cb7f7be70
Blair Learn <blairlearn@users.noreply.github.com>
2019-03-18 16:27:28 -0400

    Render longdesc with route to output the longdesc text.

---
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_infographic/src/Controller/CGovInfographicController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\cgov_infographic\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+/**
+ * Controller routines for cgov_infographic routes.
+ */
+class CGovInfographicController extends ControllerBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getModuleName() {
+    return 'cgov_infographic';
+  }
+
+  /**
+   * Generates an infographic's long description.
+   *
+   * @param int $node
+   *   Id of the infographic to render.
+   */
+  public function longDescription($node) {
+    // Make sure you don't trust the URL to be safe! Always check for exploits.
+    if (!is_numeric($node)) {
+      // We will just show a standard "access denied" page in this case.
+      throw new AccessDeniedHttpException();
+    }
+  }
+
+}
