ba39ad534721cf4f016c908ceebe73c8aa1a4abd
daquinohd <daquinohd@noreploy.github.com>
2019-03-19 22:06:16 -0400

    Moved QueryFactory out of BlogArchive and into manager class

--- docroot/profiles/custom/cgov_site/modules/custom/cgov_blog/src/Plugin/Block/BlogArchive.php
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_blog/src/Plugin/Block/BlogArchive.php
@@ -4,7 +4,6 @@ namespace Drupal\cgov_blog\Plugin\Block;
 
 use Drupal\cgov_blog\Services\BlogManagerInterface;
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Entity\Query\QueryFactory;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -27,13 +26,6 @@ class BlogArchive extends BlockBase implements ContainerFactoryPluginInterface {
   public $blogManager;
 
   /**
-   * An entity query.
-   *
-   * @var Drupal\Core\Entity\Query\QueryFactory
-   */
-  protected $entityQuery;
-
-  /**
    * Constructs a blog entity object.
    *
    * @param array $configuration
@@ -44,19 +36,15 @@ class BlogArchive extends BlockBase implements ContainerFactoryPluginInterface {
    *   The plugin implementation definition.
    * @param \Drupal\cgov_blog\Services\BlogManagerInterface $blog_manager
    *   A blog manager object.
-   * @param \Drupal\Core\Entity\Query\QueryFactory $entity_query
-   *   An entity query.
    */
   public function __construct(
     array $configuration,
     $plugin_id,
     $plugin_definition,
-    BlogManagerInterface $blog_manager,
-    QueryFactory $entity_query
+    BlogManagerInterface $blog_manager
   ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->blogManager = $blog_manager;
-    $this->entityQuery = $entity_query;
   }
 
   /**
@@ -67,8 +55,7 @@ class BlogArchive extends BlockBase implements ContainerFactoryPluginInterface {
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('cgov_blog.blog_manager'),
-      $container->get('entity.query')
+      $container->get('cgov_blog.blog_manager')
     );
   }
 
@@ -105,12 +92,8 @@ class BlogArchive extends BlockBase implements ContainerFactoryPluginInterface {
    *   The content type machine name.
    */
   private function getMonthsYears($cid, $content_type) {
-    // Build our query object.
-    $query = $this->entityQuery->get('node');
-    $query->condition('status', 1);
-    $query->condition('type', $content_type);
-    $query->sort('field_date_posted', 'DESC');
-    $entity_ids = $query->execute();
+    // Query to retrieve available nodes.
+    $entity_ids = $this->blogManager->getNodesByPostedDateDesc($content_type, '');
 
     // Create series filter.
     $filter_node = $this->blogManager->getNodeStorage()->load($cid);
--- docroot/profiles/custom/cgov_site/modules/custom/cgov_blog/src/Services/BlogManager.php
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_blog/src/Services/BlogManager.php
@@ -4,19 +4,13 @@ namespace Drupal\cgov_blog\Services;
 
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\Query\QueryFactory;
 use Drupal\Core\Routing\RouteMatchInterface;
 
 /**
  * Blog Manager Service.
  */
 class BlogManager implements BlogManagerInterface {
-
-  /**
-   * The route matcher.
-   *
-   * @var \Drupal\Core\Routing\RouteMatchInterface
-   */
-  protected $routeMatcher;
 
   /**
    * An entity query.
@@ -33,16 +27,28 @@ class BlogManager implements BlogManagerInterface {
   protected $entityTypeManager;
 
   /**
+   * The route matcher.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatcher;
+
+  /**
    * Constructor for BlogManager object.
    *
+   * @param \Drupal\Core\Entity\Query\QueryFactory $entity_query
+   *   An entity query.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
+   *   The entity type manager service.
    * @param \Drupal\Core\Routing\RouteMatchInterface $route_matcher
    *   The route matcher.
    */
   public function __construct(
+    QueryFactory $entity_query,
     EntityTypeManagerInterface $entity_type_manager,
-    RouteMatchInterface $route_matcher) {
+    RouteMatchInterface $route_matcher
+  ) {
+    $this->entityQuery = $entity_query;
     $this->entityTypeManager = $entity_type_manager;
     $this->routeMatcher = $route_matcher;
   }
@@ -131,6 +137,23 @@ class BlogManager implements BlogManagerInterface {
   public function getTaxonomyStorage() {
     $taxonomy_storage = $this->entityTypeManager->getStorage('taxonomy_term');
     return isset($taxonomy_storage) ? $taxonomy_storage : NULL;
+  }
+
+  /**
+   * Return query results based on date posted.
+   *
+   * @param string $type
+   *   Content type or bundle.
+   * @param string $lang
+   *   Language of current node.
+   */
+  public function getNodesByPostedDateDesc($type, $lang) {
+    $query = $this->entityQuery->get('node');
+    $query->condition('status', 1);
+    $query->condition('type', $type);
+    $query->sort('field_date_posted', 'DESC');
+    $nids = $query->execute();
+    return $nids;
   }
 
 }
