3436d5fa45db1bfa5b5d3e926ed979dd8073cbc6
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2019-03-25 16:28:35 -0400

    making dynamic lists follow the same paradigm as managed lists and added some shameful jquery for shared hover state to work

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/entrypoints/homelanding/Homelanding.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/entrypoints/homelanding/Homelanding.js
@@ -1,1 +1,19 @@
-import './Homelanding.scss';
+import "./Homelanding.scss";
+
+const onDOMContentLoaded = () => {
+  // thumbnail list images and titles share a hover state
+  $(".managed .title").hover(
+    function() {
+      $(this)
+        .closest(".media")
+        .addClass("hover");
+    },
+    function() {
+      $(this)
+        .closest(".media")
+        .removeClass("hover");
+    }
+  );
+};
+
+document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
