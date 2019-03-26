7d2b06672bcb612631e5f555dc603becdc6cbf7e
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2019-03-25 21:02:20 -0400

    hovers are no longer coupled

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/entrypoints/homelanding/Homelanding.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/entrypoints/homelanding/Homelanding.js
@@ -1,19 +1,1 @@
 import "./Homelanding.scss";
-
-const onDOMContentLoaded = () => {
-  // thumbnail list images and titles share a hover state
-  $(".managed .title").hover(
-    function() {
-      $(this)
-        .closest(".media")
-        .addClass("hover");
-    },
-    function() {
-      $(this)
-        .closest(".media")
-        .removeClass("hover");
-    }
-  );
-};
-
-document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
