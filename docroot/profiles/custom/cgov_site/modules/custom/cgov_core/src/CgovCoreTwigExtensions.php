8bee728256b9c86824e4b6e3c558e88285c362fb
Brian Gallagher <brian.gallagher@nih.gov>
2019-03-29 12:03:39 -0400

    Refactor Extensions, Add blog feed channel title/descriptions, improve template docs

--- docroot/profiles/custom/cgov_site/modules/custom/cgov_core/src/CgovCoreTwigExtensions.php
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_core/src/CgovCoreTwigExtensions.php
@@ -4,6 +4,8 @@ namespace Drupal\cgov_core;
 
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\file\Entity\File;
+use Drupal\views\ViewExecutable;
 use Drupal\image\Entity\ImageStyle;
 use Symfony\Component\HttpFoundation\RequestStack;
 
@@ -63,6 +65,7 @@ class CgovCoreTwigExtensions extends \Twig_Extension {
    */
   public function getFunctions() {
     return [
+      new \Twig_SimpleFunction('get_blog_info', [$this, 'getBlogInfo'], ['is_safe' => ['html']]),
       new \Twig_SimpleFunction('get_enclosure', [$this, 'getEnclosure'], ['is_safe' => ['html']]),
       new \Twig_SimpleFunction('get_list_description', [$this, 'getListDescription'], ['is_safe' => ['html']]),
       new \Twig_SimpleFunction('get_translated_absolute_path', [$this, 'getTranslatedAbsolutePath'], ['is_safe' => ['html']]),
@@ -81,23 +84,255 @@ class CgovCoreTwigExtensions extends \Twig_Extension {
    *   generated enclosure tag.
    */
   public function getEnclosure($nid) {
-    // Set $debug=TRUE to show displayed image source in comments.
-    $debug = FALSE;
-
-    // Strip any theme debugging and cast to numeric.
-    $nid = (int) strip_tags($nid);
+    $node = $this->getTranslatedNodeById($nid);
+    if (!$node) {
+      return NULL;
+    }
 
     // Image style to use when rendering images.
     $thumbnail_image_style = 'cgov_thumbnail';
 
+    $media_image_file = $this->getNodeImageFile($nid);
+    $image_info = $this->getImageData($media_image_file, $thumbnail_image_style);
+
+    $enclosure = "<enclosure url='" . $image_info['absolute_uri'] .
+      "' length='" . $image_info['filesize'] .
+      "' type='" . $image_info['mimetype'] . "' />";
+
+    return $enclosure;
+  }
+
+  /**
+   * Return metadata about an Image Filefield.
+   *
+   * Get file URL, filesize and mimetype for an image field rendered to a
+   * provided ImageStyle.  The image's imagestyle file will be created on the
+   * filesystem (which is required to determine the filesize properly).
+   *
+   * @param Drupal\file\Entity\File $media_image_file
+   *   The File entity of an image field.
+   * @param string $image_style
+   *   The Image Style to render the image with.
+   *
+   * @return array
+   *   Returns information about the image.
+   */
+  public function getImageData(File $media_image_file, $image_style) {
+    // Get URI of media image file (eg: public://2019-03/image.jpg).
+    $image_uri = $media_image_file->getFileUri();
+
+    // Generate new derivative image from imagestyle.
+    /** @var \Drupal\image\Entity\ImageStyle $imageStyle */
+    $imageStyle = ImageStyle::load($image_style);
+    // Get thumbnail_uri
+    // (eg: public://styles/cgov_thumbail/public/2019-03/image.jpg).
+    $thumbnail_uri = $imageStyle->buildUri($image_uri);
+    // Create the styled image on the filesystem
+    // (needed to find final file size and mime type).
+    $imageStyle->createDerivative($image_uri, $thumbnail_uri);
+    // Convert 'public://path/image.jpg'
+    // to '/sites/default/files/path/image.jpg'.
+    $relative_imagestyle_uri = file_url_transform_relative(file_create_url($thumbnail_uri));
+    // Remove querystring, if present (eg: path/image.jpg?h=98765az)
+    $relative_imagestyle_uri = strtok($relative_imagestyle_uri, '?');
+    // Add HTTP scheme (HTTP[S]) and hostname.
+    $absolute_imagestyle_url = $this->requestStack->getCurrentRequest()->getSchemeAndHttpHost() . ltrim(base_path(), '/') . $relative_imagestyle_uri;
+
+    // Get the filesystem path (not web path) to the imagestyle
+    // file so we can get filesize.
+    $imagestyle_filename = realpath(".") . $relative_imagestyle_uri;
+    $styled_file_size = filesize($imagestyle_filename);
+    $styled_mime_type = image_type_to_mime_type(exif_imagetype($imagestyle_filename));
+
+    $image_info['imagestyle'] = $image_style;
+    $image_info['relative_uri'] = $relative_imagestyle_uri;
+    $image_info['absolute_uri'] = $absolute_imagestyle_url;
+    $image_info['filename'] = $imagestyle_filename;
+    $image_info['filesize'] = $styled_file_size;
+    $image_info['mimetype'] = $styled_mime_type;
+
+    return($image_info);
+  }
+
+  /**
+   * Select either the List Description or the Page/Meta Description.
+   *
+   * Call this function with {{ get_list_description (nid) }}
+   *
+   * @param int $nid
+   *   Node ID to load description from.
+   *
+   * @return string
+   *   Selected description.
+   */
+  public function getListDescription($nid) {
+    $node = $this->getTranslatedNodeById($nid);
+    if (!$node) {
+      return NULL;
+    }
+
+    // Check for available descriptions to display.
+    if ($node->hasField('field_page_description')) {
+      $description = $node->field_page_description->value;
+    }
+    if ($node->hasField('field_list_description') && !$node->field_list_description->isEmpty()) {
+      $description = $node->field_list_description->value;
+    }
+    return $description;
+  }
+
+  /**
+   * Get the translated absolute path to the node.
+   *
+   * Call this function with {{ get_translated_path(nid) }}.
+   * This is a workaround to the many Core issues about views not returning the
+   * proper path for translations:
+   * https://www.drupal.org/project/drupal/issues/2877994
+   * https://www.drupal.org/project/drupal/issues/2957917
+   * https://www.drupal.org/project/drupal/issues/2862511
+   * https://www.drupal.org/project/drupal/issues/2802311
+   * etc.
+   *
+   * @param int $nid
+   *   Node ID to load description from.
+   *
+   * @return string
+   *   Path to node.
+   */
+  public function getTranslatedAbsolutePath($nid) {
+    $node = $this->getTranslatedNodeById($nid);
+
+    // Verify node was found.
+    if (!$node) {
+      return FALSE;
+    }
+
+    // Get translated absolute link.
+    $link = $node->toUrl('canonical', ['absolute' => TRUE])->toString();
+
+    return $link;
+  }
+
+  /**
+   * Get the translated version of entity, if available.
+   */
+  private function getTranslationIfExists($entity) {
+    if ($entity && $entity->hasTranslation($this->langid)) {
+      $entity = $entity->getTranslation($this->langid);
+    }
+    return $entity;
+  }
+
+  /**
+   * Get the translated node by NID.
+   *
+   * Load the translated version of a node by NID.  Will not return node if
+   * the current language's translation is not found.
+   *
+   * @param int $nid
+   *   Node ID to create enclosure tag from.
+   *
+   * @return \Drupal\node\Entity\Node
+   *   Node or NULL.
+   */
+  public function getTranslatedNodeById($nid) {
+    // Strip any theme debugging and cast to numeric.
+    $nid = (int) strip_tags($nid);
+
     // Load the node's translation, if available.
-    $node = $this->entityTypeManager->getStorage('node')->load($nid);
+    $node = $this->getTranslationIfExists($this->entityTypeManager->getStorage('node')->load($nid));
     if ($node && $node->hasTranslation($this->langid)) {
       $node = $node->getTranslation($this->langid);
     }
     else {
-      // No translation for this node, don't display it.
-      return FALSE;
+      return NULL;
+    }
+
+    return $node;
+  }
+
+  /**
+   * Get the Blog Information about Series and Topics.
+   *
+   * @param Drupal\views\ViewExecutable $view
+   *   The current view being displayed.  Uses Contextual Filters for details.
+   *
+   * @return array
+   *   Array containing information for the <channel><title>/<description> tags.
+   */
+  public function getBlogInfo(ViewExecutable $view) {
+    list($series_nid, $topic_tid) = $view->args;
+
+    if ($series_nid) {
+      // Get Blog Series Name.
+      $node = $this->getTranslatedNodeById($series_nid);
+      if (!$node) {
+        print "<!-- ERROR: No NID $series_nid with '{$this->langid}' Translation -->";
+        return NULL;
+      }
+      $series_name = $node->label();;
+      $series_desc = "SERIESDESC:" . $node->field_about_blog->value;
+    }
+
+    if ($topic_tid) {
+      // Get Blog Topics Name.
+      $term = $this->entityTypeManager->getStorage('taxonomy_term')->load($topic_tid);
+      $topic_name = $term->getName();
+      $topic_desc = $term->getDescription();
+    }
+
+    // Figure out the proper Title/Description to use.
+    if (!$series_nid && !$topic_tid) {
+      // All posts for all blogs.
+      $title = 'NCI Blogs';
+      $desc = 'All NCI Blog Posts';
+    }
+    elseif ($series_nid && !$topic_tid) {
+      // All posts for a blog, all topics.
+      $title = $series_name;
+      $desc = $series_desc;
+    }
+    elseif (!$series_nid && $topic_tid) {
+      // All blog posts across all blogs for a Topic.
+      $title = "All blogs on $topic_name";
+      $desc = "Posts on $topic_desc across all NCI blogs";
+    }
+    else {
+      // All posts from a Single blog on a Single Topic.
+      $title = "$series_name posts on $topic_name";
+      $desc = $series_name;
+    }
+
+    // Return metadata.
+    $result['series'] = $series_name;
+    $result['series_desc'] = $series_desc;
+    $result['topic'] = $topic_name;
+    $result['topic_desc'] = $topic_desc;
+    $result['title'] = $title;
+    $result['desc'] = $desc;
+
+    return $result;
+  }
+
+  /**
+   * Get Lead or Promo Image.
+   *
+   * Select the proper image file to display for a node
+   * (either Lead Image or Promo Image or none).
+   *
+   * @param int $nid
+   *   Node ID to create enclosure tag from.
+   *
+   * @return Drupal\file\Entity\File
+   *   The specified node or NULL
+   */
+  public function getNodeImageFile($nid) {
+    // Set $debug=TRUE to show displayed image source in comments.
+    $debug = FALSE;
+
+    $node = $this->getTranslatedNodeById($nid);
+    if (!$node) {
+      return NULL;
     }
 
     // Check for available images to display.
@@ -149,6 +384,10 @@ class CgovCoreTwigExtensions extends \Twig_Extension {
       }
     }
 
+    if ($debug) {
+      print "<!-- image source: $displaying -->";
+    }
+
     if (!$media_image_file) {
       if ($debug) {
         $displaying = 'NO IMAGE';
@@ -156,125 +395,7 @@ class CgovCoreTwigExtensions extends \Twig_Extension {
       return FALSE;
     }
 
-    // Get URI of media image file (eg: public://2019-03/image.jpg).
-    $image_uri = $media_image_file->getFileUri();
-
-    // Generate new derivative image from imagestyle.
-    /** @var \Drupal\image\Entity\ImageStyle $imagestyle */
-    $imagestyle = ImageStyle::load($thumbnail_image_style);
-    // Get thumbnail_uri
-    // (eg: public://styles/cgov_thumbail/public/2019-03/image.jpg).
-    $thumbnail_uri = $imagestyle->buildUri($image_uri);
-    // Create the styled image on the filesystem
-    // (needed to find final file size and mime type).
-    $imagestyle->createDerivative($image_uri, $thumbnail_uri);
-    // Convert 'public://path/image.jpg'
-    // to '/sites/default/files/path/image.jpg'.
-    $relative_imagestyle_uri = file_url_transform_relative(file_create_url($thumbnail_uri));
-    // Remove querystring, if present (eg: path/image.jpg?h=98765az)
-    $relative_imagestyle_uri = strtok($relative_imagestyle_uri, '?');
-    // Add HTTP scheme (HTTP[S]) and hostname.
-    $absolute_imagestyle_url = $this->requestStack->getCurrentRequest()->getSchemeAndHttpHost() . ltrim(base_path(), '/') . $relative_imagestyle_uri;
-
-    // Get the filesystem path (not web path) to the imagestyle
-    // file so we can get filesize.
-    $imagestyle_filename = realpath(".") . $relative_imagestyle_uri;
-    $styled_file_size = filesize($imagestyle_filename);
-    $styled_mime_type = image_type_to_mime_type(exif_imagetype($imagestyle_filename));
-
-    $enclosure = "<enclosure url='$absolute_imagestyle_url' length='$styled_file_size' type='$styled_mime_type' />";
-
-    if ($debug) {
-      $enclosure .= "<!-- image source: $displaying -->";
-    }
-
-    return $enclosure;
-  }
-
-  /**
-   * Select either the List Description or the Page/Meta Description.
-   *
-   * Call this function with {{ get_list_description (nid) }}
-   *
-   * @param int $nid
-   *   Node ID to load description from.
-   *
-   * @return string
-   *   selected description.
-   */
-  public function getListDescription($nid) {
-    // Strip any theme debugging and cast to numeric.
-    $nid = (int) strip_tags($nid);
-
-    // Load the node's translation, if available.
-    $node = $this->entityTypeManager->getStorage('node')->load($nid);
-    if ($node && $node->hasTranslation($this->langid)) {
-      $node = $node->getTranslation($this->langid);
-    }
-    else {
-      return FALSE;
-    }
-
-    // Check for available descriptions to display.
-    if ($node->hasField('field_page_description')) {
-      $description = $node->field_page_description->value;
-    }
-    if ($node->hasField('field_list_description') && !$node->field_list_description->isEmpty()) {
-      $description = $node->field_list_description->value;
-    }
-    return $description;
-  }
-
-  /**
-   * Get the translated path to the node.
-   *
-   * Call this function with {{ get_translated_path(nid) }}.
-   * This is a workaround to the many Core issues about views not returning the
-   * proper path for translations:
-   * https://www.drupal.org/project/drupal/issues/2877994
-   * https://www.drupal.org/project/drupal/issues/2957917
-   * https://www.drupal.org/project/drupal/issues/2862511
-   * https://www.drupal.org/project/drupal/issues/2802311
-   * etc.
-   *
-   * @param int $nid
-   *   Node ID to load description from.
-   *
-   * @return string
-   *   Path to node.
-   */
-  public function getTranslatedAbsolutePath($nid) {
-    // Strip any theme debugging and cast to numeric.
-    $nid = (int) strip_tags($nid);
-
-    // Load the node's translation, if available.
-    $node = $this->getTranslationIfExists($this->entityTypeManager->getStorage('node')->load($nid));
-    if ($node && $node->hasTranslation($this->langid)) {
-      $node = $node->getTranslation($this->langid);
-    }
-    else {
-      return FALSE;
-    }
-
-    // Verify node was found.
-    if (!$node) {
-      return FALSE;
-    }
-
-    // Get translated absolute link.
-    $link = $node->toUrl('canonical', ['absolute' => TRUE])->toString();
-
-    return $link;
-  }
-
-  /**
-   * Get the translated version of entity, if available.
-   */
-  private function getTranslationIfExists($entity) {
-    if ($entity && $entity->hasTranslation($this->langid)) {
-      $entity = $entity->getTranslation($this->langid);
-    }
-    return $entity;
+    return($media_image_file);
   }
 
 }
