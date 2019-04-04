4bdf216a85e72238122e8c7a1ee2ae95c5513f54
Blair Learn <blairlearn@users.noreply.github.com>
2019-04-04 14:41:40 -0400

    Set explicit entityID.

--- simplesamlphp/config/authsources.php
+++ simplesamlphp/config/authsources.php
@@ -18,7 +18,7 @@ $config = [
 
         // The entity ID of this SP.
         // Can be NULL/unset, in which case an entity ID is generated based on the metadata URL.
-        'entityID' => null,
+        'entityID' => 'https://www-int-ac.cancer.gov/simplesaml/module.php/saml/sp/metadata.php/cancergov-sp',
 
         // The entity ID of the IdP this SP should contact.
         // Can be NULL/unset, in which case the user will be shown a list of available IdPs.
