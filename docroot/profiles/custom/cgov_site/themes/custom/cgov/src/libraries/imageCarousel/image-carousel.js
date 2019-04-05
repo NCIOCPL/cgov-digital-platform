b39ee0be523385214b72da3b3883e8fd2b825c84
BrendanBeltzNIH <34315527+BrendanBeltzNIH@users.noreply.github.com>
2019-04-05 16:45:02 -0400

    Feature/image carousel (#1348)
    * Created Image Carousel Block Type (Closes #964)
    * Created Look and Feel for Carousel (Closes #1050)
    * Implemented WYSIWYG Embed for Carousel Block

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/src/libraries/imageCarousel/image-carousel.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/src/libraries/imageCarousel/image-carousel.js
@@ -3,54 +3,62 @@ import './jquery.ui.imagecarousel'
 import AdobeAnalytics from 'Core/patches/adobeAnalyticsPatch';
 
 function _initialize() {
-    // Get language for previous/next buttons
-    var prev = "Previous";
-    var next = "Next";
-    if ($('html').attr("lang") === "es") {
-        prev = "Anterior";
-        next = "Siguiente";
-    }
+  // We want a guard clause to limit the weight of this library as it
+  // now will load on all pages through Common.
+  // NOTE: There is no need to use querySelectorAll as we're just testing if any exist.
+  const hasCarousel = document.querySelector('.ic-carousel') != null;
+  if(!hasCarousel){
+    return;
+  }
 
-    // Set pagename variable for analytics function
-    var pageName = 'www.cancer.gov/';
-    var s = AdobeAnalytics.getSObject();
-    if(typeof(s) !== 'undefined') {
-        pageName = s.pageName;
-    }
+  // Get language for previous/next buttons
+  var prev = "Previous";
+  var next = "Next";
+  if ($('html').attr("lang") === "es") {
+      prev = "Anterior";
+      next = "Siguiente";
+  }
 
-    // Iterate over each carousel on the page.
-    $('.ic-carousel').each(function(i, el) {
-        var $carousel = $(this).imagecarousel({
-            // Attach on analytics handlers here
-            change: function(event, eventData) {
-                // Set variables for analytics function
-                var safeTitle = $(this).imagecarousel('getTitle');
-                if(safeTitle.length > 50) {
-                    safeTitle = safeTitle.substring(0,50);
-                }
-                if (safeTitle.length == 0) {
-                    safeTitle = "none";
-                }
-                var action = eventData.triggerEvent;
-                var direction = eventData.direction;
-                var imgNum = eventData.beforeIndex + 1;
+  // Set pagename variable for analytics function
+  var pageName = 'www.cancer.gov/';
+  var s = AdobeAnalytics.getSObject();
+  if(typeof(s) !== 'undefined') {
+      pageName = s.pageName;
+  }
 
-                NCIAnalytics.ImageCarouselClickSwipe($(this), safeTitle, action, direction, imgNum, pageName);
-            },
-            previousText: prev,
-            nextText: next
-        });
+  // Iterate over each carousel on the page.
+  $('.ic-carousel').each(function(i, el) {
+      var $carousel = $(this).imagecarousel({
+          // Attach on analytics handlers here
+          change: function(event, eventData) {
+              // Set variables for analytics function
+              var safeTitle = $(this).imagecarousel('getTitle');
+              if(safeTitle.length > 50) {
+                  safeTitle = safeTitle.substring(0,50);
+              }
+              if (safeTitle.length == 0) {
+                  safeTitle = "none";
+              }
+              var action = eventData.triggerEvent;
+              var direction = eventData.direction;
+              var imgNum = eventData.beforeIndex + 1;
 
-        // check if carousel is inside an accordion, which can cause it's width to be 0 if accordion panel is not visible
-        $(window).on('accordionactivate',function(e,ui){
-            // on activation of accordion panels, check that the activated panel is the same one the carousel is in
-            if($carousel.closest('.ui-accordion-content').is(ui.newPanel)){
-                // use setPosition to trigger a redraw so width is not 0
-                $carousel.imagecarousel('setPosition');
-            }
-        });
+              NCIAnalytics.ImageCarouselClickSwipe($(this), safeTitle, action, direction, imgNum, pageName);
+          },
+          previousText: prev,
+          nextText: next
+      });
 
-    });
+      // check if carousel is inside an accordion, which can cause it's width to be 0 if accordion panel is not visible
+      $(window).on('accordionactivate',function(e,ui){
+          // on activation of accordion panels, check that the activated panel is the same one the carousel is in
+          if($carousel.closest('.ui-accordion-content').is(ui.newPanel)){
+              // use setPosition to trigger a redraw so width is not 0
+              $carousel.imagecarousel('setPosition');
+          }
+      });
+
+  });
 }
 
 let initialized = false;
