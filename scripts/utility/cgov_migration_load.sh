b7f675a94a540f70402b68fa2532cb6f2e3ab20d
Adrian Cofie <acofie@sapient.com>
2019-05-01 13:44:50 -0400

    Consolidating ticket509 commits into 1

--- scripts/utility/cgov_migration_load.sh
+++ scripts/utility/cgov_migration_load.sh
@@ -4,8 +4,10 @@
 set -ev
 
 if [ $MIGRATION = 1 ]; then
+    drush mim summary_migration && drush mim summaryes_migration && drush mim dis_migration
     drush mim externallinksql_migration; drush mim internallinksql_migration; drush mim citation_migration
     drush mim paragraph_en_migration; drush mim paragraph_es_migration
+    drush mim cgovimage_migration && drush mim cgovimage_es_migration
     drush mim article_en_migration && drush mim article_es_migration
 else
     echo 'MIGRATION NOT SET'
