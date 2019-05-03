8ced58f53908b67f5cb04497e4b2b857601b465b
Adrian Cofie <acofie@sapient.com>
2019-05-03 12:40:45 -0400

    Add og:image tag logic.

---
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_image/src/CgovImageTools.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\cgov_image;
+
+/**
+ * A service which provides methods to alter and configure form elements.
+ */
+class CgovImageTools {
+
+  /**
+   * Helper function to get the crop name from the image style.
+   *
+   * @param string $image_style
+   *   The image_style to find the crop for.
+   *
+   * @return string
+   *   NULL if the style does not have a crop, the name of the crop otherwise.
+   */
+  public function findCropByStyle($image_style) {
+    // TODO: Actually find the crop from the style.
+    switch ($image_style) {
+      case "cgov_article":
+      case "cgov_enlarged":
+        return "freeform";
+
+      case "cgov_featured":
+      case "cgov_social_media":
+        return "4x3";
+
+      case "cgov_panoramic":
+        return "16x9";
+
+      case "cgov_thumbnail":
+        return "thumbnail";
+    }
+    return NULL;
+  }
+
+}
