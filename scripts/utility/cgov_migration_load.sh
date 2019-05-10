13dae7dafd54447500af62659cb68cfdb259cb31
Gao <gaoz@nci-02090702-ml.nih.gov>
2019-05-10 15:57:22 -0400

    video, infographic, blog etc.

--- scripts/utility/cgov_migration_load.sh
+++ scripts/utility/cgov_migration_load.sh
@@ -8,7 +8,21 @@ if [ $MIGRATION = 1 ]; then
     drush mim externallinksql_migration; drush mim internallinksql_migration; drush mim citation_migration
     drush mim paragraph_en_migration; drush mim paragraph_es_migration
     drush mim cgovimage_migration && drush mim cgovimage_es_migration
+    drush mim pressrelease_en_migration && drush mim pressrelease_es_migration
     drush mim article_en_migration && drush mim article_es_migration
+    drush mim video_en_migration && drush mim video_es_migration
+    drush mim infographic_en_migration && drush mim infographic_es_migration
+
+    drush mim region_migration && drush mim cancercentertype_migration && drush mim cancercenter_migration
+
+    drush mim blogseries_en_migration && drush mim blogseries_es_migration && drush mim blogtopics_migration && drush mim blogtopics_es_migration
+    drush mim blogpost_en_migration && drush mim blogpost_es_migration
+
+
+    drush mim cancerresearch_en_migration && drush mim cancerresearch_es_migration
+
+    drush mim contextualimage_migration && drush mim contextualimage_es_migration
+
 else
     echo 'MIGRATION NOT SET'
 fi
