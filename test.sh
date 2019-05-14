94fa1fdb44425d74ea503808f2b30c0038e5ee1a
Gao <gaoz@nci-02090702-ml.nci.nih.gov>
2019-05-14 13:36:47 -0400

    promocard externalpromocard added

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 #
 set -ev
 
-if [ $MIGRATION = 1 ]; then
+
     drush mim summary_migration && drush mim summaryes_migration && drush mim dis_migration
     drush mim externallinksql_migration; drush mim internallinksql_migration; drush mim citation_migration
     drush mim paragraph_en_migration; drush mim paragraph_es_migration
@@ -13,9 +13,7 @@ if [ $MIGRATION = 1 ]; then
     drush mim video_en_migration && drush mim video_es_migration
     drush mim infographic_en_migration && drush mim infographic_es_migration
 
-    drush mim region_migration && drush mim cancercentertype_migration && drush mim cancercenter_migration
-
-    drush mim promocard_migration; drush mim externalpromocard_migration
+    drush mim region_migration && drush mim cancercentertype_migration && drush mim cancercancer_migration
 
     drush mim blogseries_en_migration && drush mim blogseries_es_migration && drush mim blogtopics_migration && drush mim blogtopics_es_migration
     drush mim blogpost_en_migration && drush mim blogpost_es_migration
@@ -25,8 +23,5 @@ if [ $MIGRATION = 1 ]; then
 
     drush mim contextualimage_migration && drush mim contextualimage_es_migration
 
-else
-    echo 'MIGRATION NOT SET'
-fi
-
 set +v
+
