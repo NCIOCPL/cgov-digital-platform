403e6c6dcbd7f350e4d32a8f2fa86652316ef525
Gao <gaoz@nci-02090702-ml.nci.nih.gov>
2019-05-15 09:34:30 -0400

    add promocard and minilanding

--- scripts/utility/cgov_migration_load.sh
+++ scripts/utility/cgov_migration_load.sh
@@ -16,14 +16,24 @@ if [ $MIGRATION = 1 ]; then
     drush mim region_migration && drush mim cancercentertype_migration && drush mim cancercenter_migration
 
     drush mim promocard_migration; drush mim externalpromocard_migration
+    drush mim contentblock_migration; drush mim rawhtml_migration
+
+    drush mim primaryfeaturecardrow_migration && drush mim secondaryfeaturecardrow_migration && drush mim list_migration && drush mim multimediarow_migration
+
+    drush mim twocolumnrow_migration && drush mim guidecardrow_migration
+
+    drush mim twoitemfeaturecardrow_migration
+
 
     drush mim blogseries_en_migration && drush mim blogseries_es_migration && drush mim blogtopics_migration && drush mim blogtopics_es_migration
     drush mim blogpost_en_migration && drush mim blogpost_es_migration
 
+    drush mim minilanding_en_migration && drush mim minilanding_es_migration
 
     drush mim cancerresearch_en_migration && drush mim cancerresearch_es_migration
 
     drush mim contextualimage_migration && drush mim contextualimage_es_migration
+    drush mim file_en_migration
 
 else
     echo 'MIGRATION NOT SET'
