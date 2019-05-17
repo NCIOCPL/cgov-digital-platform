10bcd3ccdb9af49bad003f085b92a20aacfccadc
John Doyle <digitalpolygon@Johns-MacBook-Pro.local>
2019-05-16 21:55:33 -0400

    NCI-000: Adding prototype for parallel phpunit tests to see how it reacts in pipelines.

---
+++ blt/src/Commands/ParatestCommand.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Acquia\Blt\Custom\Commands;
+
+use Acquia\Blt\Robo\BltTasks;
+use Acquia\Blt\Custom\ParatestTrait;
+use Acquia\Blt\Robo\Exceptions\BltException;
+use Robo\Contract\VerbosityThresholdInterface;
+
+/**
+ * Defines commands in the "tests" namespace.
+ */
+class ParatestCommand extends BltTasks {
+  use ParatestTrait;
+
+  /**
+   * Directory in which test logs and reports are generated.
+   *
+   * @var string
+   */
+  protected $reportsDir;
+
+  /**
+   * The filename for PHPUnit report.
+   *
+   * @var string
+   */
+  protected $reportFile;
+
+  /**
+   * An array that contains configuration to override customize phpunit command.
+   *
+   * @var array
+   */
+  protected $phpunitConfig;
+
+  /**
+   * This hook will fire for all commands in this command file.
+   *
+   * @hook init
+   */
+  public function initialize() {
+    $this->reportsDir = $this->getConfigValue('reports.localDir') . '/phpunit';
+    $this->reportFile = $this->reportsDir . '/results.xml';
+    $this->testsDir = $this->getConfigValue('repo.root') . '/tests/phpunit';
+    $this->phpunitConfig = $this->getConfigValue('paratest');
+  }
+
+  /**
+   * Executes all Paratest (PHPUnit) tests.
+   *
+   * @command custom:paratest:run
+   * @aliases cpr paratest custom:paratest
+   */
+  public function testsParatest() {
+    $this->createLogs();
+    foreach ($this->phpunitConfig as $test) {
+      $task = $this->taskParatest()
+        ->xml($this->reportFile)
+        ->printOutput(TRUE)
+        ->printMetadata(FALSE);
+
+      if (isset($test['class'])) {
+        $task->arg($test['class']);
+        if (isset($test['file'])) {
+          $task->arg($test['file']);
+        }
+      }
+      else {
+        if (isset($test['path'])) {
+          $task->arg($test['path']);
+        }
+      }
+
+      if (isset($test['path'])) {
+        $task->dir($test['path']);
+      }
+
+      $supported_options = [
+        'config' => 'configuration',
+        'exclude-group' => 'exclude-group',
+        'filter' => 'filter',
+        'group' => 'group',
+        'testsuite' => 'testsuite',
+        'procs' => 'processes',
+      ];
+
+      foreach ($supported_options as $yml_key => $option) {
+        if (isset($test[$yml_key])) {
+          $task->option("--$option", $test[$yml_key]);
+        }
+      }
+
+      $result = $task->run();
+      $exit_code = $result->getExitCode();
+
+      if ($exit_code) {
+        throw new BltException("PHPUnit tests failed.");
+      }
+    }
+  }
+
+  /**
+   * Creates empty log directory and log file for PHPUnit tests.
+   */
+  protected function createLogs() {
+    $this->taskFilesystemStack()
+      ->mkdir($this->reportsDir)
+      ->touch($this->reportFile)
+      ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
+      ->run();
+  }
+
+}
