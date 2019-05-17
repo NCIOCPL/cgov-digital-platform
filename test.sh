89b1501d18c79a2445f8db12e7aff834cd153f5c
Gao <gaoz@nci-02090702-ml.nih.gov>
2019-05-17 16:10:17 -0400

    Update data from blue (4/22) - Static data source. - Remove unneeded folders

---
+++ test.sh
@@ -0,0 +1,17 @@
+    drush mim promocard_migration; drush mim externalpromocard_migration
+    drush mim contentblock_migration; drush mim rawhtml_migration
+
+    drush mim primaryfeaturecardrow_migration && drush mim secondaryfeaturecardrow_migration && drush mim list_migration && drush mim multimediarow_migration
+
+    drush mim twocolumnrow_migration && drush mim guidecardrow_migration
+
+    drush mim twoitemfeaturecardrow_migration
+
+
+    drush mim blogseries_en_migration && drush mim blogseries_es_migration && drush mim blogtopics_migration && drush mim blogtopics_es_migration
+    drush mim blogpost_en_migration && drush mim blogpost_es_migration
+
+    drush mim minilanding_en_migration && drush mim minilanding_es_migration
+
+    drush mim cancerresearch_en_migration && drush mim cancerresearch_es_migration
+
