8a45ceb1534386f4c7b34e7419e3b62de940c688
Gao <gaoz@nci-02090702-ml.nci.nih.gov>
2019-05-21 16:46:01 -0400

    dynamiclis, spanish homepage, blogtopics

--- scripts/utility/cgov_migration_load.sh
+++ scripts/utility/cgov_migration_load.sh
@@ -8,7 +8,7 @@ set -ev
 case $MIGRATION in
 CGOV)
     drush mim summary_migration && drush mim summaryes_migration && drush mim dis_migration
-    drush mim externallinksql_migration; drush mim internallinksql_migration; drush mim citation_migration
+    drush mim externallinksql_migration; drush mim internallinksql_migration; drush mim citation_migration; drush mim medialink_migration
     drush mim paragraph_en_migration; drush mim paragraph_es_migration
     drush mim cgovimage_migration && drush mim cgovimage_es_migration
     drush mim pressrelease_en_migration && drush mim pressrelease_es_migration
@@ -22,10 +22,13 @@ CGOV)
     drush mim contentblock_migration; drush mim rawhtml_migration
 
     drush mim primaryfeaturecardrow_migration && drush mim secondaryfeaturecardrow_migration && drush mim list_migration && drush mim multimediarow_migration
+    drush mim dynamiclist_migration
 
     drush mim twocolumnrow_migration && drush mim guidecardrow_migration
 
     drush mim twoitemfeaturecardrow_migration
+
+
 
 
     drush mim blogseries_en_migration && drush mim blogseries_es_migration && drush mim blogtopics_migration && drush mim blogtopics_es_migration
