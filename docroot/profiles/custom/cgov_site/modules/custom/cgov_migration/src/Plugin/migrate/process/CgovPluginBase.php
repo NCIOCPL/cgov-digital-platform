9c97aa6fd78b54d534372af0bb31ccba5299e87c
Adrian Cofie <acofie@sapient.com>
2019-05-28 21:15:15 -0400

    Replace percussion reference divs with placeholders.

--- docroot/profiles/custom/cgov_site/modules/custom/cgov_migration/src/Plugin/migrate/process/CgovPluginBase.php
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_migration/src/Plugin/migrate/process/CgovPluginBase.php
@@ -3,7 +3,7 @@
 namespace Drupal\cgov_migration\Plugin\migrate\process;
 
 use Drupal\migrate\ProcessPluginBase;
-// Use PHPHtmlParser\Dom;.
+use Drupal\migrate\MigrateSkipRowException;
 use DOMWrap\Document;
 
 /**
@@ -21,6 +21,32 @@ abstract class CgovPluginBase extends ProcessPluginBase {
     $this->migLog = \Drupal::service('cgov_migration.migration_logger');
     $this->doc = new Document();
 
+  }
+
+  /**
+   * Return the incoming percussion ID from the Row.
+   */
+  public function getPercId($row) {
+    if ($row->hasSourceProperty('para_id')) {
+      $pid = $row->getSource()['para_id'];
+    }
+    elseif ($row->hasSourceProperty('id')) {
+      $pid = $row->getSource()['id'];
+    }
+    elseif ($row->hasSourceProperty('citation_id')) {
+      $pid = $row->getSource()['citation_id'];
+    }
+    elseif ($row->hasSourceProperty('row_rid')) {
+      $pid = $row->getSource()['row_rid'];
+    }
+    else {
+      $message = "Item skipped due to missing id or para_id.";
+      $this->migLog->logMessage(NULL, $message, E_ERROR);
+
+      throw new MigrateSkipRowException();
+    }
+
+    return $pid;
   }
 
 }
