9aa2799f1281f63d0b90675e5ca8a67021ecaa3b
Min <zhuomingao@usrs.no-reply.github.com>
2019-06-05 17:45:08 -0400

    microsites xml

--- scripts/utility/cgov_migration_load.sh
+++ scripts/utility/cgov_migration_load.sh
@@ -8,9 +8,10 @@ set -ev
 case $MIGRATION in
 CGOV)
     drush mim summary_migration && drush mim summaryes_migration && drush mim dis_migration
+    drush mim cgovimage_migration && drush mim cgovimage_es_migration
     drush mim externallinksql_migration; drush mim internallinksql_migration; drush mim citation_migration; drush mim medialink_migration
     drush mim paragraph_en_migration; drush mim paragraph_es_migration
-    drush mim cgovimage_migration && drush mim cgovimage_es_migration
+
     drush mim pressrelease_en_migration && drush mim pressrelease_es_migration
     drush mim article_en_migration && drush mim article_es_migration
     drush mim video_en_migration && drush mim video_es_migration
@@ -61,21 +62,35 @@ DCEG)
 
   ;;
 NANO)
-    drush mim externallinksql_migration; drush mim internallinksql_migration; drush mim citation_migration
-    drush mim paragraph_en_migration
     drush mim cgovimage_migration
+    drush mim internallinksql_migration; drush mim citation_migration; drush mim medialink_migration
+    drush mim paragraph_en_migration
+
     drush mim article_en_migration
 
+    drush mim video_en_migration
+    drush mim contentblock_migration
+    drush mim promocard_migration; drush mim externalpromocard_migration
+    drush mim list_migration
+    drush mim primaryfeaturecardrow_migration
+    drush mim multimediarow_migration
+    drush mim guidecardrow_migration
+    drush mim minilanding_en_migration
+    drush mim homelanding_en_migration
+
+    drush mim file_en_migration
+    drush mim contextualimage_migration
   ;;
 MYPART)
+    drush mim cgovimage_migration
     drush mim externallinksql_migration; drush mim internallinksql_migration
     drush mim paragraph_en_migration
-    drush mim cgovimage_migration
+
     drush mim article_en_migration
     drush mim infographic_en_migration
     drush mim video_en_migration
     drush mim contentblock_migration
-    drush mim promocard_migration
+    drush mim promocard_migration; drush mim externalpromocard_migration
     drush mim list_migration
     drush mim multimediarow_migration
     drush mim primaryfeaturecardrow_migration
@@ -83,13 +98,15 @@ MYPART)
     drush mim minilanding_en_migration
     drush mim homelanding_en_migration
 
-    drush mim contextualimage_en_migration
+    drush mim contextualimage_migration
 
     ;;
-NCICONNECTOR)
+NCICONNECT)
+
+    drush mim cgovimage_migration
     drush mim internallinksql_migration
     drush mim paragraph_en_migration
-    drush mim cgovimage_migration
+
     drush mim article_en_migration
 
     drush mim video_en_migration
@@ -103,7 +120,7 @@ NCICONNECTOR)
     drush mim homelanding_en_migration
 
     drush mim file_en_migration
-    drush mim contextualimage_en_migration
+    drush mim contextualimage_migration
   ;;
 *)
   echo 'MIGRATION NOT SET'
