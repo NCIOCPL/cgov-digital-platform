61e6a833a1c437c1e9245d369c4d8eebbb634663
Brendan Beltz <bbeltz@sapient.com>
2019-06-06 15:08:37 -0400

    (#1905) Enable pattern generator for NCI organization page

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/entrypoints/minilanding/Minilanding.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/entrypoints/minilanding/Minilanding.js
@@ -1,1 +1,14 @@
-import './Minilanding.scss';
+import patternInjector from 'Core/libraries/patternInjector';
+import patternSettings from 'Core/libraries/patternInjector/configs/nciOrgPatternSettings.js';
+import "./Minilanding.scss";
+
+const onDOMContentLoaded = () => {
+  // We only want to run the pattern injector on the home page.
+  const pathName = location.pathname.toLowerCase();
+  const isNCIOrganizationPage = /^\/about-nci\/organization\/?$/.test(pathName);
+  if(isNCIOrganizationPage){
+    patternInjector(patternSettings);
+  }
+}
+
+window.addEventListener("DOMContentLoaded", onDOMContentLoaded);
