07a86fd9297b4810993c2d17795fdeaf38e2218c
Brendan Beltz <bbeltz@sapient.com>
2019-06-06 15:04:08 -0400

    (#1906) Implement home page carousel and pattern injector

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/entrypoints/homelanding/Homelanding.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/entrypoints/homelanding/Homelanding.js
@@ -1,1 +1,16 @@
+import patternInjector from 'Core/libraries/patternInjector';
+import carouselPatternSettings from 'Core/libraries/patternInjector/configs/homePageCarouselPatternSettings.js';
+import carousel from 'Core/libraries/carousel/carousel';
 import "./Homelanding.scss";
+
+const onDOMContentLoaded = () => {
+  // We only want to run the pattern injector on the home page.
+  const pathName = location.pathname.toLowerCase();
+  const isHomePage = /^\/$/.test(pathName);
+  if(isHomePage){
+    carousel();
+    patternInjector(carouselPatternSettings);
+  }
+}
+
+window.addEventListener("DOMContentLoaded", onDOMContentLoaded);
---
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/src/libraries/patternInjector/configs/homePageCarouselPatternSettings.js
@@ -0,0 +1,137 @@
+const settings = {
+  '.blue-circles': {
+      str: '',
+      options: {
+          color: '#3fb2c7',
+          generator: 'overlappingRings',
+      }
+  },
+  '.blue-diamonds': {
+      str: '',
+      options: {
+          color: '',
+          generator: 'xes',
+      }
+  },
+  '.blue-dots': {
+      str: '',
+      options: {
+          color: '#319cd2',
+          generator: 'concentricCircles',
+      }
+  },
+  '.blue-triangles': {
+      str: '',
+      options: {
+          color: '#3e53c8',
+          generator: 'tessellation',
+      }
+  },
+  '.emerald-xes': {
+      str: '',
+      options: {
+          color: '#0f7811',
+          generator: 'xes',
+      }
+  },
+  '.fuchsia-waves': {
+      str: '',
+      options: {
+          color: '#9b38b6',
+          generator: 'sineWaves',
+      }
+  },
+  '.green-arrows': {
+      str: '',
+      options: {
+          color: '#34c381',
+          generator: 'chevrons',
+      }
+  },
+  '.green-plaid': {
+      str: '',
+      options: {
+          color: '#7cc02b',
+          generator: 'plaid',
+      }
+  },
+  '.navy-squares': {
+      str: '',
+      options: {
+          color: '#002f66',
+          generator: 'nestedSquares',
+      }
+  },
+  '.orange-plaid': {
+      str: '',
+      options: {
+          color: '#f5620f',
+          generator: 'plaid',
+      }
+  },
+  '.orange-waves': {
+      str: '',
+      options: {
+          color: '#f47a0a',
+          generator: 'sineWaves',
+      }
+  },
+  '.pink-triangles': {
+      str: '',
+      options: {
+          color: '#cf2eb0',
+          generator: 'mosaicSquares',
+      }
+  },
+  '.purple-circles': {
+      str: '',
+      options: {
+          color: '#814bd2',
+          generator: 'concentricCircles'
+      }
+  },
+  '.purple-triangles': {
+      str: '',
+      options: {
+          color: '#af4f97',
+          generator: 'mosaicSquares',
+      }
+  },
+  '.red-squares': {
+      str: '',
+      options: {
+          color: '#c83335',
+          generator: 'plusSigns',
+      }
+  },
+  '.red-triangles': {
+      str: '',
+      options: {
+          color: '#d7275f',
+          generator: 'mosaicSquares',
+      }
+  },
+  '.slate-grid': {
+      str: '',
+      options: {
+          color: '#41688d',
+          generator: 'squares',
+      }
+  },
+  '.yellow-circles': {
+      str: '',
+      options: {
+          color: '#eb9017',
+          generator: 'octogons', // Library writer misspelled this in the source code. Ugh.
+      }
+  },
+  '.yellow-hexagons': {
+      str: '',
+      options: {
+          color: '#e0c714',
+          generator: 'hexagons',
+      }
+  },
+}
+
+export default settings;
