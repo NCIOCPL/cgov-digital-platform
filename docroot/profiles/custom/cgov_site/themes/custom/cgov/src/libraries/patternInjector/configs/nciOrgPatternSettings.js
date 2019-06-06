8fb9ce7af1b1640f67eccda57b25a4727bb2e8d0
Brendan Beltz <bbeltz@sapient.com>
2019-06-06 15:08:37 -0400

    (#1905) Enable pattern generator for NCI organization page

---
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/src/libraries/patternInjector/configs/nciOrgPatternSettings.js
@@ -0,0 +1,53 @@
+const settings = {
+    ".nci-organization__pattern--ccr": {
+        str: '',
+        options: {
+            color: '#34c381',
+            generator: 'chevrons'
+        }
+    },
+    ".nci-organization__pattern--dceg": {
+        str: '',
+        options: {
+            color: '#3e53c8',
+            generator: 'tessellation'
+        }
+    },
+    ".nci-organization__pattern--dcb": {
+        str: '',
+        options: {
+            color: '#3fb2c7',
+            generator: 'overlappingRings'
+        }
+    },
+    ".nci-organization__pattern--cancercontrol": {
+        str: '',
+        options: {
+            color: '#d7275f',
+            generator: 'mosaicSquares'
+        }
+    },
+    ".nci-organization__pattern--prevention": {
+        str: '',
+        options: {
+            color: '#7cc02b',
+            generator: 'plaid'
+        }
+    },
+    ".nci-organization__pattern--dctd": {
+        str: '',
+        options: {
+            color: '#f47a0a',
+            generator: 'sineWaves'
+        }
+    },
+    ".nci-organization__pattern--deainfo": {
+        str: '',
+        options: {
+            color: '#814bd2',
+            generator: 'concentricCircles'
+        }
+    }
+}
+
+export default settings;
