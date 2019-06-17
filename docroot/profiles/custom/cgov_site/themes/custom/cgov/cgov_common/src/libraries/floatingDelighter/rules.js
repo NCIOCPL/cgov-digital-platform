a525e06ff1fc3c9262f598e1a3261e3a921ab0be
Brendan Beltz <bbeltz@sapient.com>
2019-06-17 11:48:28 -0400

    (#2018) Update NCI Connect delighter path rules

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/libraries/floatingDelighter/rules.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/libraries/floatingDelighter/rules.js
@@ -7,7 +7,7 @@ import {
 /**
  * A rule is an object required to have a rule and delighter property. The third property exclude is optional. Exclude is an array of regular expressions and/or
  * objects containing a rule (regex) and whitelist (array) property.
- * 
+ *
  * NOTE: The function that tests rules is lazy and will only return the first match, so make sure a previous rule doesn't conflict with yours if it's not working.
  */
 const rules = [
@@ -34,12 +34,12 @@ const rules = [
         delighter: socialMedia,
     },
     {
-        rule: /^\/nci\/rare-brain-spine-tumor/i,
+        rule: /^\/rare-brain-spine-tumor/i,
         delighter: nciConnect,
         exclude: [
             /\/refer-participate/i,
             /\/blog/i,
-            /^\/nci\/rare-brain-spine-tumor\/about/i,
+            /\/about/i,
             /\/living\/finished-treatment/i,
             /\/living\/coping/i,
             /\/living\/stories/i
