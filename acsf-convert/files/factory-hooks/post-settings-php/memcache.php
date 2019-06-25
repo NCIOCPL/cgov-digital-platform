f41445886cbb965e59ab500f82c76a9a55d5bde0
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2019-06-25 14:13:53 -0400

    Removing memcache_enabled check.
    Removed at the suggestion of our TAM, "The flag is going to be removed in BLT soon. After the updates are finished to allow us to use the new version of memcache we're going to pull the flags out. Right now they rely on a setting inside the ACSF dashboard which is only available to Acquians."

--- acsf-convert/files/factory-hooks/post-settings-php/memcache.php
+++ acsf-convert/files/factory-hooks/post-settings-php/memcache.php
@@ -9,7 +9,6 @@
 
 // Use ACSF internal settings site flag to apply memcache settings.
 if (getenv('AH_SITE_ENVIRONMENT') &&
-    isset($site_settings['flags']['memcache_enabled']) &&
     isset($settings['memcache']['servers'])
 ) {
   require DRUPAL_ROOT . '/../vendor/acquia/blt/settings/memcache.settings.php';
