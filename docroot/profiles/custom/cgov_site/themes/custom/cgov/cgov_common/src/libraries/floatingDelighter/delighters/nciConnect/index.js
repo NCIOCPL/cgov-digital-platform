2ee36155710914558831b4af2ed7389ca0c0d427
Brendan Beltz <bbeltz@sapient.com>
2019-06-28 09:37:13 -0400

    (#2119) Updated NCI Connect Delighter Path

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/libraries/floatingDelighter/delighters/nciConnect/index.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/libraries/floatingDelighter/delighters/nciConnect/index.js
@@ -1,7 +1,7 @@
 import './index.scss';
 
 export const nciConnect = {
-    href: '/nci/rare-brain-spine-tumor/refer-participate/clinical-studies',
+    href: '/rare-brain-spine-tumor/refer-participate/clinical-studies',
     innerHTML: `
         <div class="floating-delighter__label">Join an NCI-CONNECT Study</div>
     `,
