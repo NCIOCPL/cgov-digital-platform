450b66509ca7513ae7745597d0099bf5d238550d
bryanpizzillo <bryanpizzillo@users.noreply.github.com>
2019-08-19 09:07:26 -0400

    (#2339) Created App Module Reference Field - Includes initial formatter, widget and type   - and schema! - Includes tests for initial version - Added field to cgov_application_page

---
+++ docroot/modules/custom/app_module/src/Plugin/Field/FieldType/AppModuleReferenceItem.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Drupal\app_module\Plugin\Field\FieldType;
+
+use Drupal\app_module\AppModule;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem;
+use Drupal\Core\TypedData\DataDefinition;
+
+/**
+ * Defines the 'appmodulereference' entity field type.
+ *
+ * The target type for appmodulereference fields should always be 'app_module'.
+ *
+ * @FieldType(
+ *   id = "app_module_reference",
+ *   label = @Translation("Application module reference"),
+ *   description = @Translation("A field reference to an application module."),
+ *   category = @Translation("Reference"),
+ *   default_widget = "app_module_reference_select",
+ *   default_formatter = "app_module_reference_formatter",
+ *   list_class = "\Drupal\Core\Field\EntityReferenceFieldItemList",
+ *   cardinality = 1,
+ * )
+ */
+class AppModuleReferenceItem extends EntityReferenceItem {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultStorageSettings() {
+    return [
+      'target_type' => 'app_module',
+    ] + parent::defaultStorageSettings();
+  }
+
+  /* ABOUT FIELD SETTINGS:
+   * All app modules should be up for grabs, we always allow instance
+   * settings. So, we have no field settings right now. Actually we only
+   * have one field for now for app modules. We could however have other
+   * instances like in a paragraph, or etc. Like some sort of widget. In
+   * this case we might not want all the page loading support that a full
+   * page widget would need. That could be an example setting.
+   *
+   * So we do not need to implement fieldSettingsForm() nor
+   * defaultFieldSettings().
+   */
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
+    $properties = parent::propertyDefinitions($field_definition);
+    $properties['data'] = DataDefinition::create('string')
+      ->setLabel(new TranslatableMarkup('Data'))
+      ->setDescription(new TranslatableMarkup('Settings data for this instance'));
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function schema(FieldStorageDefinitionInterface $field_definition) {
+    $schema = parent::schema($field_definition);
+    $schema['columns']['data'] = [
+      'description' => 'Serialized data.',
+      'type' => 'text',
+      'size' => 'big',
+    ];
+    return $schema;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setValue($values, $notify = TRUE) {
+    // Select widget has extra layer of items.
+    if (isset($values['target_id']) && is_array($values['target_id'])) {
+      $values['target_id'] = isset($values['target_id'][0]['target_id']) ? $values['target_id'][0]['target_id'] : NULL;
+    }
+    parent::setValue($values, FALSE);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getPreconfiguredOptions() {
+    return [];
+  }
+
+  /**
+   * Get all enabled app module names.
+   *
+   * @return array
+   *   An array of enabled app modules names keyed by app module ID.
+   */
+  protected function getAllAppModuleNames() {
+    $appmodules = AppModule::getEnabledAppModules();
+    $options = [];
+    foreach ($appmodules as $appmodule) {
+      $options[$appmodule->id] = $appmodule->label;
+    }
+    return $options;
+  }
+
+}
---
+++ docroot/modules/custom/app_module/src/Plugin/Field/FieldWidget/AppModuleReferenceSelectWidget.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\app_module\Plugin\Field\FieldWidget;
+
+use Drupal\app_module\AppModules;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\Plugin\Field\FieldWidget\OptionsSelectWidget;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'options_select' widget.
+ *
+ * @FieldWidget(
+ *   id = "app_module_reference_select",
+ *   label = @Translation("App module reference select list"),
+ *   description = @Translation("An autocomplete app module select list field."),
+ *   field_types = {
+ *     "app_module_reference"
+ *   }
+ * )
+ */
+class AppModuleReferenceSelectWidget extends OptionsSelectWidget {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $select_element['target_id'] = parent::formElement($items, $delta, $element, $form, $form_state);
+
+    // This sets up the form element.
+    $select_element = $this->fieldElement($items, $delta, $select_element, $form, $form_state);
+    $select_element['target_id']['#multiple'] = FALSE;
+    if (!$this->isDefaultValueWidget($form_state)) {
+      $available_apps = $this->getAppModuleNames();
+      if (count($available_apps) >= 1) {
+        $first_option = ['_none' => '- None -'];
+        $select_element['target_id']['#options'] = array_merge($first_option, $available_apps);
+      }
+    }
+    return $select_element;
+  }
+
+  /**
+   * Get app module names for a list of app machine names.
+   *
+   * @return array
+   *   An array with app module labels keyed by machine name.
+   */
+  protected function getAppModuleNames() {
+    $app_modules = AppModules::getEnabledAppModules();
+    $options = [];
+    foreach ($app_modules as $app_module) {
+      $options[$app_module->id] = $app_module->label;
+    }
+    return $options;
+  }
+
+  /**
+   * Build a field element for a app_module_reference field.
+   *
+   * @param \Drupal\Core\Field\FieldItemListInterface $items
+   *   Array of default values for this field.
+   * @param int $delta
+   *   The order of this item in the array of sub-elements (0, 1, 2, etc.).
+   * @param array $element
+   *   The field item element.
+   * @param array $form
+   *   The overall form array.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   Array of default values for this field.
+   *
+   * @return array
+   *   The changed field element.
+   */
+  public function fieldElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    // On selection of view, an Ajax call must be made to get the settings
+    // for this app module. This will then either replace the select element,
+    // or replace it.
+    return $element;
+  }
+
+}
