7dcc9d7a16bb88b25bb744215a5f60fe710faa47
Sarina <sarinapadilla@users.noreply.github.com>
2019-09-17 13:23:26 -0400

    (#2383, #2384) Schema.org structured data pilot
    * Create cgov_schema_org module to create schema org data field
    * Create field widget for schema org data field (like syndication)
    * Add field storage for schema org data field
    * Add database schema for schema org data field
    * Add preprocessor to attach schema org markup to html head
    * Add cgov_schema_org dependencies to site and article
    * Install cgov_schema_org module on cgov_article install
    * Add field definition for schema org data on article
    * Update form and view displays to include schema org data
    * Add field and paragraph preprocessors to access schema org data field
    * Update article body template to include schema org markup
    * Update body section template to include schema org markup
    * Closes #2383
    * Closes #2384

---
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_schema_org/src/Plugin/Field/CgovSchemaOrgEntityFieldItemList.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drupal\cgov_schema_org\Plugin\Field;
+
+use Drupal\Core\Field\FieldItemList;
+
+/**
+ * Defines a cgov_schema_org list class for better normalization targeting.
+ */
+class CgovSchemaOrgEntityFieldItemList extends FieldItemList {
+}
