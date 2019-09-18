76c93f20a63621b3ecfaa5ab030cdb12284eb5c0
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2019-09-17 21:18:23 -0400

    fixing side-by-side

--- src/components/search-modules/ZipCode/ZipCode.jsx
+++ src/components/search-modules/ZipCode/ZipCode.jsx
@@ -7,7 +7,7 @@ const ZipCode = () => {
       id="zip"
       legend="U.S. Zip Code"
       helpUrl="https://www.cancer.gov/about-cancer/treatment/clinical-trials/search/help#basicsearch">
-      <TextInput id="z" label="" maxLength={5} />
+      <TextInput id="z" label="" inputHelpText="Show trials near this U.S. ZIP code." maxLength={5} />
     </Fieldset>
   );
 };
