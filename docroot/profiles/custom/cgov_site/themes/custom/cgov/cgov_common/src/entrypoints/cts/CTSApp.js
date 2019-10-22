d14ce67b0273ec26972edffcaf9fced4d7b8fdac
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2019-10-22 17:33:12 -0400

    - Added entrypoint for CTS app - Added CTS app module template

---
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/entrypoints/cts/CTSApp.js
@@ -0,0 +1,9 @@
+// Import CTS
+import './CTSApp.scss';
+
+const onDOMContentLoaded = () => {
+  //do something here
+  console.log("CTS!!!")
+}
+
+window.addEventListener("DOMContentLoaded", onDOMContentLoaded);
