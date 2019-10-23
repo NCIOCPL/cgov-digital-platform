c5f4bc19a2856edc6091583b575d43341a596a60
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2019-10-23 15:55:38 -0400

    (#2502) Create Clinical Trials Search module - Created module - Created App Module & Plugin shell - Created theme template - Updated YML - Created JS entry point and library entry - Added entrypoint for CTS app - Added CTS app module template

---
+++ docroot/modules/custom/cgov_cts/tests/src/Unit/ClinicalTrialsSearchAppModulePluginTests.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\Tests\cgov_cts\Unit;
+
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Tests for the PathProcessorAppModule.
+ */
+class ClinicalTrialsSearchAppModulePluginTests extends UnitTestCase {
+
+  /**
+   * Tests CTSPlugin functions.
+   */
+  public function testCtsPlugin() {
+    $this->assertTrue(TRUE);
+  }
+
+}
