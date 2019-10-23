66e29b67d9cd370ea0ad8b9c617aa5373877e976
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2019-10-22 23:03:50 -0400

    CTS Mess
    Co-authored-by: Paul Arce <parce@sapient.com>

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/entrypoints/cts/CTSApp.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/entrypoints/cts/CTSApp.js
@@ -1,8 +1,11 @@
-// Import CTS
-import './CTSApp.scss';
+import initializeCTSApp from 'Libraries/cts/cts';
+
+import '@nciocpl/clinical-trials-search-app/build/static/main.f6e704c2.chunk.scss'; // gotta change the output to not use the hash
 
 const onDOMContentLoaded = () => {
   //do something here
+  //ctsReactApp();
+  initializeCTSApp();
   console.log("CTS!!!")
 }
 
---
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/libraries/cts/cts.js
@@ -0,0 +1,9 @@
+import ctsReactApp from '@nciocpl/clinical-trials-search-app';
+
+const config = {};
+
+const initializeCTSApp = () => {
+    ctsReactApp(config);
+}
+
+export default initializeCTSApp;
