c3b14f3f512c8d76c05180f846b29eca880d50ee
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2019-11-24 20:40:47 -0500

    (#2562) Metadata support for App Modules     - Added in scaffolding for modules to participate in the       hook_page_attachments_alter hook and hook_tokens_alter     - Added a method to identify what tokens an app module       would use. Implemented hook_tokens_alter to filter       out calls to app module based on available tokens.     - Added additional app_module parameters to the request       query params in order to support these hook events,       as hooks run outside of our FieldFormatter, which is       what actually renders the app module.     - Updated tests for the path processor to validate our       additional request parameters.     - NOTE: There is an issue with testing this scaffolding       as our .module hooks are not being registered and       thus are not firing during a Functional test.
        Co-authored-by: sarinapadilla <sarinapadilla@user.noreply.github.com>

--- docroot/modules/custom/app_module/src/Plugin/app_module/MultiRouteAppModulePluginBase.php
+++ docroot/modules/custom/app_module/src/Plugin/app_module/MultiRouteAppModulePluginBase.php
@@ -76,4 +76,52 @@ abstract class MultiRouteAppModulePluginBase extends AppModulePluginBase {
     }
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  final public function alterPageAttachments(array &$attachments, $path, array $options = []) {
+    parent::alterPageAttachments($attachments, $path, $options);
+
+    /* @var \Drupal\app_module\Plugin\app_module\MultiRouteAppModuleBuilderBase */
+    $builder = $this->getBuilderForRoute($path);
+
+    if ($builder) {
+      $builder->alterPageAttachments($attachments, $options);
+    }
+
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * The default implementation is a NOOP.
+   */
+  final public function alterTokens(array &$replacements, array $context, $path, array $options = []) {
+    parent::alterTokens($replacements, $context, $path, $options);
+
+    /* @var \Drupal\app_module\Plugin\app_module\MultiRouteAppModuleBuilderBase */
+    $builder = $this->getBuilderForRoute($path);
+
+    if ($builder) {
+      $builder->alterTokens($replacements, $context, $options);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * The default implementation is a NOOP.
+   */
+  public function getTokensForAltering($path, array $options = []) {
+    $builder = $this->getBuilderForRoute($path);
+
+    if ($builder) {
+      $tokensToAlter = $builder->getTokensForAltering($options);
+      return $tokensToAlter;
+    }
+    else {
+      return [];
+    }
+  }
+
 }
