6cffe223448ba1c7c8fda760dae2956eaed6b950
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2019-11-27 18:02:21 -0500

    cts pkg update to 0.9.4

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/libraries/cts/cts.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/libraries/cts/cts.js
@@ -8,6 +8,28 @@ const ctsSearch = () => {
   return service;
 };
 
+// React Helmet is removing all elements with data-react-helmet="true" on the page except the ones it directly controls. This is a big
+// issue. This code adds it only to the elements we want to control manually to avoid that issue.
+const elementSelectors = [
+  ["name", "description"],
+  ["property", "og:title"],
+  ["property", "og:description"],
+  ["property", "og:url"]
+];
+
+const elementsUsedByCTS = elementSelectors.map(el => {
+  return document.querySelector(`meta[${el[0]}="${el[1]}"]`);
+});
+// title tag is updated by CTS app so want to prevent duplication here, too
+elementsUsedByCTS.push(document.querySelector('title'));
+
+elementsUsedByCTS.forEach(el => {
+  // Some elements are originated by react-helmet after this script runs and will be null
+  if (el) {
+    el.setAttribute("data-react-helmet", "true");
+  }
+});
+
 // this will probably be changed to pull from window config
 // ex. const config = window.nciCTSConfig;
 const config = {
