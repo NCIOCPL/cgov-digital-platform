6b93c9721e743e47a1211da3697bf61b6dfbc000
Sarina <sarinapadilla@users.noreply.github.com>
2019-12-03 10:36:30 -0500

    (#2580) Update CTS Advanced Search metatags
    * Update metatag overwrites for CTS Advanced Search
    * Closes #2580

--- docroot/modules/custom/cgov_cts/src/MultiRouteBuilders/CTSAdvancedSearchBuilder.php
+++ docroot/modules/custom/cgov_cts/src/MultiRouteBuilders/CTSAdvancedSearchBuilder.php
@@ -22,6 +22,18 @@ class CTSAdvancedSearchBuilder extends CTSBuilderBase {
   public function alterTokens(array &$replacements, array $context, array $options = []) {
     foreach ($replacements as $key => $value) {
       switch ($key) {
+        case '[cgov_tokens:cgov-title]':
+          $replacements[$key] = 'Find NCI-Supported Clinical Trials - Advanced Search';
+          break;
+
+        case '[node:field_page_description:value]':
+          $replacements[$key] = 'Use our advanced search to find an NCI-supported clinical trial—and learn how to locate other research studies—that may be right for you or a loved one.';
+          break;
+
+        case '[node:field_browser_title:value]':
+          $replacements[$key] = 'Find NCI-Supported Clinical Trials - Advanced Search';
+          break;
+
         case '[node:url]':
           $replacements[$key] = $value . '/advanced';
           break;
@@ -43,6 +55,9 @@ class CTSAdvancedSearchBuilder extends CTSBuilderBase {
    */
   public function getTokensForAltering(array $options = []) {
     $tokensToAlter = [
+      '[cgov_tokens:cgov-title]',
+      '[node:field_page_description:value]',
+      '[node:field_browser_title:value]',
       '[node:url]',
       '[current-page:url]',
     ];
