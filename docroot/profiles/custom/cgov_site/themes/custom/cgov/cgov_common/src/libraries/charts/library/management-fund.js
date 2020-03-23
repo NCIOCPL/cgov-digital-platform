671d8804e217b7b60e7f59f60900875e88cfcb7a
Erik Meyer <erik.meyer@publicissapient.com>
2020-03-23 16:55:10 -0400

    (#2666) NIH Managment Fund: Highcharts Pie updated data

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/libraries/charts/library/management-fund.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/libraries/charts/library/management-fund.js
@@ -8,12 +8,12 @@ function initChart(Chart) {
           type: 'NCI_pie'
       },
       title: {text: 'NIH Management Fund, Service & Supply Fund, and GSA Rent'},
-      subtitle: {text: 'Fiscal Year 2018'},
+      subtitle: {text: 'Fiscal Year 2019'},
       series: [{
           name: 'Amount',
           data: [
-              {name: 'NCI', y: 369327642, drilldown: 'NCI'},
-              {name: 'All Others',y: 1512273171}
+              {name: 'NCI', y: 394852398, drilldown: 'NCI'},
+              {name: 'All Others',y: 1589193079}
           ]
       }],
       drilldown: {
@@ -21,11 +21,11 @@ function initChart(Chart) {
               {name: 'NCI',
                 id: 'NCI',
                 data: [
-                  ['Clinical Center', 137441910],
-                  ['Center for Scientific Review', 26186932],
-                  ['Center for Information Technology', 7509131],
-                  ['Service and Supply Fund Assessment', 184485965],
-                  ['Other Research Services', 13703703],
+                  ['Clinical Center', 146225820],
+                  ['Center for Scientific Review', 26444914],
+                  ['Center for Information Technology', 30690142],
+                  ['Service and Supply Fund Assessment', 178058636],
+                  ['Other Research Services', 13432885],
                   ['Other OD', 0]
               ]}
           ]
