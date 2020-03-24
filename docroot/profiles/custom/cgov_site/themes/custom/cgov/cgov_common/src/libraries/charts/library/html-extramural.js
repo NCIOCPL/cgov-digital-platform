19ed6a66ba5bfd76328169af15954586be9f0b2d
Erik Meyer <erik.meyer@publicissapient.com>
2020-03-24 11:24:24 -0400

    (#2664 Extramural Funding Pie updated data

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/libraries/charts/library/html-extramural.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/libraries/charts/library/html-extramural.js
@@ -6,16 +6,16 @@ function initChart(Chart) {
     new Chart(id, {
       chart: { type: 'NCI_pie' },
       title: { text: 'Extramural Funds' },
-      subtitle: { text: 'Fiscal Year 2018' },
+      subtitle: { text: 'Fiscal Year 2019' },
       series: [{
         name: 'Budget',
         data: [{
           name: 'Contracts',
-          y: 843406010,
+          y: 786095181,
           drilldown: 'Contracts'
         }, {
           name: 'Grants',
-          y: 3696412163,
+          y: 3791406979,
           drilldown: 'Grants'
         }]
       }],
@@ -24,12 +24,12 @@ function initChart(Chart) {
           name: 'Contracts',
           id: 'Contracts',
           colors: ['#2DC799', '#ABE9D6', '#229573', '#17644D', '#D5F4EB', '#0B3226'],
-          data: [['Research & Development (R&D) Contracts', 825406010], ['Buildings and Facilities', 18000000], ['Construction Contracts', 0]]
+          data: [['Research & Development (R&D) Contracts', 768095181], ['Buildings and Facilities', 18000000], ['Construction Contracts', 0]]
         }, {
           name: 'Grants',
           id: 'Grants',
           colors: ['#80378B', '#602968', '#B387B9', '#532C68', '#D0B9D7', '#5A4F79', '#DCD5E1', '#995FA2'],
-          data: [['Research Project Grants (RPGs)', 2450557744], ['Centers/Specialized Centers/SPORES', 625575487], ['NRSA', 82413198], ['Other Research Grants', 537865734]]
+          data: [['Research Project Grants (RPGs)', 2541699571], ['Centers/Specialized Centers/SPORES', 655966379], ['NRSA', 86977607], ['Other Research Grants', 506763422]]
         }]
       }
     });
