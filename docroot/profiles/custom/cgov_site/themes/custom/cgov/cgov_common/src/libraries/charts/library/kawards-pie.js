5571dd68444ab78b3aa54cf1363c6a5d15ac2578
Erik Meyer <erik.meyer@publicissapient.com>
2020-04-07 16:43:34 -0400

    (#2671) K Awards: Highcharts Pie updated data closes #2671

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/libraries/charts/library/kawards-pie.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/libraries/charts/library/kawards-pie.js
@@ -14,7 +14,7 @@ function initChart(Chart) {
     colors: ['#1B5768', '#70858C', '#E2F0F4', '#83C5D8', '#319FBE', '#123A46', '#5090A2', '#00181D', '#5AB2CB', '#24748B'],
     chart: { type: 'NCI_pie' },
     title: { text: 'Percent of Total Research Career Awards Funded' },
-    subtitle: { text: 'Fiscal Year 2018' },
+    subtitle: { text: 'Fiscal Year 2019' },
     legend: {
       layout: 'horizontal',
       align: 'center',
@@ -53,18 +53,18 @@ function initChart(Chart) {
     series: [{
       name: 'Funding',
       data: [
-        ['K00 Post-Doc-Fellow Awards', 2294],
-        ['K01 Research Scientist Development Award', 5826],
-        ['K05 Research Scientist Award', 444],
-        ['K07 Preventive Oncology', 11272],
-        ['K08 Clinical Investigator', 20858],
-        ['K12 Institutional Clinical Oncology Research', 14228],
-        ['K22 Transitional Career Development', 10304],
-        ['K23 Patient-Oriented Career', 2166],
-        ['K24 Patient-Oriented Career - Mid Career', 2380],
-        ['K25 Mentored Quantitative Research Career Development Award', 847],
-        ['K43 Mentored Career Devel/Temin Intl Career', 154],
-        ['K99 NIH Pathway to Independence Awards', 7564]
+        ['K00 Post-Doc-Fellow Awards', 4480],
+        ['K01 Research Scientist Development Award', 5346],
+        ['K05 Research Scientist Award', 172],
+        ['K07 Preventive Oncology', 9916],
+        ['K08 Clinical Investigator', 30547],
+        ['K12 Institutional Clinical Oncology Research', 15653],
+        ['K22 Transitional Career Development', 9354],
+        ['K23 Patient-Oriented Career', 1689],
+        ['K24 Patient-Oriented Career - Mid Career', 1513],
+        ['K25 Mentored Quantitative Research Career Development Award', 570],
+        ['K43 Mentored Career Devel/Temin Intl Career', 233],
+        ['K99 NIH Pathway to Independence Awards', 5518]
       ]
     }]
   });
