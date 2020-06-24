dd273239903291e095ea45ac04e50af5915e6b48
Sarina <sarinapadilla@users.noreply.github.com>
2020-06-24 16:55:12 -0400

    (#2796) Glossary app config updates
    * Update configs for glossary app in YAML content
    * Update setting of config values in glossary js
    * Update dom manipulation with helper functions for config variables
    * Update package reference for glossary-app to release package
    * Closes #2796

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/libraries/glossary/glossary.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/libraries/glossary/glossary.js
@@ -4,11 +4,47 @@
  */
 
 import initialize from '@nciocpl/glossary-app';
+import { getCanonicalURL, getDocumentLanguage, getMetaData, parseUrl } from 'Utilities';
 
-const config = window.glossaryAppSettings;
+const instanceConfig = window.glossaryAppSettings;
 
-const initializeGlossaryApp = () => {
-  initialize(config);
-};
+const metaTags = getMetaData([
+  ['name', 'dcterms.subject'],
+  ['name', 'dcterms.issued'],
+  ['property', 'og:site_name']
+], document);
+
+
+const language = getDocumentLanguage(document);
+
+const canonicalHref = parseUrl(getCanonicalURL(document), document);
+if (!canonicalHref) {
+  throw new Error("GlossaryApp cannot be loaded - canonical URL not found");
+}
+
+const canonicalHost = canonicalHref.protocol + "//" + canonicalHref.host;
+
+const alternateLangLink = document.querySelector('link[rel="alternate"]:not([hreflang="' + language + '"])');
+const alternateLangHref = alternateLangLink ? parseUrl(alternateLangLink.href, document) : null;
+const altLanguageDictionaryBasePath = alternateLangHref ? alternateLangHref.pathname : undefined;
+
+const drupalConfig = {
+  baseHost: window.location.protocol + "//" + window.location.hostname,
+  basePath: canonicalHref ? canonicalHref.pathname :
+  canonicalHost,
+  altLanguageDictionaryBasePath,
+  analyticsChannel: metaTags['dcterms.subject'],
+  analyticsPublishedDate: metaTags['dcterms.issued'],
+  rootId: "NCI-glossary-root",
+  dictionaryEndpoint: window.CDEConfig.glossaryConfig.apiServer,
+  languageToggleSelector: "#LangList1 a",
+  language,
+  siteName: metaTags['og:site_name'],
+}
+
+const config = {
+  ...instanceConfig,
+  ...drupalConfig
+}
 
 export default initializeGlossaryApp;
