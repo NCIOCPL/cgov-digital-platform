b24902d74e4701a48565c51c01879fe85362fb74
Adrian Cofie <acofie@sapient.com>
2020-07-02 07:34:54 -0400

    (#2779)  Content Block Link For AP
         * Adds example PDF file to YAML Content
         * Add analytics
         * Hide on mobile
         * Adds blue pdf svg-sprite

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/src/libraries/analytics/nci-analytics-functions.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/src/libraries/analytics/nci-analytics-functions.js
@@ -1711,7 +1711,18 @@ var NCIAnalytics = {
             67: pageName
         };
         clickParams.LogToOmniture();
-    }
+    },
+  /* ********************************************************************** */
+    SpecialReportFileClick: function (sender, pageName) {
+      var clickParams = new NCIAnalytics.ClickParams(sender,
+        'nciglobal', 'o', ' v');
+      clickParams.Events = [52];
+      clickParams.Props = {
+        66:  "filedownload_icon",
+        67: pageName
+      };
+      clickParams.LogToOmniture();
+    },
 };
 
 /* End the giant NCIAnalytics object functions */
