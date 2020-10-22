cc0554452e8c82e08cfba0bd1ae6cd8cce70c651
Erik Meyer <emeyer@sapient.com>
2020-10-22 09:52:12 -0400

    (#2669 Number of RPG Awards: Highcharts Bar) Updated data

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/libraries/charts/library/rpg-funding.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/libraries/charts/library/rpg-funding.js
@@ -14,19 +14,18 @@ function initChart(Chart) {
       }
     },
     colors: ['#80378B', '#602968', '#B387B9', '#532C68', '#D0B9D7', '#5A4F79', '#DCD5E1', '#995FA2'],
-    title: { text: 'Percent Share of Total RPG Funds, FY 18' },
-    subtitle: { text: '(Dollars in Thousands)'},
+    title: { text: 'Percent Share of Total RPG Funds, FY 19' },
     series: [{
       name: 'Funding',
       data: [
-        ['R01 Traditional Grants', 1375890],
-        ['P01 Program Projects', 165529],
-        ['R03 Small Grants', 12227],
-        ['R21 Exploratory Phase I', 74013],
-        ['R33 Exploratory Phase II', 17863],
-        ['R35', 118267],
-        ['SBIR/STTR', 142899],
-        ['Other', 459841]
+        ['R01 Traditional Grants', 58.0],
+        ['P01 Program Projects', 7.3],
+        ['R03 Small Grants', 0.4],
+        ['R21 Exploratory Phase I', 3.5],
+        ['R33 Exploratory Phase II', 0.6],
+        ['R35', 5.3],
+        ['SBIR/STTR', 5.6],
+        ['Other', 19.4]
       ]
     }],
     responsive: {
