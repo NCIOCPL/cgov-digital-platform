92d33130df3322a96156efd3ff3a01955480faad
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2020-11-19 13:52:18 -0500

    (2970) Make it possible to preserve environment on code update.

--- hooks/common/post-code-update/post-code-update.sh
+++ hooks/common/post-code-update/post-code-update.sh
@@ -40,51 +40,64 @@ if [[ $target_env =~ ^ode\d* ]]; then
   target_env="ode";
 fi
 
-## Clear last install out of Memcache.
-## Drush CR will not work on a non-existant site. Drush CC will not find the memcached
-## backed bins if the site is not installed. So we made our own to work with empty sites
-## to clean out memcache. (DB backing stores disappear as soon as the DB is dropped.)
-drush cgov:destroy-cache
+# This variable must be the word true for it to update instead of reinstall
+if [[ $PRESERVE_ON_REDEPLOY == "true" ]]; then
+  ## Just do a drupal update and update translations
+  blt deploy:update --environment=$target_env -v --no-interaction -D drush.ansi=false
 
-## Perform a fresh install.
-blt artifact:install:drupal --environment=$target_env -v --no-interaction -D drush.ansi=false
+  ## Reload translation pack.
+  blt cgov:locales:translate -D drush.ansi=false
 
-## Load our test users.
-blt cgov:user:load-all -D cgov.drupal_users_file=$users_file -D drush.ansi=false
+  ## Setup some default JS globals.
+  cat FrontendGlobals.json | drush config:set cgov_core.frontend_globals config_object -
+else
+  ## Clear last install out of Memcache.
+  ## Drush CR will not work on a non-existant site. Drush CC will not find the memcached
+  ## backed bins if the site is not installed. So we made our own to work with empty sites
+  ## to clean out memcache. (DB backing stores disappear as soon as the DB is dropped.)
+  drush cgov:destroy-cache
 
-## Reload translation pack.
-blt cgov:locales:translate -D drush.ansi=false
+  ## Perform a fresh install.
+  blt artifact:install:drupal --environment=$target_env -v --no-interaction -D drush.ansi=false
 
-## Setup some default JS globals.
-cat FrontendGlobals.json | drush config:set cgov_core.frontend_globals config_object -
+  ## Load our test users.
+  blt cgov:user:load-all -D cgov.drupal_users_file=$users_file -D drush.ansi=false
 
-## Execute a migration.
-case $MIGRATION in
-CGOV)
-  blt cgov:install:site-sections --no-interaction -D drush.ansi=false  # This (of course) loads the site sections and megamenus.
-  ./scripts/utility/cgov_migration_load.sh
-  ;;
-DCEG)
-  blt cgov:install:site-sections --no-interaction -D drush.ansi=false   # This (of course) loads the site sections and megamenus.
-  ./scripts/utility/cgov_migration_load.sh
-  ;;
-NANO)
-  blt cgov:install:site-sections --no-interaction -D drush.ansi=false   # This (of course) loads the site sections and megamenus.
-  ./scripts/utility/cgov_migration_load.sh
-  ;;
-MYPART)
-  blt cgov:install:site-sections --no-interaction -D drush.ansi=false   # This (of course) loads the site sections and megamenus.
-  ./scripts/utility/cgov_migration_load.sh
-  ;;
-NCICONNECT)
-  blt cgov:install:site-sections --no-interaction   # This (of course) loads the site sections and megamenus.
-  ./scripts/utility/cgov_migration_load.sh
-  ;;
-*)
-  blt custom:install_cgov_yaml_content_by_module cgov_yaml_content -D drush.ansi=false
-  ## Uninstall cgov_yaml_content once done to turn off entity presave hack for supporting
-  ## drupal-entity.
-  drush pmu cgov_yaml_content
-  ;;
-esac
+  ## Reload translation pack.
+  blt cgov:locales:translate -D drush.ansi=false
+
+  ## Setup some default JS globals.
+  cat FrontendGlobals.json | drush config:set cgov_core.frontend_globals config_object -
+
+  ## Execute a migration.
+  case $MIGRATION in
+  CGOV)
+    blt cgov:install:site-sections --no-interaction -D drush.ansi=false  # This (of course) loads the site sections and megamenus.
+    ./scripts/utility/cgov_migration_load.sh
+    ;;
+  DCEG)
+    blt cgov:install:site-sections --no-interaction -D drush.ansi=false   # This (of course) loads the site sections and megamenus.
+    ./scripts/utility/cgov_migration_load.sh
+    ;;
+  NANO)
+    blt cgov:install:site-sections --no-interaction -D drush.ansi=false   # This (of course) loads the site sections and megamenus.
+    ./scripts/utility/cgov_migration_load.sh
+    ;;
+  MYPART)
+    blt cgov:install:site-sections --no-interaction -D drush.ansi=false   # This (of course) loads the site sections and megamenus.
+    ./scripts/utility/cgov_migration_load.sh
+    ;;
+  NCICONNECT)
+    blt cgov:install:site-sections --no-interaction   # This (of course) loads the site sections and megamenus.
+    ./scripts/utility/cgov_migration_load.sh
+    ;;
+  *)
+    blt custom:install_cgov_yaml_content_by_module cgov_yaml_content -D drush.ansi=false
+    ## Uninstall cgov_yaml_content once done to turn off entity presave hack for supporting
+    ## drupal-entity.
+    drush pmu cgov_yaml_content
+    ;;
+  esac
+fi
+
 set +v
