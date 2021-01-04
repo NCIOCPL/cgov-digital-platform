db378457e420e0112b57fe270ec19c30647d3a86
Erik Meyer <erik.meyer@publicissapient.com>
2021-01-04 13:15:55 -0500

    (#2996 RPGs Number of Awards Chart Change) Removed 2009 from title and data set. Closes #2996

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/libraries/charts/library/number-rpg-awards.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/libraries/charts/library/number-rpg-awards.js
@@ -8,10 +8,10 @@ function initChart(Chart) {
       type: 'NCI_column'
     },
     title: { text: 'RPGs Number of Awards' },
-    subtitle: { text: 'Fiscal Years 2009 - 2019' },
+    subtitle: { text: 'Fiscal Years 2010 - 2019' },
     plotOptions: {
       series: {
-        pointStart: 2009
+        pointStart: 2010
       }
     },
     xAxis: [{
@@ -25,10 +25,10 @@ function initChart(Chart) {
     },
     series: [{
       name: 'Competing',
-      data: [1388, 1387, 1106, 1220, 1095, 1378, 1371, 1365, 1311, 1356, 1336]
+      data: [1387, 1106, 1220, 1095, 1378, 1371, 1365, 1311, 1356, 1336]
     },{
       name: 'Non-Competing',
-      data: [3791, 3692, 3913, 3801, 3721, 3436, 3396, 3301, 3352, 3424, 3648]
+      data: [3692, 3913, 3801, 3721, 3436, 3396, 3301, 3352, 3424, 3648]
     }]
   });
 };
