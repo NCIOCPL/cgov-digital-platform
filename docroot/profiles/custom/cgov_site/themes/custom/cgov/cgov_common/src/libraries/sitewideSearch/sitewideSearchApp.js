92f7a7ace601741ba7fac2ecd936e23c04d27a67
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2021-02-03 16:50:10 -0500

    (#3019) remove sws app module

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/libraries/sitewideSearch/sitewideSearchApp.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/libraries/sitewideSearch/sitewideSearchApp.js
@@ -3,37 +3,56 @@
  * Sets app config for initialization.
  */
 
-import initialize from '@nciocpl/sitewide-search-app';
-import { getCanonicalURL, getDocumentLanguage, getMetaData, parseUrl } from 'Utilities';
+const initialize = () => {
+  throw new Error("This has been removed for due to issue #3003");
+};
+
+import {
+  getCanonicalURL,
+  getDocumentLanguage,
+  getMetaData,
+  parseUrl
+} from "Utilities";
 
 const instanceConfig = window.sitewideSearchAppSettings;
 
-const metaTags = getMetaData([
-  ['name', 'dcterms.subject'],
-  ['name', 'dcterms.isPartOf'],
-  ['name', 'dcterms.issued'],
-  ['property', 'og:site_name'],
-  ['property', 'og:title']
-], document);
-
+const metaTags = getMetaData(
+  [
+    ["name", "dcterms.subject"],
+    ["name", "dcterms.isPartOf"],
+    ["name", "dcterms.issued"],
+    ["property", "og:site_name"],
+    ["property", "og:title"]
+  ],
+  document
+);
 
 const language = getDocumentLanguage(document);
 
 const canonicalHref = parseUrl(getCanonicalURL(document), document);
 if (!canonicalHref) {
-  throw new Error("SitewideSearchApp cannot be loaded - canonical URL not found");
+  throw new Error(
+    "SitewideSearchApp cannot be loaded - canonical URL not found"
+  );
 }
 
 const canonicalHost = canonicalHref.protocol + "//" + canonicalHref.host;
 
 let pathName = null;
-if(canonicalHref.pathname) {
-  pathName = (canonicalHref.pathname.charAt(0) === "/") ? canonicalHref.pathname : "/" + canonicalHref.pathname;
+if (canonicalHref.pathname) {
+  pathName =
+    canonicalHref.pathname.charAt(0) === "/"
+      ? canonicalHref.pathname
+      : "/" + canonicalHref.pathname;
 }
 
 // If the dictionary path, name, and audience are set, pass in the dictionary URL,
 // name, audience, and Glossary API endpoint.
-const validateAndGetDictionaryConfig = (dictionaryPath, dictionaryName, dictionaryAudience) => {
+const validateAndGetDictionaryConfig = (
+  dictionaryPath,
+  dictionaryName,
+  dictionaryAudience
+) => {
   if (dictionaryPath || dictionaryName || dictionaryAudience) {
     if (!dictionaryPath) {
       console.error("dictionaryPath is not set in app module config.");
@@ -47,7 +66,7 @@ const validateAndGetDictionaryConfig = (dictionaryPath, dictionaryName, dictiona
       console.error("dictionaryAudience is not set in app module config.");
     }
 
-    if(!window.CDEConfig.glossaryConfig.apiServer) {
+    if (!window.CDEConfig.glossaryConfig.apiServer) {
       console.error("Glossary API Server is not set in CDEConfig.");
     }
 
@@ -57,13 +76,13 @@ const validateAndGetDictionaryConfig = (dictionaryPath, dictionaryName, dictiona
         dictionaryName,
         dictionaryAudience,
         glossaryEndpoint: window.CDEConfig.glossaryConfig.apiServer
-      }
+      };
     }
   }
-}
+};
 
 // If the Best Bets collection is set, pass in it and the Best Bets API endpoint.
-const validateAndGetBestBetsConfig = (bestbetsCollection) => {
+const validateAndGetBestBetsConfig = bestbetsCollection => {
   if (bestbetsCollection) {
     if (!window.CDEConfig.sitewideSearchConfig.bestBetsApiServer) {
       console.error("Best Bets API Server is not set in CDEConfig.");
@@ -72,35 +91,39 @@ const validateAndGetBestBetsConfig = (bestbetsCollection) => {
     return {
       bestbetsCollection,
       bestbetsEndpoint: window.CDEConfig.sitewideSearchConfig.bestBetsApiServer
-    }
+    };
   }
-}
+};
 
 const drupalConfig = {
-  analyticsChannel: metaTags['dcterms.subject'],
-  analyticsContentGroup: metaTags['dcterms.isPartOf'],
-  analyticsPublishedDate: metaTags['dcterms.issued'],
+  analyticsChannel: metaTags["dcterms.subject"],
+  analyticsContentGroup: metaTags["dcterms.isPartOf"],
+  analyticsPublishedDate: metaTags["dcterms.issued"],
   apiBaseEndpoint: window.CDEConfig.general.apiServer,
   baseHost: window.location.protocol + "//" + window.location.hostname,
   basePath: pathName ? pathName : canonicalHref,
   rootId: "NCI-app-root",
   searchEndpoint: window.CDEConfig.sitewideSearchConfig.searchApiServer,
   language,
-  siteName: metaTags['og:site_name'],
-  title: metaTags['og:title']
-}
+  siteName: metaTags["og:site_name"],
+  title: metaTags["og:title"]
+};
 
 const config = {
   ...instanceConfig,
   ...drupalConfig,
-  ...validateAndGetDictionaryConfig(instanceConfig.dictionaryPath, instanceConfig.dictionaryName, instanceConfig.dictionaryAudience),
+  ...validateAndGetDictionaryConfig(
+    instanceConfig.dictionaryPath,
+    instanceConfig.dictionaryName,
+    instanceConfig.dictionaryAudience
+  ),
   ...validateAndGetBestBetsConfig(instanceConfig.bestbetsCollection)
-}
+};
 
 // This is a temporary fix for removing the breadcrumb element that displays on Spanish and microsite search results
 // pages. REMOVE THIS when the sitewide fix for the breadcrumbs is implemented!
 let breadcrumbs = document.querySelector(".breadcrumbs");
-if(breadcrumbs !== null && breadcrumbs.parentNode) {
+if (breadcrumbs !== null && breadcrumbs.parentNode) {
   breadcrumbs.parentNode.removeChild(breadcrumbs);
 }
 
