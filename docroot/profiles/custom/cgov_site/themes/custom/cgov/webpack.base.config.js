04e4bd9eb458a5b49a77b25d92c9e35e59899c3d
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2021-10-06 11:01:12 -0400

    (#2782) Add fingerprinting to generated svg sprite

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/webpack.base.config.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/webpack.base.config.js
@@ -1,67 +1,66 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserJSPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 const config = {
-  target: 'web',
+  target: "web",
   output: {
-      filename: '[name].js',
-      path: path.join(__dirname, 'dist/js'),
+    filename: "[name].js",
+    path: path.join(__dirname, "dist/js")
   },
   externals: {
-    'jquery': 'jQuery',
-    'jQuery': 'jQuery',
-    'jquery-ui': 'jQuery.ui',
-    'CDEConfig': 'CDEConfig'
+    jquery: "jQuery",
+    jQuery: "jQuery",
+    "jquery-ui": "jQuery.ui",
+    CDEConfig: "CDEConfig"
   },
   optimization: {
-    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
   },
   module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: ['babel-loader']
-			},
-			{
-				test: /\.s?css$/,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader
-					},
-					'css-loader',
-					'postcss-loader',
-					'sass-loader'
-				],
-			},
-			{
-				test: /\.svg$/,
-				use: [
-					{
-            loader: 'file-loader',
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: ["babel-loader"]
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "file-loader",
             options: {
-              outputPath: '../images/sprites',
-							name: '[name].[ext]',
+              outputPath: "../images/sprites",
+              name: "[name].[ext]?v=[hash:8]",
               emitFile: false
             }
-					}
-				]
-			}
-
-		]
-	},
-    plugins: [
-		new MiniCssExtractPlugin({
-			filename: "../css/[name].css",
-    }),
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "../css/[name].css"
+    })
   ],
   watchOptions: {
     aggregateTimeout: 1000,
     poll: 300,
-    ignored: [/node_modules/,/dist/,/sprites/]
+    ignored: [/node_modules/, /dist/, /sprites/]
   }
-}
+};
 
 module.exports = config;
