10c247f73e61653023e0095f65e9161a00f76275
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2021-10-14 14:34:47 -0400

    (#3181) Update Drupal Core to 9.2.x
    - Updated BLT to 12.x
    - Added in all the BLT plugins that were removed from 12.x
    - (#3195) Updated to Drush 10.x. (This came with BLT 12.x)
    - Updated Paratest to 6.x to work with PHPUnit 9.x
    - Rerolled patches to htaccess that changed in D9
    - Fixed our deprecation check commands to be compatible with BLT 12
    - Fixed the phpunit attributes in phpunit config to turn off caching and
       to match D9 core settings. These were newly created in PHPUnit 9.x.
    - Updated git commit hooks now that BLT has changed the commands.
    - Updated translation files
    - Reverted changes from #3213 now that composer 2 is part of the project
    - Fixed invalid yaml issues in behat.yml
    - BLT 12 removed features as a config strategy. Lucky for us we already
      had replaced drupal:config:import... So we updated the function to
      be more like BLT 12, with a little of the BLT 11 features sprinkled
      in.
    - Updated blt.yml to indicate that only the default site should be
      installed. Otherwise deploying to an ODE was trying to install
      simpletest. Who knows what ACSF will do to us?
    - The "do you want to send data" prompt showed up in a deployment log.
      In fact, it broke the deployment. So adding the telemetry disable to
      hope to try to have that never happen again. Also added in a number
      of missing --no-interaction, which the BLT team is pushing should be
      the fix, not, oh it is not an interactive console in Acquia Cloud
      hooks. Nor, why does it really need to capture usage???
    - Added in a Drupal sass partial to work around some issues that D9
       introduced which affected the moderation sidebar buttons. This can
       be the home for future admin items that appear through the front
       end.
    - Altered the button text for moderation sidebar to not use translatable
      strings. The issue was that a half the buttons were being translated
      and the other half were not.

--- blt/src/Blt/Plugin/Commands/CgovConfigCommands.php
+++ blt/src/Blt/Plugin/Commands/CgovConfigCommands.php
@@ -2,8 +2,11 @@
 
 namespace Cgov\Blt\Plugin\Commands;
 
+use Acquia\Blt\Robo\Blt;
 use Acquia\Blt\Robo\Exceptions\BltException;
 use Acquia\Blt\Robo\Commands\Drupal\ConfigCommand;
+use Acquia\Blt\Robo\Common\UserConfig;
+use Zumba\Amplitude\Amplitude;
 
 /**
  * Defines commands in the "cgov" namespace.
@@ -21,39 +24,112 @@ class CgovConfigCommands extends ConfigCommand {
   public function import() {
     $strategy = $this->getConfigValue('cm.strategy');
 
-    if ($strategy != 'none') {
-      $cm_core_key = $this->getConfigValue('cm.core.key');
-      $this->logConfig($this->getConfigValue('cm'), 'cm');
-      $task = $this->taskDrush();
+    $userConfig = new UserConfig(Blt::configDir());
+    $eventInfo = $userConfig->getTelemetryUserData();
+    $eventInfo['strategy'] = $strategy;
+    Amplitude::getInstance()->queueEvent('config import', $eventInfo);
 
-      $this->invokeHook('pre-config-import');
-
-      // If exported site UUID does not match site active site UUID, set active
-      // to equal exported.
-      // @see https://www.drupal.org/project/drupal/issues/1613424
-      $exported_site_uuid = $this->getExportedSiteUuid($cm_core_key);
-      if ($exported_site_uuid) {
-        $task->drush("config:set system.site uuid $exported_site_uuid");
-      }
-
-      if ($strategy == 'features') {
-        $this->importFeatures($task, $cm_core_key);
-      }
-      else {
-        // Do nothing.
-      }
-
-      $task->drush("cache-rebuild");
-      $result = $task->run();
-      if (!$result->wasSuccessful()) {
-        throw new BltException("Failed to import configuration!");
-      }
-
-      $this->checkFeaturesOverrides($cm_core_key);
-
-      $result = $this->invokeHook('post-config-import');
-
+    if ($strategy == 'none') {
+      // Still clear caches to regenerate frontend assets and such.
+      $result = $this->taskDrush()->drush("cache-rebuild")->run();
       return $result;
+    }
+
+    // The config setting was removed in BLT 12, and this is what the
+    // BLT code hard codes it to. We have not overwritten that key,
+    // and this is the value of the old build.yml. It must be set to
+    // something since it is now gone.
+    $cm_core_key = 'sync';
+    $this->logConfig($this->getConfigValue('cm'), 'cm');
+    $task = $this->taskDrush();
+
+    $this->invokeHook('pre-config-import');
+
+    // If using core-only or config-split strategies, first check to see if
+    // required config is exported.
+    if (in_array($strategy, ['core-only', 'config-split'])) {
+      $core_config_file = $this->getConfigValue('docroot') . '/' . $this->getConfigValue("cm.core.dirs.$cm_core_key.path") . '/core.extension.yml';
+
+      if (!file_exists($core_config_file)) {
+        $this->logger->warning("BLT will NOT import configuration, $core_config_file was not found.");
+        // This is not considered a failure.
+        return 0;
+      }
+    }
+
+    // If exported site UUID does not match site active site UUID, set active
+    // to equal exported.
+    // @see https://www.drupal.org/project/drupal/issues/1613424
+    $exported_site_uuid = $this->getExportedSiteUuid($cm_core_key);
+    if ($exported_site_uuid) {
+      $task->drush("config:set system.site uuid $exported_site_uuid");
+    }
+
+    switch ($strategy) {
+      case 'features':
+        $this->importFeatures($task, $cm_core_key);
+        break;
+
+      case 'core-only':
+        $this->importCoreOnly($task, $cm_core_key);
+        break;
+
+      case 'config-split':
+        // Drush task explicitly to turn on config_split and check if it was
+        // successfully enabled. Otherwise default to core-only.
+        $check_task = $this->taskDrush();
+        $check_task->drush("pm-enable")->arg('config_split');
+        $result = $check_task->run();
+        if (!$result->wasSuccessful()) {
+          $this->logger->warning('Import strategy is config-split, but the config_split module does not exist. Falling back to core-only.');
+          $this->importCoreOnly($task, $cm_core_key);
+          break;
+        }
+        $this->importConfigSplit($task, $cm_core_key);
+        break;
+    }
+
+    $task->drush("cache-rebuild");
+    $result = $task->run();
+    if (!$result->wasSuccessful()) {
+      throw new BltException("Failed to import configuration!");
+    }
+
+    if ($strategy == 'features') {
+      $this->checkFeaturesOverrides($cm_core_key);
+    }
+    $this->checkConfigOverrides();
+
+    $result = $this->invokeHook('post-config-import');
+
+    // Redundant cache-rebuild upon the conclusion of drupal:config:import
+    // to resolve fatal errors post-config actions.
+    $task = $this->taskDrush();
+    $task->drush("cache-rebuild")->run();
+
+    return $result;
+  }
+
+  /**
+   * Import configuration using features module.
+   *
+   * @param mixed $task
+   *   Drush task.
+   * @param string $cm_core_key
+   *   Cm core key.
+   */
+  protected function importFeatures($task, $cm_core_key) {
+    $task->drush("config-import")->arg($cm_core_key)->option('partial');
+    $task->drush("pm-enable")->arg('features');
+    $task->drush("cc")->arg('drush');
+    if ($this->getConfig()->has('cm.features.bundle')) {
+      // Clear drush caches to register features drush commands.
+      foreach ($this->getConfigValue('cm.features.bundle') as $bundle) {
+        $task->drush("features-import-all")->option('bundle', $bundle);
+        // Revert all features again!
+        // @see https://www.drupal.org/node/2851532
+        $task->drush("features-import-all")->option('bundle', $bundle);
+      }
     }
   }
 
