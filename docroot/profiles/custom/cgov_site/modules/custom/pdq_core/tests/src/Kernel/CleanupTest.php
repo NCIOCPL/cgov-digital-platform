c02f6cb9332e3b1ffa6cef21beec3372959fecc5
Bob Kline <bkline@rksystems.com>
2021-10-14 14:35:45 -0400

    [#3204] Fix PDQ cleanup test breakage in Drupal 9
    Rewrite the CleanupTest class completely. The new version ...
    
    * covers more cases than the one where both languages get pushed for every publishing job
    * makes it more obvious what the expected behavior should be and how we're testing
    * verifies the correct set of preserved published revisions after each simulated publishing job
    * compares entire arrays at once instead of each pair of elements separately, so
    * reduces the number of assertions
    * is better documented
    * is as efficient as the test it replaced
    * passes under Drupal 9
    
    One thing that I think was tripping us up is that under Drupal 9 the
    first save of a node leaves behind two revisions instead of one, which
    may have been behind the count discrepancy @bryanpnih ran into. I
    solved that problem by ignoring the dropped revision IDs, instead
    focusing on verifying that the set of revisions left behind after each
    purge operation is exactly what it should be, which is what we really
    care about.

--- docroot/profiles/custom/cgov_site/modules/custom/pdq_core/tests/src/Kernel/CleanupTest.php
+++ docroot/profiles/custom/cgov_site/modules/custom/pdq_core/tests/src/Kernel/CleanupTest.php
@@ -3,10 +3,12 @@
 namespace Drupal\Tests\pdq_core\Kernel;
 
 use CgovPlatform\Tests\CgovSchemaExclusions;
-use Drupal\KernelTests\Core\Entity\EntityKernelTestBase;
+use Drupal\KernelTests\KernelTestBase;
 use Drupal\language\Entity\ConfigurableLanguage;
 use Drupal\node\Entity\NodeType;
-use Drupal\node\Entity\Node;
+use Drupal\Tests\cgov_core\Traits;
+use Drupal\Tests\node\Traits\NodeCreationTrait;
+use Drupal\Tests\user\Traits\UserCreationTrait;
 
 /**
  * Verify that the PDQ content cleanup works as advertised (issue #2815).
@@ -14,19 +16,46 @@ use Drupal\node\Entity\Node;
  * @group cgov
  * @group cgov_site
  */
-class CleanupTest extends EntityKernelTestBase {
+class CleanupTest extends KernelTestBase {
+
+  use Traits\CGovWorkflowAttachmentTrait;
+  use NodeCreationTrait;
+  use UserCreationTrait;
+
+  /**
+   * Tell the pruner to keep this many published revisions for each language.
+   */
+  const REVISIONS_TO_KEEP = 3;
 
   /**
    * {@inheritdoc}
    */
-  public static $modules = ['node', 'language', 'block_content', 'pdq_core'];
+  public static $modules = [
+    'path_alias',
+    'system',
+    'user',
+  ];
 
   /**
-   * Use our own profile instead of one from the standard distribution.
+   * Revision pruning service.
    *
-   * @var string
+   * @var \Drupal\pdq_core\RevisionPruner
    */
-  protected $profile = 'cgov_site';
+  protected $pruner;
+
+  /**
+   * Storage interface.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $storage;
+
+  /**
+   * User ID for node/revision saving.
+   *
+   * @var int
+   */
+  protected $uid;
 
   /**
    * {@inheritdoc}
@@ -37,68 +66,211 @@ class CleanupTest extends EntityKernelTestBase {
     static::$configSchemaCheckerExclusions =
       CgovSchemaExclusions::$configSchemaCheckerExclusions;
     parent::setUp();
-    $type = NodeType::create(['type' => 'test', 'name' => 'test']);
-    $type->save();
-    $this->installSchema('node', 'node_access');
-    $this->installEntitySchema('block_content');
+
+    // Install schemas, configuration, etc., that we need.
+    $this->installEntitySchema('user');
+    $this->installEntitySchema('path_alias');
+    $this->installSchema('system', ['sequences']);
+    $this->installConfig(['system', 'user', 'path_alias']);
+    \Drupal::service('module_installer')->install(['pdq_core']);
+    $node_type = NodeType::create(['type' => 'test', 'name' => 'test']);
+    $node_type->save();
     ConfigurableLanguage::createFromLangcode('es')->save();
+
+    // Plug in PDQ workflow to handle draft->published transition.
+    $this->attachContentTypeToWorkflow('test', 'pdq_workflow');
+
+    // Initialize our test object's properties.
+    $entityTypeManager = \Drupal::service('entity_type.manager');
+    $this->storage = $entityTypeManager->getStorage('node');
+    $this->setCurrentUser($this->createUser([], 'Zeus', TRUE));
+    $this->uid = \Drupal::currentUser()->id();
+    $this->pruner = \Drupal::service('pdq_core.revision_pruner');
   }
 
   /**
    * Test pruning of older revisions.
+   *
+   * For each case, we simulate running a series of PDQ publishing jobs,
+   * in which we send one or both languages for a given PDQ summary node.
+   * The job creates the draft revision(s) for the language(s), and then
+   * publishes the draft(s). Each time a published revision is created, we
+   * remember its revision ID, and which language it was created for. The
+   * last step in the job is to invoke the purge service to clear out the
+   * unwanted revisions, which is all the draft revisions, and all but the
+   * last three published revisions for each language. We verify at the end
+   * of each job that we are left with EXACTLY the revisions we expect.
    */
   public function testPruneOldRevisions() {
-    $admin = $this->drupalCreateUser([], 'Zeus', TRUE);
-    $this->setCurrentUser($admin);
-    $uid = \Drupal::currentUser()->id();
-    $node = Node::create([
-      'title' => 'Sam and Janet Evening',
-      'type' => 'test',
-      'uid' => $uid,
-      'language' => 'en',
-    ]);
-    $node->save();
-    $nid = $node->id();
-    $drop = [$node->getRevisionId()];
-    $node = $node->addTranslation('es');
-    $node->setTitle('No necesitamos insignias apestosas');
-    $node->setNewRevision(TRUE);
-    $node->save();
-    $drop[] = $node->getRevisionId();
-    $languages = ['en', 'es'];
-    $storage = $this->entityTypeManager->getStorage('node');
-    $pub_revisions_to_create = 10;
-    $pub_revisions_to_keep = 3;
-    $keep = [];
-    for ($i = 0; $i < $pub_revisions_to_create; ++$i) {
-      foreach ($languages as $langcode) {
-        $node = $node->getTranslation($langcode);
-        $node->setPublished(FALSE);
-        $node->setNewRevision(TRUE);
-        $node->save();
-        $drop[] = $node->getRevisionId();
-        $node->setPublished(TRUE);
-        $node->setNewRevision(TRUE);
-        $node->save();
-        if ($i < $pub_revisions_to_create - $pub_revisions_to_keep) {
-          $drop[] = $node->getRevisionId();
+
+    // Run each test case, verifying what's left after a purge.
+    $cases = $this->getTestCases();
+    foreach ($cases as $values) {
+      $nid = $values['nid'];
+      $published = ['en' => [], 'es' => []];
+
+      // Emulate what a PDQ publishing job does.
+      foreach ($values['languages'] as $languages) {
+
+        // Create drafts for each of the languges in this job.
+        $job = 1;
+        foreach ($languages as $langcode) {
+          $title = "Node $nid [$langcode] - job $job";
+          $node = $this->storage->load($nid);
+
+          // Create the node if it doesn't yet exist.
+          if (empty($node)) {
+            $translation = $this->createNode([
+              'type' => 'test',
+              'id' => $nid,
+              'langcode' => $langcode,
+            ]);
+          }
+
+          // Otherwise, load it and make sure we have the translation we need.
+          else {
+            $vid = $this->storage->getLatestRevisionId($nid);
+            $revision = $this->storage->loadRevision($vid);
+            if ($revision->hasTranslation($langcode)) {
+              $translation = $revision->getTranslation($langcode);
+            }
+            else {
+              $translation = $revision->addTranslation($langcode);
+            }
+          }
+          $translation->setRevisionCreationTime(\time());
+          $translation->setRevisionUserId($this->uid);
+          $translation->setTitle($title);
+          $translation->moderation_state->value = 'draft';
+          $translation->save();
         }
-        else {
-          $keep[] = $node->getRevisionId();
+
+        // Now publish the draft(s).
+        foreach ($languages as $langcode) {
+          $vid = $this->storage->getLatestRevisionId($nid);
+          $revision = $this->storage->loadRevision($vid);
+          $translation = $revision->getTranslation($langcode);
+          $translation->moderation_state->value = 'published';
+          $translation->setRevisionTranslationAffected(TRUE);
+          $translation->setRevisionCreationTime(\time());
+          $translation->setRevisionUserId($this->uid);
+          $translation->save();
+          $published[$langcode][] = $translation->getRevisionId();
         }
+
+        // Prune the unwanted revisions and verify the results.
+        $this->pruner->dropOldRevisions($nid, self::REVISIONS_TO_KEEP);
+        $node = $this->storage->load($nid);
+        $kept = $this->storage->revisionIds($node);
+        $expected_english = array_slice($published['en'], -self::REVISIONS_TO_KEEP);
+        $expected_spanish = array_slice($published['es'], -self::REVISIONS_TO_KEEP);
+        $expected = array_merge($expected_english, $expected_spanish);
+        sort($kept);
+        sort($expected);
+        $message = "Prune misbehaved for node $nid at the end of job $job";
+        $this->assertEquals($expected, $kept, $message);
+        $job++;
       }
     }
-    $pruner = \Drupal::service('pdq_core.revision_pruner');
-    $dropped = $pruner->dropOldRevisions($nid, $pub_revisions_to_keep);
-    $this->assertCount(count($drop), $dropped);
-    foreach ($dropped as $vid) {
-      $this->assertContains($vid, $drop);
-    }
-    $kept = $storage->revisionIds($node);
-    $this->assertCount(count($keep), $kept);
-    foreach ($kept as $vid) {
-      $this->assertContains($vid, $keep);
-    }
+  }
+
+  /**
+   * Create some test case data.
+   *
+   * In order to achieve acceptable coverage, we use different patterns for
+   * how PDQ publishing jobs send the English and Spanish versions of a PDQ
+   * summary. In some cases the two are always sent together, sometimes there
+   * is only one language for a node, sometimes there are both, but they
+   * don't always get sent in the same jobs. Some of these cases avoid simple
+   * patterns for how the languages alternate with each other.
+   *
+   * Note that we don't have the business rule enforced for cancer information
+   * summaries, which requires that there must be an English revision for a
+   * node before a Spanish translation is created. So we can also test the
+   * case here in which the Spanish translation is saved first, even though
+   * it's not currently needed.
+   *
+   * @return array
+   *   Value sets with a node ID and sequence of languages for simulated
+   *   publishing jobs.
+   */
+  private function getTestCases() {
+    return [
+      [
+        'nid' => 1,
+        'languages' => [
+          ['en', 'es'],
+          ['en', 'es'],
+          ['en', 'es'],
+          ['en', 'es'],
+          ['en', 'es'],
+          ['en', 'es'],
+        ],
+      ],
+      [
+        'nid' => 2,
+        'languages' => [
+          ['en'],
+          ['en'],
+          ['en'],
+          ['en'],
+          ['en'],
+          ['en'],
+        ],
+      ],
+      [
+        'nid' => 3,
+        'languages' => [
+          ['es'],
+          ['es'],
+          ['es'],
+          ['es'],
+          ['es'],
+          ['es'],
+          ['en'],
+        ],
+      ],
+      [
+        'nid' => 4,
+        'languages' => [
+          ['en', 'es'],
+          ['en', 'es'],
+        ],
+      ],
+      [
+        'nid' => 5,
+        'languages' => [
+          ['en'],
+          ['en'],
+          ['es'],
+          ['es'],
+          ['es'],
+          ['en'],
+          ['en'],
+          ['en'],
+          ['en'],
+          ['es'],
+          ['es'],
+          ['es'],
+          ['en'],
+          ['en'],
+        ],
+      ],
+      [
+        'nid' => 6,
+        'languages' => [
+          ['en'],
+          ['es'],
+          ['es'],
+          ['en', 'es'],
+          ['es', 'en'],
+          ['en', 'es'],
+          ['en'],
+          ['es'],
+          ['es'],
+        ],
+      ],
+    ];
   }
 
 }
