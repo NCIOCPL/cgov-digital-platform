94372ad1a2f95eb623e93cf67eb09a5b17d454d8
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2022-01-07 15:38:48 -0500

    (#3260) removal of In-This-Page javascript generation

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/entrypoints/pdq/PDQ.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/src/entrypoints/pdq/PDQ.js
@@ -1,6 +1,3 @@
-// import $ from 'jquery';
-import { getNodeArray } from 'Core/utilities/domManipulation';
-import { lang } from 'Core/libraries/nciConfig/NCI.config';
 import linkAudioPlayer from 'Core/libraries/linkAudioPlayer/linkAudioPlayer';
 import './PDQ.scss';
 
@@ -9,8 +6,6 @@ const language = document.documentElement.lang;
 const onDOMContentLoaded = () => {
   // move health professional/patient toggle up to article head
   moveToggle();
-
-  buildInThisSection(getNodeArray('#cgvBody .accordion > section'));
 
   buildAudioLinks();
 
@@ -27,40 +22,6 @@ const moveToggle = () => {
     pageTitle.insertAdjacentElement('afterend',toggle);
 
   }
-};
-
-/* TODO: create an object or field on the content type that holds In This Section link structure */
-const buildInThisSection = (sections) => {
-  sections.map((section)=>{
-    const headers = getNodeArray('h3, h4',section);
-
-    // filter out "About This PDQ" section, sections with no h3's or h4's and sections with Key Points
-    if(section.id !== '_AboutThis_1' && headers.length > 0 && !headers[0].id.match(/kpBox/)) {
-      let nav = `<nav class="in-this-section role="navigation"><h6>${lang.InThisSection[language]}</h6><ul>`;
-      headers.map((header,i) => {
-        // check to see if we need to nest a new unordered list for h4's
-        if( i !== 0 && headers[i-1].nodeName.toLowerCase() === 'h3' && header.nodeName.toLowerCase() === 'h4' ){
-          nav += `<ul><li><a href="#${header.id}">${header.innerHTML}</a>`;
-        } else {
-          if(i === 0) {
-            // this is the first item in the list
-            nav += `<li><a href="#${header.id}">${header.innerHTML}</a>`;
-          } else {
-            // this is an h3 with a previous sibling of h3 or
-            // this is an h4 with a previous sibling of h4
-            nav += `</li><li><a href="#${header.id}">${header.innerHTML}</a>`;
-          }
-        }
-        // next header is an h3 so close this nested list
-        if (headers[i+1] && headers[i+1].nodeName.toLowerCase() === 'h3' && header.nodeName.toLowerCase() === 'h4') {
-          nav += `</li></ul>`;
-        }
-      });
-      nav += `</li></ul></nav>`;
-
-      section.querySelector(".pdq-sections").insertAdjacentHTML('afterbegin',nav);
-    }
-  });
 };
 
 // fix citation anchor links
