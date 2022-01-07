e1b3fd4739e45333e095fba682a10c995afde91d
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2022-01-07 16:18:48 -0500

    (#3275) Fix sprite paths.
    This is a short term fix as all of our image references are pretty
    questionable. There is a mix of absolute references and some references using
    Aliases. Alias end up resolving to relative paths.
    
    The initial issue was that the previous fix to update the loaders
    bumped css-loader, which now looks at absolute paths. So a fix was
    added to ignore all urls from resolution. This is where things went
    wrong. This mean Aliases were no longer resolved.
    
    So TL;DR; we:
    - added a new Alias for the Dist/Image folder for sprites.
    - updated calls to sprites to reference that alias, which follows the
      previous file-loader outputPath
    - Updated the css-loader config to ignore absolute urls. This probably
      should be fixed at some point to use the ImageDist alias.

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/webpack.base.config.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/webpack.base.config.js
@@ -32,7 +32,19 @@ const config = {
           {
             loader: MiniCssExtractPlugin.loader
           },
-          "css-loader?url=false",
+          {
+            loader: "css-loader",
+            options: {
+              url: (uri, resourcePath) => {
+                // Ignore absolute paths.
+                if (uri.startsWith('/')) {
+                  return false;
+                }
+
+                return true;
+              }
+            }
+          },
           "postcss-loader",
           "sass-loader"
         ]
@@ -44,8 +56,8 @@ const config = {
             loader: "file-loader",
             options: {
               outputPath: "../images/sprites",
-              name: "[name].[ext]",
-              emitFile: false
+              name: "[name].[ext]?v=[hash]",
+              emitFile: false,
             }
           }
         ]
