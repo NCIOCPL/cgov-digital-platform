e8fddedfc3d9070d418199d5e0fb278e85496086
Ramu Challa <challar2@nih.giv>
2022-06-17 10:28:21 -0400

    (#3402) Unsanitized URL parameter passing input through
     Fixed XSS vulnerabilities of year, month and topic parameters:
     - Added XSS and HTML escape filters
     - Added validations for year, month and topic parameters.
     - Added 400 Bad Request status code if any input is invalid
     - Added year, month and topic validations for the places where getBlogSeriesTitle method is called.
     - Updated year, month and topic validations

--- docroot/profiles/custom/cgov_site/modules/custom/cgov_blog/src/CgovBlogTwigExtensions.php
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_blog/src/CgovBlogTwigExtensions.php
@@ -3,6 +3,7 @@
 namespace Drupal\cgov_blog;
 
 use Drupal\cgov_blog\Services\BlogManagerInterface;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 /**
  * Extend Drupal's Twig_Extension class.
@@ -55,6 +56,22 @@ class CgovBlogTwigExtensions extends \Twig_Extension {
    *   Field has text content.
    */
   public function getBlogSeriesTitle($month, $year, $topic, $node) {
+
+    // Throw an exception if the year is invalid.
+    if (isset($year) && (!is_numeric($year) || $year < 0)) {
+      throw new BadRequestHttpException('year is invalid');
+    }
+    // Throw an exception if the month is invalid.
+    if (isset($month) && (!is_numeric($month) || $month < 1 || $month > 12)) {
+      throw new BadRequestHttpException('month is invalid');
+    }
+
+    // Make sure For topic, only alphanumeric characters should be
+    // allowed, plus colon ( : ) and hyphen ( - ).
+    if (!empty($topic) && !preg_match('/^[a-zA-Z0-9:-]*$/', $topic)) {
+      throw new BadRequestHttpException('month is invalid');
+    }
+
     return $this->blogManager->getBlogSeriesTitle($month, $year, $topic, $node);
   }
 
