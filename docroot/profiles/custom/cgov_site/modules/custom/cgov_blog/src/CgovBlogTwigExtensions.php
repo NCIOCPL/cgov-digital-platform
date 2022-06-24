fb5868124ad057f6f97b4bbc459d0031a7c49cef
Blair Learn <blairl-nih@users.noreply.github.com>
2022-06-24 14:35:44 -0400

    (#3402) Sanitize inputs for blog title display.
    - Prevent injection of non-numeric month and year values in blog title.
    - Safer node loading for breadcrumb and language toggle.
    - Modify logic for retrieving blog series title for clarity.

--- docroot/profiles/custom/cgov_site/modules/custom/cgov_blog/src/CgovBlogTwigExtensions.php
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_blog/src/CgovBlogTwigExtensions.php
@@ -3,6 +3,7 @@
 namespace Drupal\cgov_blog;
 
 use Drupal\cgov_blog\Services\BlogManagerInterface;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 /**
  * Extend Drupal's Twig_Extension class.
@@ -47,7 +48,7 @@ class CgovBlogTwigExtensions extends \Twig_Extension {
    * @param string $year
    *   Year value.
    * @param string $topic
-   *   Topic value.
+   *   Topic ID if present.
    * @param object $node
    *   Node object.
    *
@@ -55,7 +56,20 @@ class CgovBlogTwigExtensions extends \Twig_Extension {
    *   Field has text content.
    */
   public function getBlogSeriesTitle($month, $year, $topic, $node) {
-    return $this->blogManager->getBlogSeriesTitle($month, $year, $topic, $node);
+
+    // The only allowed characters in dates are numbers. Once we eliminate
+    // everything else, there's no need to do further checks.
+    if($year != NULL && (!is_numeric($year) || $year < 0)) {
+        throw new BadRequestHttpException('year is invalid');
+    }
+    if($month != NULL && (!is_numeric($month) || $month < 1 || $month > 12)) {
+        throw new BadRequestHttpException('month is invalid');
+    }
+
+    // When called from a twig template, $topic may be an empty string.
+    $includeTopic = isset($topic) && (strlen(trim($topic)) > 0);
+
+    return $this->blogManager->getBlogSeriesTitle($month, $year, $includeTopic, $node);
   }
 
 }
