204399f53deffb11158edce1ebd7908083cc464d
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2022-06-29 21:00:40 -0400

    (#3384) Update CTHP for NCIDS
    - Changed around CTHP audience toggle on Table now that page options have
      moved.

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/cthp/cthp.ts
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/cthp/cthp.ts
@@ -2,6 +2,7 @@ import dropdownInjector from 'Libraries/cthpDropdown';
 import patternInjector from 'Core/libraries/patternInjector';
 import accordionSettings from 'Core/libraries/patternInjector/configs/cthp.js';
 import './cthp.scss';
+import './cthp-legacy.scss';
 
 const onDOMContentLoaded = () => {
 	dropdownInjector();
