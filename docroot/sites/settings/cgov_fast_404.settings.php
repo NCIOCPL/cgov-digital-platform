a08305a1e19f7094bcfe3e8897968741586093a4
James Frank <james.frank@nih.gov>
2022-07-20 16:20:36 -0400

    (#3450) Add meta tag for generator to fast 404 page
    - Adds a generator tag to be used in scripts to detect fast 404 responses

--- docroot/sites/settings/cgov_fast_404.settings.php
+++ docroot/sites/settings/cgov_fast_404.settings.php
@@ -8,4 +8,4 @@
 $config['system.performance']['fast_404']['enabled'] = TRUE;
 $config['system.performance']['fast_404']['paths'] = '/.*/';
 $config['system.performance']['fast_404']['exclude_paths'] = '/\/ErrorPages\/.*/i';
-$config['system.performance']['fast_404']['html'] = '<!DOCTYPE html><html><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
+$config['system.performance']['fast_404']['html'] = '<!DOCTYPE html><html><head><title>404 Not Found</title><meta name="Generator" content="CGOV Fast 404" /></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
