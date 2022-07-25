28bf1238cfa7b422eb1f35fb9ac592b104392b28
Blair Learn <blairl-nih@users.noreply.github.com>
2022-07-25 13:30:02 -0400

    Reallow test

--- docroot/profiles/custom/cgov_site/tests/src/Functional/PdqNodeTypeTest.php
+++ docroot/profiles/custom/cgov_site/tests/src/Functional/PdqNodeTypeTest.php
@@ -1,83 +1,83 @@
 <?php
 
-// namespace Drupal\Tests\node\Functional;
+namespace Drupal\Tests\node\Functional;
 
-// use Drupal\node\Entity\NodeType;
-// use Drupal\Tests\system\Functional\Menu\AssertBreadcrumbTrait;
-// use Drupal\Tests\system\Functional\Cache\AssertPageCacheContextsAndTagsTrait;
-// use CgovPlatform\Tests\CgovSchemaExclusions;
+use Drupal\node\Entity\NodeType;
+use Drupal\Tests\system\Functional\Menu\AssertBreadcrumbTrait;
+use Drupal\Tests\system\Functional\Cache\AssertPageCacheContextsAndTagsTrait;
+use CgovPlatform\Tests\CgovSchemaExclusions;
 
-// /**
-//  * Ensures the PDQ content types are correctly configured.
-//  *
-//  * @group node
-//  */
-// class PdqNodeTypeTest extends NodeTestBase {
+/**
+ * Ensures the PDQ content types are correctly configured.
+ *
+ * @group node
+ */
+class PdqNodeTypeTest extends NodeTestBase {
 
-//   use AssertBreadcrumbTrait;
-//   use AssertPageCacheContextsAndTagsTrait;
+  use AssertBreadcrumbTrait;
+  use AssertPageCacheContextsAndTagsTrait;
 
-//   /**
-//    * Load the CancerGov site profile.
-//    *
-//    * @var string
-//    */
-//   protected $profile = 'cgov_site';
+  /**
+   * Load the CancerGov site profile.
+   *
+   * @var string
+   */
+  protected $profile = 'cgov_site';
 
-//   /**
-//    * The theme to use with this test.
-//    *
-//    * @var string
-//    */
-//   protected $defaultTheme = 'cgov_common';
+  /**
+   * The theme to use with this test.
+   *
+   * @var string
+   */
+  protected $defaultTheme = 'cgov_common';
 
-//   /**
-//    * Modules to enable.
-//    *
-//    * @var array
-//    */
-//   public static $modules = [];
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = [];
 
 
-//   /**
-//    * PDQ Content types.
-//    *
-//    * This field contains a list of all the content types to be tested.
-//    * Potentially, this could be expanded to include a list of the fields
-//    * expected to be associated with the type.
-//    *
-//    * @var array
-//    */
-//   private $contentTypes = [
-//     'pdq_cancer_information_summary',
-//   ];
+  /**
+   * PDQ Content types.
+   *
+   * This field contains a list of all the content types to be tested.
+   * Potentially, this could be expanded to include a list of the fields
+   * expected to be associated with the type.
+   *
+   * @var array
+   */
+  private $contentTypes = [
+    'pdq_cancer_information_summary',
+  ];
 
-//   /**
-//    * {@inheritdoc}
-//    */
-//   public function setUp() {
-//     static::$configSchemaCheckerExclusions = CgovSchemaExclusions::$configSchemaCheckerExclusions;
-//     parent::setUp();
-//   }
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    static::$configSchemaCheckerExclusions = CgovSchemaExclusions::$configSchemaCheckerExclusions;
+    parent::setUp();
+  }
 
-//   /**
-//    * Ensures that node type functions (node_type_get_*) work correctly.
-//    *
-//    * Load available node types and validate the returned data.
-//    */
-//   public function testNodeTypesArePresent() {
+  /**
+   * Ensures that node type functions (node_type_get_*) work correctly.
+   *
+   * Load available node types and validate the returned data.
+   */
+  public function testNodeTypesArePresent() {
 
-//     foreach ($this->contentTypes as $name) {
-//       $node = NodeType::load($name);
+    foreach ($this->contentTypes as $name) {
+      $node = NodeType::load($name);
 
-//       $this->assertTrue(isset($node), "Node type $name is available.");
+      $this->assertTrue(isset($node), "Node type $name is available.");
 
-//       $label = $node->label();
-//       $this->assertTrue($label != NULL && strlen($label) > 0, "Node type $name has a non-blank label.");
+      $label = $node->label();
+      $this->assertTrue($label != NULL && strlen($label) > 0, "Node type $name has a non-blank label.");
 
-//       $description = $node->getDescription();
-//       $this->assertTrue($description != NULL && strlen($description) > 0, "Node type $name has a non-blank description.");
-//     }
-//   }
+      $description = $node->getDescription();
+      $this->assertTrue($description != NULL && strlen($description) > 0, "Node type $name has a non-blank description.");
+    }
+  }
 
-// }
+}
