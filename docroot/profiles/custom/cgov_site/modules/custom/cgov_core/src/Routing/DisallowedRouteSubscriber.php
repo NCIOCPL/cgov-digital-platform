a4b41150aa85097383109a55a2263248bed7b03e
Ramu Challa <challar2@nih.giv>
2022-07-28 13:35:46 -0400

    (#3441) Disable password reset functionality in Drupal
    - Added disallow_routes config variable
    - Created a new route subscriber to disallow the required routes

---
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_core/src/Routing/DisallowedRouteSubscriber.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\cgov_core\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+use Drupal\Core\Site\Settings;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class DisallowedRouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    // Always deny access to unwanted routes.
+    $disallow_routes = Settings::get('disallow_routes', []);
+    // Loop through all of the disallowed routes.
+    foreach ($disallow_routes as $disallow_route) {
+      if ($route = $collection->get($disallow_route)) {
+        $route->setRequirement('_access', 'FALSE');
+      }
+    }
+  }
+
+}
