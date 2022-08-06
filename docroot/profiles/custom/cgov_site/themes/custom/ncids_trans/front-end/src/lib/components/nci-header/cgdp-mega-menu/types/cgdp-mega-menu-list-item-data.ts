362f9695d13feeebe88cec3376c53b413d879ac8
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2022-08-05 22:50:30 -0400

    (#3277) Implement NCIDS Header Megamenu JS
    Fix mega menu formatting and tests

---
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/lib/components/nci-header/cgdp-mega-menu/types/cgdp-mega-menu-list-item-data.ts
@@ -0,0 +1,11 @@
+/**
+ * Represents a list item (aka link) in a Mega Menu list.
+ */
+type CgdpMegaMenuListItemData = {
+	/** The text of the link */
+	label: string;
+	/** The url of the link */
+	url: string;
+};
+
+export default CgdpMegaMenuListItemData;
