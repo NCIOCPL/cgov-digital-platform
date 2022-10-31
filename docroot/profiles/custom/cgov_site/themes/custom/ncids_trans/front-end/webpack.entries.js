65abfe47bc86cf9edd8b19d338e05cc1473508cc
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2022-10-31 10:41:26 -0400

    (#3387) Remove Legacy Regions from ncids theme

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/webpack.entries.js
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/webpack.entries.js
@@ -6,7 +6,6 @@
 const path = require('path');
 
 module.exports = {
-	Common: path.resolve(__dirname, 'src/entrypoints/global/Common.js'),
 	'ncids-common': path.resolve(
 		__dirname,
 		'src/entrypoints/global/ncids-common.ts'
