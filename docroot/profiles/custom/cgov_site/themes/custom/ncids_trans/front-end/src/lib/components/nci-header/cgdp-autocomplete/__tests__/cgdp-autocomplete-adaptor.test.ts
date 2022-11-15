cb87fc211e35cdf2370f687243de38917d507745
Dev Rana <dev-rana-publicis@users.noreply.github.com>
2022-11-15 14:20:32 -0500

    (#3279) Autosuggest Setup

---
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/lib/components/nci-header/cgdp-autocomplete/__tests__/cgdp-autocomplete-adaptor.test.ts
@@ -0,0 +1,87 @@
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
+import * as nock from 'nock';
+import CgdpAutocompleteAdapter from '../cgdp-autocomplete-adapter';
+import axios from 'axios';
+axios.defaults.adapter = require('axios/lib/adapters/http');
+
+describe('cgdp-megamenu-adapter', () => {
+	let consoleError: jest.SpyInstance;
+
+	beforeEach(() => {
+		consoleError = jest.spyOn(console, 'error').mockImplementation(() => {
+			return null;
+		});
+	});
+
+	afterEach(() => {
+		document.getElementsByTagName('body')[0].innerHTML = '';
+		consoleError.mockRestore();
+	});
+
+	beforeAll(() => {
+		nock.disableNetConnect();
+	});
+
+	afterAll(() => {
+		nock.cleanAll();
+		nock.enableNetConnect();
+		jest.restoreAllMocks();
+	});
+
+	const clientAutocomplete = axios.create({
+		baseURL: 'https://webapis.cancer.gov/sitewidesearch/v1/',
+		responseType: 'json',
+	});
+
+	it('the adaptor', async () => {
+		const scope = nock('https://webapis.cancer.gov/sitewidesearch/v1/')
+			.get('/Autosuggest/cgov/en/can')
+			.once()
+			.reply(200, {
+				results: [
+					{ term: 'breast cancer' },
+					{ term: 'lung cancer' },
+					{ term: 'cervical cancer' },
+					{ term: 'ovarian cancer' },
+					{ term: 'prostate cancer' },
+					{ term: 'skin cancer' },
+					{ term: 'liver cancer' },
+					{ term: 'colon cancer' },
+					{ term: 'pancreatic cancer' },
+					{ term: 'bone cancer' },
+				],
+				total: 1173,
+			});
+		const ACSource = new CgdpAutocompleteAdapter(
+			clientAutocomplete,
+			'cgov',
+			'en'
+		);
+		const terms = await ACSource.getAutocompleteSuggestions('can');
+		expect(terms).toHaveLength(10);
+
+		scope.isDone();
+	});
+
+	it('handles bad responses', async () => {
+		const scope = nock('https://webapis.cancer.gov/sitewidesearch/v1/')
+			.get('/Autosuggest/foo/en/can')
+			.once()
+			.reply(404);
+		const ACSource = new CgdpAutocompleteAdapter(
+			clientAutocomplete,
+			'foo',
+			'en'
+		);
+
+		const terms = await ACSource.getAutocompleteSuggestions('can');
+
+		expect(terms).toHaveLength(0);
+		expect(consoleError).toHaveBeenCalledWith(
+			'Autosuggest Fetch Error: Error: Request failed with status code 404'
+		);
+
+		scope.isDone();
+	});
+});
--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/lib/components/nci-header/nci-header.ts
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/lib/components/nci-header/nci-header.ts
@@ -1,16 +1,84 @@
 import axios from 'axios';
-import { NCIExtendedHeaderWithMegaMenu } from '@nciocpl/ncids-js';
+import {
+	NCIExtendedHeaderWithMegaMenu,
+	NCIAutocomplete,
+} from '@nciocpl/ncids-js';
 import { CgdpMobileMenuAdaptor } from './cgdp-mobile-menu';
 import CgdpMegaMenuAdaptor from './cgdp-mega-menu/cgdp-megamenu-adapter';
+import CgdpAutocompleteAdapter from './cgdp-autocomplete/cgdp-autocomplete-adapter';
 import {
 	primaryNavLinkClickHandler,
 	megaMenuOpenHandler,
 	megaMenuLinkClickHandler,
 } from './mega-menu-analytics-handlers';
+import { searchSubmitHandler } from './auto-suggest-analytics-handlers';
+declare global {
+	interface CDEConfig {
+		sitewideSearchConfig: {
+			searchApiServer: string;
+		};
+	}
+}
 
 /**
- * Wires up a usa-sidenav for the cdgp requirements.
+ * Initializes the autocomplete, and someday the search form.
  */
+const initializeSearch = () => {
+	// TODO: Handle the use case for analytics when there is no autocomplete.
+
+	// So there must be a collection in order to have an autocomplete.
+	// There must also be an input too. Good thing they are both handled here.
+	const inputEl = document.getElementById(
+		'nci-header-search__field'
+	) as HTMLInputElement;
+	const collection = inputEl?.dataset.autosuggestCollection;
+
+	// No collection is ok, this just means no autocomplete.
+	if (!collection) {
+		return;
+	}
+
+	// There must be an API endpoint for an autocomplete.
+	const searchApiServer =
+		window.CDEConfig?.sitewideSearchConfig?.searchApiServer;
+
+	if (!searchApiServer) {
+		console.error(`CDEConfig searchApiServer must be provided`);
+		return;
+	}
+
+	// Default to English is somehow we are missing the lang, but we should not.
+	const lang =
+		document.documentElement.lang === '' ? 'en' : document.documentElement.lang;
+
+	const client = axios.create({
+		baseURL: searchApiServer,
+		responseType: 'json',
+	});
+
+	const ACSource = new CgdpAutocompleteAdapter(client, collection, lang);
+	let minPlaceholderMsg = 'Please enter 3 or more characters';
+	if (lang !== 'en') {
+		minPlaceholderMsg = 'Ingrese 3 o más caracteres';
+	}
+	NCIAutocomplete.create(inputEl, {
+		autocompleteSource: ACSource,
+		maxOptionsCount: 10,
+		minCharCount: 3,
+		// TODO: Translate the placeholder.
+		minPlaceholderMsg: minPlaceholderMsg,
+		listboxClasses: 'listboxWidth',
+	});
+
+	// const acForm = document.getElementsByClassName('nci-header-search');
+	// const acFormEl = acForm[0] as HTMLFormElement;
+	// TODO: Change this to inputEl when ncids is updated.
+	inputEl.addEventListener(
+		'nci-autocomplete:formSubmission',
+		searchSubmitHandler
+	);
+};
+
 const initialize = () => {
 	const headerEl = document.getElementById('nci-header');
 	if (!headerEl) {
@@ -57,6 +125,10 @@ const initialize = () => {
 		mobileMenuSource,
 		megaMenuSource,
 	});
+
+	// The autocomplete for the sitewide search is actually a separate element.
+	// At this time the header is not needed.
+	initializeSearch();
 };
 
 export default initialize;
