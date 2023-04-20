b44bd1dd5a36b8171eb9a9f1ab4a5b55ec9980c5
Dev Rana <dev-rana-publicis@users.noreply.github.com>
2023-04-20 16:40:45 -0400

    (#3612, #3690, #3691) Build NCIDS External Feature Card.
    - (#3690, #3691) NCIDS Feature Card 16:9 & 4:3 Image Styles.
    
    Closes #3612
    Closes #3690
    Closes #3691

---
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_image/src/CgovImageTwigExtensions.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\cgov_image;
+
+use Drupal\image\Entity\ImageStyle;
+use Drupal\cgov_image\CgovImageTools;
+
+/**
+ * Class CgovImageTwigExtensions.
+ */
+class CgovImageTwigExtensions extends \Twig_Extension {
+  /**
+   * A list with all the defined functions. The first parameter is the name
+   * by which we will call the function. The second parameter is the
+   * name of the function that will implement the function.
+   */
+
+  public function getFunctions() {
+    return [
+      new \Twig_SimpleFunction('get_placeholder_image', [$this, 'getPlaceholderImage']),
+    ];
+  }
+
+  /**
+   * Returns the extension name.
+   */
+  public function getName() {
+    return 'cgov_image.CgovImageTwigExtensions';
+  }
+
+  /**
+   * Function that returns an example text with a color passed as a parameter.
+   */
+  public function getPlaceholderImage($image_style) {
+    $test = new CgovImageTools();
+    $crop = $test->findCropByStyle($image_style);
+    $uri = 'module://cgov_image/img/placeholder-' . $crop . '.png';
+    $image_style = ImageStyle::load($image_style);
+    $absolute_path = \Drupal::service('file_url_generator')->transformRelative($image_style->buildUrl($uri));
+    return $absolute_path;
+  }
+
+}
