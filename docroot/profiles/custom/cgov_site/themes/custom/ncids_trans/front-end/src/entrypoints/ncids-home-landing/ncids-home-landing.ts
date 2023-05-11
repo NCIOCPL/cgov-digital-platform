fc079a0ef6ff5ceb61f5a9d9433ef2df7b631681
Olivia Tharp <olivia.tharp@nih.gov>
2023-05-11 16:56:22 -0500

    (#3786) Add feature card analtyics
    - Adds landing page click tracker helper
    
    Closes #3786

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/ncids-home-landing/ncids-home-landing.ts
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/ncids-home-landing/ncids-home-landing.ts
@@ -1,1 +1,10 @@
 import './ncids-home-landing.scss';
+
+import cgdpFeatureCardRow from '../../lib/components/cgdp-feature-card-row';
+
+const onDOMContentLoaded = () => {
+	// Init feature cards
+	cgdpFeatureCardRow();
+};
+
+window.addEventListener('DOMContentLoaded', onDOMContentLoaded);
