e9cdaf1193a3bbc2bb43e93c4eb8a6b53ed5e1ff
Dev Rana <dev-rana-publicis@users.noreply.github.com>
2023-05-15 15:50:09 -0400

    (#3788) NCIDS Guide Card with Img and Desc Analytics
    - Added labels to guide cards for a11y
    
    Closes #3788

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/ncids-home-landing/ncids-home-landing.ts
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/ncids-home-landing/ncids-home-landing.ts
@@ -1,11 +1,14 @@
 import './ncids-home-landing.scss';
 
 import cgdpFeatureCardRow from '../../lib/components/cgdp-feature-card-row';
+import cgdpGuideCardRow from '../../lib/components/cgdp-guide-card-row';
 import cgdpPromoBlockInit from '../../lib/components/nci-promo-block';
 
 const onDOMContentLoaded = () => {
 	// Init feature cards
 	cgdpFeatureCardRow();
+	// Init guide cards
+	cgdpGuideCardRow();
 	// Init promoblock
 	cgdpPromoBlockInit();
 };
---
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/lib/components/cgdp-guide-card-row/__tests__/cgdp-guide-card-row.bad.dom.ts
@@ -0,0 +1,109 @@
+export const cgdpGuideCardRowBadDom = `
+  <section class="usa-section usa-section--light cgdp-guide-card-row" data-eddl-landing-row>
+    <div class="grid-container">
+      <div class="nci-title-aligned-card-group">
+      <!-- Missing aria-labelled-by and description (send error) -->
+        <h2 class="nci-guide-card__header" id="1">Patients and Caregivers, with a Title Long Enough to Wrap to a Second Line</h2>
+        <div class="nci-guide-card nci-guide-card--with-image-and-description" data-eddl-landing-item="guide_card">
+          <div class="nci-guide-card__wrapper">
+            <picture class="nci-guide-card__image">
+              <img src="/sites/default/files/styles/ncids_guide_card_16x9/module/cgov_image/img/placeholder-16x9.png?itok=2kKgOzXH" alt="No Image Placeholder">
+            </picture>
+            <div class="nci-guide-card__body">
+              <ul class="nci-card__button-group">
+                <li>
+                  <a href="https://www.google.com" aria-label="" class="usa-button usa-button--outline usa-button--secondary" data-eddl-landing-item-link-type="External">Learn More</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- No heading (send description) -->
+        <div class="nci-guide-card nci-guide-card--with-image-and-description" data-eddl-landing-item="guide_card">
+          <div class="nci-guide-card__wrapper">
+            <picture class="nci-guide-card__image">
+              <img src="/sites/default/files/styles/ncids_guide_card_16x9/module/cgov_image/img/placeholder-16x9.png?itok=2kKgOzXH" alt="No Image Placeholder">
+            </picture>
+            <div class="nci-guide-card__body">
+              <p class="nci-guide-card__description">Support for the best science underpins everything NCI does. NCI supports the best scientists and research projects through a rigorous grant application and peer review process.</p>
+              <ul class="nci-card__button-group">
+                <li>
+                  <a href="https://www.google.com" aria-label="Learn More about something important" class="usa-button usa-button--outline usa-button--secondary" data-eddl-landing-item-link-type="External">Learn More</a>
+                </li>
+                <li>
+                  <a href="https://www.google.com" aria-label="Learn More 1" class="usa-button usa-button--outline usa-button--secondary" data-eddl-landing-item-link-type="External">Learn More 1</a>
+                </li>
+                <li>
+                  <a href="/test/internal-promo-block-test-article" aria-label="Internal Button Alt Text" class="usa-button usa-button--outline usa-button--secondary" data-eddl-landing-item-link-type="Internal">[Internal Promo Block] Card Title</a>
+                </li>
+                <li>
+                  <a href="/test/internal-promo-block-test-article-0" aria-label="Internal Button Alt Text" class="usa-button usa-button--outline usa-button--secondary" data-eddl-landing-item-link-type="Internal">[Guide Card] Browser Title</a>
+                </li>
+                <li>
+                  <a href="/test/internal-promo-block-test-article-0" aria-label="Internal Button Alt Text" class="usa-button usa-button--outline usa-button--secondary" data-eddl-landing-item-link-type="Internal">Internal Button Text</a>
+                </li>
+                <li>
+                  <a href="/about-cancer/coping/feelings" aria-label="Internal Button Alt Text" class="usa-button usa-button--outline usa-button--secondary" data-eddl-landing-item-link-type="Internal">Feelings and Cancer</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="usa-section usa-section--light cgdp-guide-card-row" data-eddl-landing-row>
+    <div class="grid-container">
+      <div class="nci-title-aligned-card-group">
+        <h2 class="nci-guide-card__header" id="3"></h2>
+        <div class="nci-guide-card nci-guide-card--with-image-and-description" data-eddl-landing-item="guide_card" aria-labelledby="3">
+          <div class="nci-guide-card__wrapper">
+            <picture class="nci-guide-card__image">
+              <img src="/sites/default/files/styles/ncids_guide_card_16x9/module/cgov_image/img/placeholder-16x9.png?itok=2kKgOzXH" alt="No Image Placeholder">
+            </picture>
+            <div class="nci-guide-card__body">
+              <p class="nci-guide-card__description">NCI is the nation's trusted source for cancer information. We're here with information about causes and risk factors, early detection and diagnosis, and treatment options.</p>
+              <ul class="nci-card__button-group">
+                <li>
+                  <a href="https://www.google.com" aria-label="" class="usa-button usa-button--outline usa-button--secondary" data-eddl-landing-item-link-type="External">Learn More</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- Missing matching id (send description) -->
+        <h2 class="nci-guide-card__header">Researchers</h2>
+        <div class="nci-guide-card nci-guide-card--with-image-and-description" data-eddl-landing-item="guide_card" aria-labelledby="4">
+          <div class="nci-guide-card__wrapper">
+            <picture class="nci-guide-card__image">
+              <img src="/sites/default/files/styles/ncids_guide_card_16x9/module/cgov_image/img/placeholder-16x9.png?itok=2kKgOzXH" alt="No Image Placeholder">
+            </picture>
+            <div class="nci-guide-card__body">
+              <p class="nci-guide-card__description">Support for the best science underpins everything NCI does. NCI supports the best scientists and research projects through a rigorous grant application and peer review process.</p>
+              <ul class="nci-card__button-group">
+                <li>
+                  <a href="https://www.google.com" aria-label="Learn More about something important" class="usa-button usa-button--outline usa-button--secondary" data-eddl-landing-item-link-type="External">Learn More     </a>
+                </li>
+                <li>
+                  <a href="https://www.google.com" aria-label="Learn More 1" class="usa-button usa-button--outline usa-button--secondary" data-eddl-landing-item-link-type=""></a>
+                </li>
+                <li>
+                  <a href="/test/internal-promo-block-test-article" aria-label="Internal Button Alt Text" class="usa-button usa-button--outline usa-button--secondary" data-eddl-landing-item-link-type="Internal">[Internal Promo Block] Card Title</a>
+                </li>
+                <li>
+                  <a href="/test/internal-promo-block-test-article-0" aria-label="Internal Button Alt Text" class="usa-button usa-button--outline usa-button--secondary" data-eddl-landing-item-link-type="Internal">[Guide Card] Browser Title</a>
+                </li>
+                <li>
+                  <a href="/test/internal-promo-block-test-article-0" aria-label="Internal Button Alt Text" class="usa-button usa-button--outline usa-button--secondary" data-eddl-landing-item-link-type="Internal">Internal Button Text</a>
+                </li>
+                <li>
+                  <a href="/about-cancer/coping/feelings" aria-label="Internal Button Alt Text" class="usa-button usa-button--outline usa-button--secondary" data-eddl-landing-item-link-type="Internal">Feelings and Cancer</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+`;
