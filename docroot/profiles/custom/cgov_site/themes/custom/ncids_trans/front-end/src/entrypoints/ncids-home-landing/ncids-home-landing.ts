bf21f2b32086082b1c3e2d1a0f9aafcdd98ef2e1
Derek Lescarbeau <derek.lescarbeau@publicissapient.com>
2023-05-16 14:13:35 -0400

    (#3787) PromoBlock Analytics
    - Adds analytics tracking for promoblock along with test coverage
    
    Closes #3787

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/ncids-home-landing/ncids-home-landing.ts
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/ncids-home-landing/ncids-home-landing.ts
@@ -1,10 +1,13 @@
 import './ncids-home-landing.scss';
 
 import cgdpFeatureCardRow from '../../lib/components/cgdp-feature-card-row';
+import cgdpPromoBlockInit from '../../lib/components/nci-promo-block';
 
 const onDOMContentLoaded = () => {
 	// Init feature cards
 	cgdpFeatureCardRow();
+	// Init promoblock
+	cgdpPromoBlockInit();
 };
 
 window.addEventListener('DOMContentLoaded', onDOMContentLoaded);
