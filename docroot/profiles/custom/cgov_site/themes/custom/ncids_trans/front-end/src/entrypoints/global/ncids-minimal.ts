09ddf7e646d45cd1162755359d1e16457c6cb21b
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2023-05-21 12:14:26 -0600

    (#3491) Inline video for home and landing pages
    - added new paragraph type ncids-inline-video for home and landing page
    
    Closes #3491.
    
    removed errant view mode

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/global/ncids-minimal.ts
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/global/ncids-minimal.ts
@@ -6,6 +6,10 @@ import usaFooterInit from '../../lib/components/usa-footer';
 import usaSiteAlertInit from '../../lib/components/usa-site-alert';
 import nciHeaderInit from '../../lib/components/nci-header';
 
+// Temporarily added here to prevent multiple imports of axios.
+// TODO: once axios is made a webpack external, move to home-landing.
+import inlineVideoInit from '../../lib/components/cgdp-inline-video';
+
 document.addEventListener('DOMContentLoaded', () => {
 	/* NCIDS Event Handlers will come first. */
 	// Initialize the Banner/Language toggle.
@@ -19,4 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	//Init the site alert
 	usaSiteAlertInit();
+
+	// Init InlineVideo here, unfortunately
+	inlineVideoInit();
 });
