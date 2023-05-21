f3c63a8078999f4c06ab34d51a07cd8ef770c692
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2023-05-21 12:14:26 -0600

    (#3491) Inline video for home and landing pages
    - added new paragraph type ncids-inline-video for home and landing page
    
    Closes #3491.
    
    removed errant view mode

---
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/lib/components/cgdp-inline-video/index.ts
@@ -0,0 +1,10 @@
+import renderPreviewThumbnails from 'Core/libraries/videoPlayer/flexVideo';
+
+const initialize = (): void => {
+	// init flexVideo
+	renderPreviewThumbnails();
+
+	//start analytics here
+};
+
+export default initialize;
