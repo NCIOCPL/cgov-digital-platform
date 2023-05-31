4d09467eb2f89ff08262c1b82fc861b2f289ffa9
Derek Lescarbeau <derek.lescarbeau@publicissapient.com>
2023-05-31 10:11:15 -0400

    (#3790) Adds NCI Hero analytics
    Edits analytics click helper to also take a string for linkPosition
    
    Closes #3790

---
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/lib/components/nci-hero/__tests__/nci-hero.dom.ts
@@ -0,0 +1,91 @@
+export const nciHeroTestDom = `
+<section class="usa-section usa-section--light usa-section--cgdp-no-top" aria-labelledby="paragraph-851" data-eddl-landing-row>
+	<div class="nci-hero " data-eddl-landing-item="hero">
+		<picture class="nci-hero__image">
+			<source media="(min-width: 1024px)" srcset="/sites/default/files/widescreen.jpg">
+			<source media="(min-width: 880px)" srcset="/sites/default/files/desktop.jpg">
+			<source media="(min-width: 640px)" srcset="/sites/default/files/tablet_large.jpg">
+			<source media="(min-width: 480px)" srcset="/sites/default/files/tablet.jpg">
+			<source media="(min-width: 320px)" srcset="/sites/default/files/mobile_large.jpg">
+			<img loading="lazy" src="/sites/default/files/mobile.jpg" width="640" height="640" alt="">
+		</picture>
+		<div class="nci-hero__cta-container">
+			<div class="nci-hero__cta nci-hero__cta--with-button">
+				<h2 class="nci-hero__cta-tagline" id="851">NCI Hero Title 1</h2>
+				<a data-testid="herobutton1" href="/about-cancer/coping/feelings" aria-label="Internal Button Alt Text" class="nci-hero__cta-button usa-button" data-eddl-landing-item-link-type="Internal">Feelings and Cancer Link 1</a>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="usa-section usa-section--light usa-section--cgdp-no-top" aria-labelledby="paragraph-860" data-eddl-landing-row>
+  <div class="nci-hero nci-hero--with-cta-strip" data-eddl-landing-item="hero">
+    <picture class="nci-hero__image">
+      <source media="(min-width: 1024px)" srcset="/sites/default/files/widescreen.jpg">
+      <source media="(min-width: 880px)" srcset="/sites/default/files/desktop.jpg">
+      <source media="(min-width: 640px)" srcset="/sites/default/files/tablet_large.jpg">
+      <source media="(min-width: 480px)" srcset="/sites/default/files/tablet.jpg">
+      <source media="(min-width: 320px)" srcset="/sites/default/files/mobile_large.jpg">
+			<img loading="lazy" src="/sites/default/files/mobile.jpg" width="640" height="640" alt="">
+		</picture>
+    <div class="nci-hero__cta-container">
+      <div class="nci-hero__cta nci-hero__cta--with-button">
+        <h2 class="nci-hero__cta-tagline" id="860">NCI Hero Title 2</h2>
+				<a data-testid="herobutton2" href="/about-cancer/coping/feelings" aria-label="Internal Button Alt Text" class="nci-hero__cta-button usa-button" data-eddl-landing-item-link-type="Internal">Feelings and Cancer Link 2</a>
+      </div>
+    </div>
+    <div class="nci-hero__nci-cta-strip-container">
+      <div class="grid-container">
+        <ul class="nci-cta-strip">
+    			<li><a data-testid="ctabutton1" href="https://www.google.com" class="usa-button" data-eddl-landing-item-link-type="External">External Link Button</a></li>
+    			<li><a data-testid="ctabutton2" href="/about-cancer/coping/feelings" aria-label="Internal Button Alt Text" class="usa-button" data-eddl-landing-item-link-type="Internal">Feelings and Cancer</a></li>
+    			<li><a data-testid="ctabutton3" href="/test/internal-promo-block-test-article-0" aria-label="Internal Button Alt Text" class="usa-button" data-eddl-landing-item-link-type="Internal">[Guide Card] Browser Title</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="usa-section usa-section--light usa-section--cgdp-no-top" aria-labelledby="paragraph-869" data-eddl-landing-row>
+  <div class="nci-hero nci-hero--with-cta-strip" data-eddl-landing-item="hero">
+    <picture class="nci-hero__image">
+      <source media="(min-width: 1024px)" srcset="/sites/default/files/widescreen.jpg">
+      <source media="(min-width: 880px)" srcset="/sites/default/files/desktop.jpg">
+      <source media="(min-width: 640px)" srcset="/sites/default/files/tablet_large.jpg">
+      <source media="(min-width: 480px)" srcset="/sites/default/files/tablet.jpg">
+      <source media="(min-width: 320px)" srcset="/sites/default/files/mobile_large.jpg">
+			<img loading="lazy" src="/sites/default/files/mobile.jpg" width="640" height="640" alt="">
+		</picture>
+    <div class="nci-hero__cta-container">
+      <div class="nci-hero__cta">
+        <h2 class="nci-hero__cta-tagline" id="869">NCI Hero Title 3</h2>
+      </div>
+    </div>
+    <div class="nci-hero__nci-cta-strip-container">
+      <div class="grid-container">
+        <ul class="nci-cta-strip">
+    			<li><a data-testid="justctabutton1" href="https://www.google.com" class="usa-button" data-eddl-landing-item-link-type="External">External Link Button 3</a></li>
+    			<li><a data-testid="justctabutton2" href="/about-cancer/coping/feelings" aria-label="Internal Button Alt Text" class="usa-button" data-eddl-landing-item-link-type="Internal">Feelings and Cancer 3</a></li>
+    			<li><a data-testid="justctabutton3" href="/test/internal-promo-block-test-article-0" aria-label="Internal Button Alt Text" class="usa-button" data-eddl-landing-item-link-type="Internal">[Guide Card] Browser Title 3</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="usa-section usa-section--light usa-section--cgdp-no-top" aria-labelledby="paragraph-869" data-eddl-landing-row>
+  <div class="nci-hero" data-eddl-landing-item="hero">
+    <picture class="nci-hero__image">
+      <source media="(min-width: 1024px)" srcset="/sites/default/files/widescreen.jpg">
+      <source media="(min-width: 880px)" srcset="/sites/default/files/desktop.jpg">
+      <source media="(min-width: 640px)" srcset="/sites/default/files/tablet_large.jpg">
+      <source media="(min-width: 480px)" srcset="/sites/default/files/tablet.jpg">
+      <source media="(min-width: 320px)" srcset="/sites/default/files/mobile_large.jpg">
+			<img loading="lazy" src="/sites/default/files/mobile.jpg" width="640" height="640" alt="">
+		</picture>
+    <div class="nci-hero__cta-container">
+      <div class="nci-hero__cta">
+        <h2 class="nci-hero__cta-tagline" id="869">NCI Hero Title 4</h2>
+				<a data-testid="herobutton4" href="/about-cancer/coping/feelings" aria-label="Internal Button Alt Text" class="nci-hero__cta-button usa-button" data-eddl-landing-item-link-type="Internal">Feelings and Cancer Link 4</a>
+      </div>
+    </div>
+  </div>
+</section>
+`;
---
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/lib/components/nci-hero/nci-hero.ts
@@ -0,0 +1,116 @@
+import { landingClickTracker } from '../../core/analytics/landing-page-contents-helper';
+
+/**
+ * Gets the title of the Hero (CTA Tagline)
+ * @param {HTMLElement} nciHeroElement - The NCI Hero Element
+ */
+const getHeroTitle = (nciHeroElement: HTMLElement): string =>
+	nciHeroElement
+		.querySelector('.nci-hero__cta-container')
+		?.querySelector('.nci-hero__cta')
+		?.querySelector('.nci-hero__cta-tagline')?.textContent || '_ERROR_';
+
+/**
+ * Gets the Variant of the Hero (With(out) CTA Strip/Button)
+ * Confirms whether Hero has a CTA Strip, CTA Button, or Both
+ * @param {HTMLElement} nciHeroElement - The NCI Hero Element
+ */
+const getHeroVariant = (nciHeroElement: HTMLElement): string => {
+	const heroCtaHasButton = Boolean(
+		nciHeroElement
+			.querySelector('.nci-hero__cta-container')
+			?.querySelector('.nci-hero__cta--with-button')
+	);
+	const heroHasCtaStrip = Boolean(
+		nciHeroElement.classList.contains('nci-hero--with-cta-strip')
+	);
+	if (heroCtaHasButton && heroHasCtaStrip) {
+		return 'Button with CTA Strip';
+	} else if (heroCtaHasButton) {
+		return 'Button with no CTA Strip';
+	} else if (heroHasCtaStrip) {
+		return 'No Button with CTA Strip';
+	} else {
+		return '_ERROR_';
+	}
+};
+
+/**
+ * Checks the Link Area of the Hero (Primary/Secondary Button)
+ * Primary Button accompanies the CTA (returns True)
+ * Secondary Buttons are in the CTA Strip (returns False)
+ * @param {HTMLElement} linkTarget - Clicked anchor element.
+ */
+const checkHeroLinkArea = (linkTarget: HTMLElement): boolean =>
+	Boolean(linkTarget.closest('.nci-hero__cta'));
+
+/**
+ * Gets the Link Position of the clicked link of the Hero
+ * Positions: (1,1) for CTA button, (2,[index in CTA string]) for CTA Strip buttons
+ * @param {HTMLElement} linkTarget - Clicked anchor element.
+ */
+const getHeroLinkPosition = (linkTarget: HTMLElement): string => {
+	// The CTA Strip
+	const ctaStrip = linkTarget.closest('.nci-cta-strip');
+	// The List Element (Parent of Anchor)
+	const anchorElementParent = linkTarget.closest('li');
+	// Array of CTA Strip Elements
+	const ctaStripElementArray = Array.from(ctaStrip?.children || []);
+
+	if (ctaStripElementArray.length < 1) {
+		return '1-1';
+	} else if (anchorElementParent) {
+		return `2-${ctaStripElementArray.indexOf(anchorElementParent) + 1}`;
+	} else {
+		return '_ERROR_';
+	}
+};
+
+/**
+ * Click handler for the language toggle click.
+ * @param nciHero the hero element.
+ * @param totalLinks the total numbe of links in the hero.
+ */
+const nciHeroLinkClickHandler =
+	(nciHero: HTMLElement, totalLinks: number) => (evt: Event) => {
+		const target = evt.currentTarget as HTMLElement;
+
+		landingClickTracker(
+			target,
+			'Hero', // linkName
+			1, // rowItems
+			1, // rowItemsIndex
+			'Hero', // componentType
+			'Light', // componentTheme
+			getHeroVariant(nciHero) as string, // componentVariant
+			getHeroTitle(nciHero) as string, // title
+			target.dataset.eddlLandingItemLinkType || '_ERROR_', // linkType
+			!target.textContent ? '_ERROR_' : target.textContent.trim(), // linkText
+			checkHeroLinkArea(target) ? 'Primary Button' : 'Secondary Button', // linkArea
+			totalLinks, // totalLinks
+			getHeroLinkPosition(target) // linkPosition
+		);
+	};
+
+/**
+ * Wires up a hero for the cdgp requirements.
+ */
+const initialize = () => {
+	const nciHero = document.querySelectorAll(
+		'[data-eddl-landing-item="hero"]'
+	) as NodeListOf<HTMLElement>;
+	if (!nciHero.length) return;
+
+	nciHero.forEach((hero) => {
+		const linkButton = hero.querySelectorAll('a');
+		if (!linkButton.length) return;
+		linkButton.forEach((link) =>
+			link.addEventListener(
+				'click',
+				nciHeroLinkClickHandler(hero, linkButton.length)
+			)
+		);
+	});
+};
+
+export default initialize;
