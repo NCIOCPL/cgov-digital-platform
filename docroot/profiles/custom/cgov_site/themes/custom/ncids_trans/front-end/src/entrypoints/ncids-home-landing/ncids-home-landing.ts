040f1b063ef0c19aa292d3b1857207e0021402fa
Derek Lescarbeau <derek.lescarbeau@publicissapient.com>
2023-05-31 10:11:15 -0400

    (#3790) Adds NCI Hero analytics
    Edits analytics click helper to also take a string for linkPosition
    
    Closes #3790

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/ncids-home-landing/ncids-home-landing.ts
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/ncids-home-landing/ncids-home-landing.ts
@@ -4,6 +4,7 @@ import cgdpFeatureCardRow from '../../lib/components/cgdp-feature-card-row';
 import cgdpGuideCardRow from '../../lib/components/cgdp-guide-card-row';
 import cgdpPromoBlockInit from '../../lib/components/nci-promo-block';
 import nciCtaStrip from '../../lib/components/nci-cta-strip';
+import nciHeroInit from '../../lib/components/nci-hero';
 
 const onDOMContentLoaded = () => {
 	// Init feature cards
@@ -14,6 +15,8 @@ const onDOMContentLoaded = () => {
 	cgdpPromoBlockInit();
 	// Init cta
 	nciCtaStrip();
+	// Init NCI Hero
+	nciHeroInit();
 };
 
 window.addEventListener('DOMContentLoaded', onDOMContentLoaded);
---
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/lib/components/nci-hero/index.ts
@@ -0,0 +1,3 @@
+// Passthrough for cleaner imports.
+import initialize from './nci-hero';
+export default initialize;
