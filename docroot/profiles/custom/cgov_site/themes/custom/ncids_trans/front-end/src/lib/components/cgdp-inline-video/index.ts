f30d72c57786144dfd0a65873f9489b87fe10226
Olivia Tharp <olivia.tharp@nih.gov>
2023-06-05 12:42:34 -0500

    (#3860) Wire up inline video analytics
    Closes #3860

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/lib/components/cgdp-inline-video/index.ts
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/lib/components/cgdp-inline-video/index.ts
@@ -1,10 +1,12 @@
 import renderPreviewThumbnails from 'Core/libraries/videoPlayer/flexVideo';
+import initVideoAnalytics from './cgdp-inline-video-analytics';
 
 const initialize = (): void => {
 	// init flexVideo
 	renderPreviewThumbnails();
 
 	//start analytics here
+	initVideoAnalytics();
 };
 
 export default initialize;
