ec9829ad527d494a8e258f7e59ea1a165d883877
Olivia Tharp <olivia.tharp@nih.gov>
2023-06-05 12:42:34 -0500

    (#3860) Wire up inline video analytics
    Closes #3860

---
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/lib/components/cgdp-inline-video/cgdp-inline-video-analytics.ts
@@ -0,0 +1,40 @@
+import { landingClickTracker } from '../../core/analytics/landing-page-contents-helper';
+
+/**
+ * Handle play button on click handler.
+ */
+const playVideoClickHandler = (evt: Event): void => {
+	const video = evt.currentTarget as HTMLElement;
+
+	landingClickTracker(
+		video,
+		'InlineVideo',
+		1,
+		1,
+		'Inline Video',
+		'N/A',
+		'Standard YouTube Video',
+		video.dataset.videoTitle || '_ERROR_',
+		'Video Player',
+		'Play',
+		'Play',
+		1,
+		'Play'
+	);
+};
+
+/**
+ * Wire up component per cgdp requirements.
+ */
+const initVideoAnalytics = (): void => {
+	const videoContainers = document.querySelectorAll(
+		'[data-eddl-landing-item="inline_video"]'
+	);
+
+	videoContainers.forEach((videoContainer) => {
+		const video = videoContainer.querySelector('.cgdp-video');
+		video?.addEventListener('click', playVideoClickHandler);
+	});
+};
+
+export default initVideoAnalytics;
