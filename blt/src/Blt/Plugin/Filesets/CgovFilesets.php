3dd0a0a9f9703c7b5e5647d5ba1314d5032c534c
Naveen Sunkara <navsunka@users.noreply.github.com>
2023-07-31 21:38:49 -0400

    (#3927) Enable PHP Linting in BLT
    Closes #3927

---
+++ blt/src/Blt/Plugin/Filesets/CgovFilesets.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Cgov\Blt\Plugin\Filesets;
+
+// Do not remove this, even though it appears to be unused.
+// @codingStandardsIgnoreLine
+use Acquia\Blt\Annotations\Fileset;
+use Acquia\Blt\Robo\Config\ConfigAwareTrait;
+use Robo\Contract\ConfigAwareInterface;
+use Acquia\Blt\Robo\Filesets\FilesetsTrait;
+
+/**
+ * Class Filesets.
+ *
+ * Each fileset in this class should be tagged with a @fileset annotation and
+ * should return \Symfony\Component\Finder\Finder object.
+ *
+ * @package Acquia\Blt\Custom
+ * @see \Acquia\Blt\Robo\Filesets\Filesets
+ */
+class CgovFilesets implements ConfigAwareInterface {
+  use ConfigAwareTrait;
+  use FilesetsTrait;
+
+  /**
+   * Get fileset php custom profiles.
+   *
+   * @fileset(id="files.php.cgov.profiles")
+   *
+   * @return \Symfony\Component\Finder\Finder
+   *   Finder.
+   */
+  public function getFilesetPhpCustomProfiles() {
+    $finder = $this->getPhpFilesetFinder();
+    $finder->in([$this->getConfigValue('docroot') . '/profiles/custom']);
+    return $finder;
+  }
+
+}
