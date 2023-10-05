1bc4414b43b48ca28fcec400db7b107b86e4abe3
Naveen Sunkara <navsunka@WKMZT17FF301.global.publicisgroupe.net>
2023-10-05 11:35:08 -0500

    (#4086) Added patch for table properties inside ckeditor5
    - Fixed CSS prefix selector
    - Added table properties patch to work with d9
    
    Closes #4086

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/postcss.config.js
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/postcss.config.js
@@ -1,38 +1,34 @@
+const postcssPrefixSelector = require('postcss-prefix-selector');
 module.exports = {
+
 	plugins: [
-		[
-			'postcss-prefix-selector',
-			{
-				prefix: '.cgdpl',
-				transform(prefix, selector, prefixedSelector, filepath) {
-					if (filepath.match(/-legacy\.scss$/)) {
-						return prefixedSelector;
-					} else if (filepath.match(/r4r-app\/.*\.s?css$/)) {
-						// Hack for legacy R4R app
+    postcssPrefixSelector({
+			prefix: '.cgdpl',
+			transform(prefix, selector, prefixedSelector, filepath) {
+				if (filepath.match(/-legacy\.scss$/)) {
+					return prefixedSelector;
+				} else if (filepath.match(/r4r-app\/.*\.s?css$/)) {
+					// Hack for legacy R4R app
+					return prefixedSelector;
+				}
+				return selector;
+			},
+    }),
+    postcssPrefixSelector({
+			prefix: '.ck-content',
+			transform(prefix, selector, prefixedSelector, filepath) {
+				if (filepath.match(/ckeditor\.scss$/)) {
+					if (selector === '.ck-content') {
+						return selector;
+					} else if (selector.startsWith('.contentzone ')) {
+						return selector.replace(/^\.contentzone/, '.ck-content');
+					} else {
 						return prefixedSelector;
 					}
-					return selector;
-				},
+				}
+				return selector;
 			},
-		],
-		[
-			'postcss-prefix-selector',
-			{
-				prefix: '.ck-content',
-				transform(prefix, selector, prefixedSelector, filepath) {
-					if (filepath.match(/ckeditor\.scss$/)) {
-						if (selector === '.ck-content') {
-							return selector;
-						} else if (selector.startsWith('.contentzone ')) {
-							return selector.replace(/^\.contentzone/, '.ck-content');
-						} else {
-							return prefixedSelector;
-						}
-					}
-					return selector;
-				},
-			},
-		],
+    }),
 		['autoprefixer'],
-	],
+  ],
 };
