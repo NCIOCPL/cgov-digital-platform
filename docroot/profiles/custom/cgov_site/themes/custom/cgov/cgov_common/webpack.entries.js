0867ed128618e3a78298e7d87fbac5c897fe99f3
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2023-11-14 16:46:05 -0700

    (#4147) Update twitter logo in svgs sprite
    Closes #4147
    
    - update twitter logo svg for use in sprites
    - update aria labels to reflect X instead of twitter
    - removed custom tweet from page options and init
    
    - removed unused R4R from cgov common referencing old twitter logos
    - removed unused social items from cgov common page options
    - removed blue, purple & green theme variants
    - changed twitter-blue to twitter-x-blue for news& events landing

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/webpack.entries.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/cgov_common/webpack.entries.js
@@ -36,9 +36,6 @@ module.exports = {
     "src/entrypoints/blogSeries/BlogSeries.js"
   ),
   Biography: path.resolve(__dirname, "src/entrypoints/biography/Biography.js"),
-  Purple: path.resolve(__dirname, "src/variants/purple/entrypoints/Purple.js"),
-  Blue: path.resolve(__dirname, "src/variants/blue/entrypoints/Blue.js"),
-  Green: path.resolve(__dirname, "src/variants/green/entrypoints/Green.js"),
   Connect: path.resolve(__dirname, "src/variants/connect/entrypoints/Connect.js"),
   CTHP: path.resolve(__dirname, "src/entrypoints/cthp/CTHP.js"),
   Event: path.resolve(__dirname, "src/entrypoints/event/Event.js"),
