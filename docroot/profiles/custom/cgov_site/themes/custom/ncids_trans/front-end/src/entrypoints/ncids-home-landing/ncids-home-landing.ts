cb0dbd740173a9aef7ce3dd6446c9b6fedaf7bf4
Dev Rana <dev-rana-publicis@users.noreply.github.com>
2023-12-18 16:37:30 -0500

    (#4070) NCIDS Dynamic List
    - Added pageRowCols and pageRowColIndex to handle container items that
       are in columns (NCDIS 2 Column Container).
    - This commit also adds pageRowCols and pageRowColIndex to CGDP Raw
       HTML.
    
    Closes #4070

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/ncids-home-landing/ncids-home-landing.ts
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/ncids-home-landing/ncids-home-landing.ts
@@ -6,6 +6,7 @@ import cgdpPromoBlockInit from '../../lib/components/nci-promo-block';
 import cgdpLandingRawHtml from '../../lib/components/cgdp-landing-raw-html';
 import nciCtaStrip from '../../lib/components/nci-cta-strip';
 import nciHeroInit from '../../lib/components/nci-hero';
+import cgdpDynamicListInit from '../../lib/components/cgdp-dynamic-list';
 
 const onDOMContentLoaded = () => {
 	// Init feature cards
@@ -20,6 +21,8 @@ const onDOMContentLoaded = () => {
 	nciCtaStrip();
 	// Init hero
 	nciHeroInit();
+	// Init collections
+	cgdpDynamicListInit();
 };
 
 window.addEventListener('DOMContentLoaded', onDOMContentLoaded);
