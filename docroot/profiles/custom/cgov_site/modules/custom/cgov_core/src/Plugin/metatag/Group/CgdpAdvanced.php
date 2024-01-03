222c7d1337274c38f032ea7dd37001505a9657b2
Naveen Sunkara <navsunka@WKMZT17FF301.global.publicisgroupe.net>
2024-01-03 10:19:21 -0600

    (#3912) Added new metatag template for all content types
    - updated yaml to match production as the current content has invalid value.
    - including summary_type along with svpc flag due to change in requirment.
    - added default value for media types infographic and video.
    
    Closes #3912

---
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_core/src/Plugin/metatag/Group/CgdpAdvanced.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\cgov_core\Plugin\metatag\Group;
+
+use Drupal\metatag\Plugin\metatag\Group\GroupBase;
+
+/**
+ * Provides a plugin for the 'Cgdp Additional Tags' meta tag group.
+ *
+ * @MetatagGroup(
+ *   id = "cgdpAdvanced_advanced",
+ *   label = @Translation("Cgdp Additional Tags"),
+ *   description = @Translation("These tags are not part of the Metadata Element Set but may be useful for certain scenarios."),
+ *   weight = 4,
+ * )
+ */
+class CgdpAdvanced extends GroupBase {
+  // Just a placeholder class for a plugin.
+}
