60a4c4429c0d3623cd53ea8e127c5f33178f9a3a
Naveen Sunkara <navsunka@WKMZT17FF301.global.publicisgroupe.net>
2024-01-03 10:19:21 -0600

    (#3912) Added new metatag template for all content types
    - updated yaml to match production as the current content has invalid value.
    - including summary_type along with svpc flag due to change in requirment.
    - added default value for media types infographic and video.
    
    Closes #3912

---
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_core/src/Plugin/metatag/Tag/CgdpTemplate.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\cgov_core\Plugin\metatag\Tag;
+
+use Drupal\metatag\Plugin\metatag\Tag\MetaNameBase;
+
+/**
+ * Cgdp template.
+ *
+ * @MetatagTag(
+ *   id = "cgdp_template",
+ *   label = @Translation("Cgdp Template"),
+ *   description = @Translation("Cgdp Template Meta Properties."),
+ *   name = "cgdp.template",
+ *   group = "cgdpAdvanced_advanced",
+ *   weight = 6,
+ *   type = "label",
+ *   secure = FALSE,
+ *   multiple = FALSE
+ * )
+ */
+class CgdpTemplate extends MetaNameBase {
+  // Just a placeholder class for a plugin.
+}
