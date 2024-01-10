bfd7c6a1e5382afdc783b23f19356ef21586f202
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2024-01-10 14:42:51 -0500

    (#3802) Updated @nciocpl packages to not be local
    - Needed to add override for prettier 2.5.1 as that was the version installed,
      but an install of the non-local files triggered a bump to 2.8.8 which left
      us with 1336 issues.
    - Bumped npm in docker container and workflow as we need a newer 8 to support
      overrides in package.json.
    - Setup github actions workflow to define the github npm registry and nciocpl
      scope.
    - added some logging information to front-end package installs.
    - add npmrc for cgov_common to use legacy peer deps because the newer version
      of npm was complaining about missing deps and `npm ci` would fail..
    
    NOTE: I left r4r as a legacy dep as it really needs to be turned into a generic
    app module.
    
    Closes #3802

--- scripts/blt/custom/frontend-reqs.sh
+++ scripts/blt/custom/frontend-reqs.sh
@@ -5,11 +5,13 @@ set -e
 ## This assumes working directory is docroot/profile/custom/cgov_site/themes/custom
 
 ## Install ncids_trans packages
+echo "Installing ncids_trans packages"
 pushd ncids_trans/front-end
 npm ci
 popd
 
 ## Install cgov_common packages
+echo "Installing cgov_common packages"
 pushd cgov/cgov_common
 npm ci
 popd
