b789af2c588f3041432f196ffbbd2435a9ce07fd
Dev Rana <dev-rana-publicis@users.noreply.github.com>
2024-03-27 12:56:10 -0400

    (#4217) Add Accordion to Cancer Types Page
    Closes #4217

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/addl-cancer-types-landing-page/addl-cancer-types-landing-page.ts
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/addl-cancer-types-landing-page/addl-cancer-types-landing-page.ts
@@ -1,1 +1,7 @@
 import './addl-cancer-types-landing-page.scss';
+import cancerTypesAZList from './components/cgdp-cancer-type-az-list';
+
+const onDOMContentLoaded = () => {
+	cancerTypesAZList();
+};
+window.addEventListener('DOMContentLoaded', onDOMContentLoaded);
