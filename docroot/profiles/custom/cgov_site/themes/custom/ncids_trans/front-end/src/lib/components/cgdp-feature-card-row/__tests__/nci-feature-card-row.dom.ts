a247016759c7fe4ef69332f78a5b842cbbbb691e
Dev Rana <dev-rana-publicis@users.noreply.github.com>
2024-06-17 12:52:51 -0400

    (#4367) Change Feature Card Image to 16x9
    Closes #4367

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/lib/components/cgdp-feature-card-row/__tests__/nci-feature-card-row.dom.ts
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/lib/components/cgdp-feature-card-row/__tests__/nci-feature-card-row.dom.ts
@@ -10,7 +10,6 @@ export const nciFeatureCardRowDom = () => {
 					<li class="nci-card tablet-lg:grid-col-4">
 						<a href="https://www.google.com" aria-labelledby="paragraph-743" data-eddl-landing-item="feature_card" data-eddl-landing-item-link-type="External">
 							<picture class="nci-card__image">
-								<source media="(min-width: 880px)" srcset="/sites/default/files/styles/ncids_featured_4x3/public/cgov_image/ncids_promo_art_4x3/2023-05/override-placeholder-4x3.png?itok=W4PWeoBj">
 								<img loading="lazy" src="/sites/default/files/styles/ncids_featured_16x9/public/cgov_image/ncids_promo_art_16x9/2023-05/override-placeholder-16x9.png?itok=r3dIgZ_u" width="1408" height="792" alt="Override Placeholder">
 							</picture>
 							<div class="nci-card__body">
@@ -22,7 +21,6 @@ export const nciFeatureCardRowDom = () => {
 					<li class="nci-card tablet-lg:grid-col-4">
 						<a href="https://www.google.com" aria-labelledby="paragraph-744" data-eddl-landing-item="feature_card" data-eddl-landing-item-link-type="Internal">
 							<picture class="nci-card__image">
-								<source media="(min-width: 880px)" srcset="/sites/default/files/styles/ncids_featured_4x3/public/cgov_image/media_image/2023-05/feature_card_test_cat_override_source.jpeg?h=17a8321b&amp;itok=qwM1JqcH">
 								<img loading="lazy" src="/sites/default/files/styles/ncids_featured_16x9/public/cgov_image/media_image/2023-05/feature_card_test_cat_override_source.jpeg?h=5238be8d&amp;itok=jYGPlXTe" width="1408" height="792" alt="Feature Card Cat">
 							</picture>
 							<div class="nci-card__body">
@@ -34,7 +32,6 @@ export const nciFeatureCardRowDom = () => {
 					<li class="nci-card tablet-lg:grid-col-4">
 						<a href="https://www.google.com" aria-labelledby="paragraph-745" data-eddl-landing-item="feature_card" data-eddl-landing-item-link-type="Media">
 							<picture class="nci-card__image">
-								<source media="(min-width: 880px)" srcset="/sites/default/files/styles/ncids_featured_4x3/public/cgov_image/media_image/2023-05/feature_card_test_cat_override_source.jpeg?h=17a8321b&amp;itok=qwM1JqcH">
 								<img loading="lazy" src="/sites/default/files/styles/ncids_featured_16x9/public/cgov_image/ncids_promo_art_16x9/2023-05/feature_card_test_cat_16x9_override.jpeg?itok=g9HIJfvz" width="1408" height="792" alt="16x9 Override Image">
 							</picture>
 							<div class="nci-card__body">
