43254faed4981f64a3688fcba8442f7ea4751422
Dev Rana <dev-rana-publicis@users.noreply.github.com>
2024-09-12 15:32:23 -0400

    (#4389) NCIDS Flag Card Group
    Closes #4389

---
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/lib/components/cgdp-flag-card-group/__tests__/cgdp-flag-card-group.test.ts
@@ -0,0 +1,256 @@
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
+
+import { fireEvent, screen } from '@testing-library/dom';
+
+import * as eddlUtil from '../../../core/analytics/eddl-util';
+
+import cgdpFlagCardInit from '../cgdp-flag-card-group';
+import { cgdpFlagCardGroupDom } from './cgdp-flag-card-group.dom';
+import { cgdpFlagCardGroupBadDom } from './cgdp-flag-card-group.bad.dom';
+
+jest.mock('../../../core/analytics/eddl-util');
+
+describe('CGDP Flag Cards', () => {
+	beforeEach(() => {
+		window.print = jest.fn();
+		document.head.insertAdjacentHTML(
+			'beforeend',
+			`
+			<meta name="dcterms.type" content="cgvMiniLanding">
+			<meta name="cgdp.template" content="ncids_without_title">
+			`
+		);
+	});
+	afterEach(() => {
+		// Hack to clean out the dom.
+		document.getElementsByTagName('body')[0].innerHTML = '';
+		document.head.innerHTML = '';
+		jest.resetAllMocks();
+	});
+
+	it('does not blow up when cards do not exist', () => {
+		const dom = `<section class="usa-section" data-eddl-landing-row></div>`;
+
+		// Lets make a spy to ensure that trackOther is called correctly
+		const spy = jest.spyOn(eddlUtil, 'trackOther');
+
+		// Inject the HTML into the dom.
+		document.body.insertAdjacentHTML('beforeend', dom);
+
+		// Create the JS
+		cgdpFlagCardInit();
+
+		expect(spy).not.toHaveBeenCalled();
+	});
+
+	it('internal feature card', () => {
+		const dom = cgdpFlagCardGroupDom();
+
+		// Lets make a spy to ensure that trackOther is called correctly
+		const trackOtherSpy = jest.spyOn(eddlUtil, 'trackOther');
+
+		// Inject the HTML into the dom.
+		document.body.insertAdjacentHTML('beforeend', dom.outerHTML);
+
+		// Create the JS
+		cgdpFlagCardInit();
+
+		const card = screen.getAllByRole('link');
+
+		fireEvent.click(card[0]);
+
+		expect(trackOtherSpy).toHaveBeenCalledWith(
+			'MLP:FlagCard:LinkClick',
+			'MLP:FlagCard:LinkClick',
+			{
+				location: 'Body',
+				pageType: 'cgvMiniLanding',
+				pageTemplate: 'ncids_without_title',
+				pageRows: 2,
+				pageRowIndex: 1,
+				pageRowCols: 0,
+				pageRowColIndex: 0,
+				containerItems: 5,
+				containerItemIndex: 1,
+				componentType: 'Flag Card',
+				componentTheme: 'Not Defined',
+				componentVariant: 'Not Defined',
+				title: 'The Optional Flag Card Group Heading',
+				linkType: 'Internal',
+				linkText: 'About Cancer',
+				linkArea: 'Card',
+				totalLinks: 1,
+				linkPosition: 1,
+			}
+		);
+	});
+
+	it('external feature card', () => {
+		const dom = cgdpFlagCardGroupDom();
+
+		// Lets make a spy to ensure that trackOther is called correctly
+		const trackOtherSpy = jest.spyOn(eddlUtil, 'trackOther');
+
+		// Inject the HTML into the dom.
+		document.body.insertAdjacentHTML('beforeend', dom.outerHTML);
+
+		// Create the JS
+		cgdpFlagCardInit();
+
+		const card = screen.getAllByRole('link');
+
+		fireEvent.click(card[1]);
+
+		expect(trackOtherSpy).toHaveBeenCalledWith(
+			'MLP:FlagCard:LinkClick',
+			'MLP:FlagCard:LinkClick',
+			{
+				location: 'Body',
+				pageType: 'cgvMiniLanding',
+				pageTemplate: 'ncids_without_title',
+				pageRows: 2,
+				pageRowIndex: 1,
+				pageRowCols: 0,
+				pageRowColIndex: 0,
+				containerItems: 5,
+				containerItemIndex: 2,
+				componentType: 'Flag Card',
+				componentTheme: 'Not Defined',
+				componentVariant: 'Not Defined',
+				title: 'The Optional Flag Card Group Heading',
+				linkType: 'External',
+				linkText: 'External Card Title',
+				linkArea: 'Card',
+				totalLinks: 1,
+				linkPosition: 1,
+			}
+		);
+	});
+
+	it('multimedia feature card', () => {
+		const dom = cgdpFlagCardGroupDom();
+
+		// Lets make a spy to ensure that trackOther is called correctly
+		const trackOtherSpy = jest.spyOn(eddlUtil, 'trackOther');
+
+		// Inject the HTML into the dom.
+		document.body.insertAdjacentHTML('beforeend', dom.outerHTML);
+
+		// Create the JS
+		cgdpFlagCardInit();
+
+		const card = screen.getAllByRole('link');
+
+		fireEvent.click(card[2]);
+
+		expect(trackOtherSpy).toHaveBeenCalledWith(
+			'MLP:FlagCard:LinkClick',
+			'MLP:FlagCard:LinkClick',
+			{
+				location: 'Body',
+				pageType: 'cgvMiniLanding',
+				pageTemplate: 'ncids_without_title',
+				pageRows: 2,
+				pageRowIndex: 1,
+				pageRowCols: 0,
+				pageRowColIndex: 0,
+				containerItems: 5,
+				containerItemIndex: 3,
+				componentType: 'Flag Card',
+				componentTheme: 'Not Defined',
+				componentVariant: 'Not Defined',
+				title: 'The Optional Flag Card Group Heading',
+				linkType: 'Media',
+				linkText: 'Media Card Title',
+				linkArea: 'Card',
+				totalLinks: 1,
+				linkPosition: 1,
+			}
+		);
+	});
+
+	it('bad DOM flag card', () => {
+		const dom = cgdpFlagCardGroupBadDom();
+
+		// Lets make a spy to ensure that trackOther is called correctly
+		const trackOtherSpy = jest.spyOn(eddlUtil, 'trackOther');
+
+		// Inject the HTML into the dom.
+		document.body.insertAdjacentHTML('beforeend', dom.outerHTML);
+
+		// Create the JS
+		cgdpFlagCardInit();
+
+		const card = screen.getAllByRole('link');
+
+		fireEvent.click(card[2]);
+
+		expect(trackOtherSpy).toHaveBeenCalledWith(
+			'MLP:FlagCard:LinkClick',
+			'MLP:FlagCard:LinkClick',
+			{
+				location: 'Body',
+				pageType: 'cgvMiniLanding',
+				pageTemplate: 'ncids_without_title',
+				pageRows: 0,
+				pageRowIndex: '_ERROR_',
+				pageRowCols: 0,
+				pageRowColIndex: 0,
+				containerItems: 0,
+				containerItemIndex: 0,
+				componentType: 'Flag Card',
+				componentTheme: 'Not Defined',
+				componentVariant: 'Not Defined',
+				title: 'Not Defined',
+				linkType: '_ERROR_',
+				linkText: '_ERROR_',
+				linkArea: 'Card',
+				totalLinks: 1,
+				linkPosition: 1,
+			}
+		);
+	});
+
+	it('optional heading on flag card group', () => {
+		const dom = cgdpFlagCardGroupDom();
+
+		// Lets make a spy to ensure that trackOther is called correctly
+		const trackOtherSpy = jest.spyOn(eddlUtil, 'trackOther');
+
+		// Inject the HTML into the dom.
+		document.body.insertAdjacentHTML('beforeend', dom.outerHTML);
+
+		// Create the JS
+		cgdpFlagCardInit();
+
+		const card = screen.getAllByRole('link');
+
+		fireEvent.click(card[6]);
+
+		expect(trackOtherSpy).toHaveBeenCalledWith(
+			'MLP:FlagCard:LinkClick',
+			'MLP:FlagCard:LinkClick',
+			{
+				location: 'Body',
+				pageType: 'cgvMiniLanding',
+				pageTemplate: 'ncids_without_title',
+				pageRows: 2,
+				pageRowIndex: 2,
+				pageRowCols: 0,
+				pageRowColIndex: 0,
+				containerItems: 5,
+				containerItemIndex: 2,
+				componentType: 'Flag Card',
+				componentTheme: 'Not Defined',
+				componentVariant: 'Not Defined',
+				title: 'Not Defined',
+				linkType: 'External',
+				linkText: 'External Card Title',
+				linkArea: 'Card',
+				totalLinks: 1,
+				linkPosition: 1,
+			}
+		);
+	});
+});
---
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/lib/components/cgdp-flag-card-group/cgdp-flag-card-group.ts
@@ -0,0 +1,86 @@
+import { landingClickTracker } from '../../core/analytics/landing-page-contents-helper';
+
+/**
+ * Gets text content by selector.
+ * @param {HTMLElement} link - Selected card.
+ * @param {string} selector - Selector for query.
+ */
+const getText = (link: HTMLElement, selector: string): string => {
+	const element = link.querySelector(selector);
+	const text = element?.textContent?.trim();
+
+	return text || '_ERROR_';
+};
+
+/**
+ * Gets the title of the flag card group.
+ * @param {HTMLElement} link - selected card.
+ */
+const getTitle = (link: HTMLElement): string => {
+	const parentSection = link.closest('.usa-section');
+	const title = parentSection?.querySelector('.nci-heading-h2')?.textContent;
+	return title?.trim() || 'Not Defined';
+};
+
+/**
+ * Feature card on click handler.
+ * @param {number} containerItems - Number of cards in row.
+ * @param {number} containerItemIndex - Index of card selected in row.
+ */
+const flagCardLinkClickHandler =
+	(containerItems: number, containerItemIndex: number | '_ERROR_') =>
+	(evt: Event): void => {
+		const link = evt.currentTarget as HTMLElement;
+		landingClickTracker(
+			link,
+			'FlagCard',
+			containerItems,
+			containerItemIndex,
+			'Flag Card',
+			'Not Defined',
+			'Not Defined',
+			getTitle(link),
+			link.dataset.eddlLandingItemLinkType || '_ERROR_',
+			getText(link, '.cgdp-flag-card__title').trim(),
+			'Card',
+			1,
+			1
+		);
+	};
+
+/**
+ * Gathers data to pass through to event listener.
+ * @param {HTMLElement} card - The Feature Card element.
+ */
+const flagCardHelper = (card: HTMLElement): void => {
+	const parentRowContainer = card.closest('[data-eddl-landing-row]');
+	const allCards = parentRowContainer
+		? Array.from(
+				parentRowContainer.querySelectorAll('[data-eddl-landing-item]')
+		  )
+		: [];
+	const containerItemIndex = allCards.indexOf(card) + 1;
+	const containerItems = allCards.length;
+
+	card.addEventListener(
+		'click',
+		flagCardLinkClickHandler(containerItems, containerItemIndex)
+	);
+};
+
+/**
+ * Wire up component per cgdp requirements.
+ */
+const initialize = (): void => {
+	const flagCardEls = document.querySelectorAll(
+		'[data-eddl-landing-item="flag_card"]'
+	);
+	if (!flagCardEls.length) return;
+
+	flagCardEls.forEach((flagCardEl) => {
+		const flagCard = flagCardEl as HTMLElement;
+		flagCardHelper(flagCard);
+	});
+};
+
+export default initialize;
