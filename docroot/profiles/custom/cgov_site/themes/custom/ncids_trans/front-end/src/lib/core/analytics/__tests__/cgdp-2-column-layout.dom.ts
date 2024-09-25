d1ac2659e215af9dc847f1151fb7b1500d7a4ef1
Dev Rana <dev-rana-publicis@users.noreply.github.com>
2024-09-25 17:21:35 -0400

    (#4400) NCIDS 2 Column Layout
    Closes #4400

---
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/lib/core/analytics/__tests__/cgdp-2-column-layout.dom.ts
@@ -0,0 +1,32 @@
+export const cgdp2ColumnLayoutDom = `
+<div class="usa-section" data-eddl-landing-row>
+	<div class="grid-row grid-gap-4" data-eddl-landing-row-variant="TwoColumn50:50">
+		<div class="tablet-lg:grid-col-6" data-eddl-landing-row-col="">
+			<div class="usa-section">
+				<div class="usa-prose">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis elit consequat, efficitur risus id, blandit mauris. Curabitur scelerisque leo sed risus vulputate gravida. Cras consectetur est est, eu posuere mi molestie ac. Cras accumsan consequat nibh vitae scelerisque. Suspendisse sagittis leo eu elit vestibulum, feugiat cursus nisl finibus. Nulla posuere scelerisque velit, et ornare arcu suscipit in. Curabitur ut venenatis nisl, vitae hendrerit ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis elit consequat, efficitur risus id, blandit mauris. Curabitur scelerisque leo sed risus vulputate gravida. Cras Curabitur ut venenatis nisl, vitae hendrerit ligula orem ipsu</p>
+				</div>
+			</div>
+		</div>
+		<div class="tablet-lg:grid-col-6" data-eddl-landing-row-col="">
+			<aside class="usa-section" data-eddl-landing-row="">
+				<div class="usa-summary-box cgdp-summary-box">
+					<div class="usa-summary-box__body">
+						<h2 class="usa-summary-box__heading">
+							Key Information
+						</h2>
+						<div class="usa-prose usa-summary-box__text">
+							<ul>
+								<li>If you are under a winter storm warning, <a href="https://www.google.com">find shelter</a> right away.</li>
+								<li>Sign up for <a href="https://www.google.com">your community's warning system.</a></li>
+								<li>Learn the signs of, and basic treatments for, <a href="https://www.google.com">frostbite</a> and <a href="https://www.google.com">hypothermia</a>.</li>
+								<li>Gather emergency supplies for your <a href="https://www.google.com">home</a> and your <a href="https://www.google.com">car</a>.</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</aside>
+		</div>
+	</div>
+</div>
+`;
