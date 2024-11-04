561edd7be44057d0f4999720ead4ab6039966399
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2024-11-04 09:58:07 -0500

    (#4483) Update to CKEditor 5
    Closes #4483

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/ckeditor/ckeditor.js
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/ckeditor/ckeditor.js
@@ -1,1 +1,6 @@
+// All classes in this import will be scoped to the
+// .ck-content element so there is no bleedthrough
+// into the rest of the page.
+import './ckeditor-ck-scope.scss';
+// This is for classes outside of .ck-content.
 import './ckeditor.scss';
