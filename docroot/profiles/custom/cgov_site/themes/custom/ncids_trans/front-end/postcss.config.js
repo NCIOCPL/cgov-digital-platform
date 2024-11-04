5710698055ec0e4480958ab456f4dcdc1dc87535
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2024-11-04 09:58:07 -0500

    (#4483) Update to CKEditor 5
    Closes #4483

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/postcss.config.js
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/postcss.config.js
@@ -1,8 +1,11 @@
+const postcssPrefixSelector = require('postcss-prefix-selector');
 module.exports = {
 	plugins: [
 		[
-			'postcss-prefix-selector',
-			{
+			// This rule handles legacy CSS for the digital platform and adds a prefix
+			// to all *-legacy.scss files to ensure that the styles are only applied to
+			// elements within a .cgdpl container.
+			postcssPrefixSelector({
 				prefix: '.cgdpl',
 				transform(prefix, selector, prefixedSelector, filepath) {
 					if (filepath.match(/-legacy\.scss$/)) {
@@ -13,7 +16,37 @@ module.exports = {
 					}
 					return selector;
 				},
-			},
+			}),
+			// This rule handles CKEditor styles and adds a prefix to all selectors
+			// for the WYSIWYG editor to ensure that the styles are only applied to
+			// elements within a .ck-content container. This is necessary to prevent
+			// CKEditor styles from leaking into the rest of the site after the
+			// CKEditor 5 upgrade, which no longer uses an iframe.
+			postcssPrefixSelector({
+				prefix: '.ck-content',
+				transform(prefix, selector, prefixedSelector, filepath) {
+					if (filepath.match(/ckeditor\.scss$/)) {
+						if (selector.startsWith('.ck-content')) {
+							return selector;
+						} else if (selector.startsWith('.contentzone ')) {
+							return selector.replace(/^\.contentzone/, '.ck-content');
+						} else if (selector.startsWith('table.table-default')) {
+							return selector.replace(
+								/^table\.table-default/,
+								'.ck-content .table table.table-default'
+							);
+						} else if (selector.startsWith('table[data-sortable]')) {
+							return selector.replace(
+								/table\[data-sortable\]/,
+								'.ck-content .table table[data-sortable]'
+							);
+						} else {
+							return prefixedSelector;
+						}
+					}
+					return selector;
+				},
+			}),
 		],
 		['autoprefixer'],
 	],
