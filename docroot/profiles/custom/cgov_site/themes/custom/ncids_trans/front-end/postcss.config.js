2a6b2e5cc4ad5497061999e6c5a79415669cbd1e
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2024-11-15 14:00:38 -0500

    Fixed (I think) css issues bleeding through

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/postcss.config.js
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/postcss.config.js
@@ -1,31 +1,28 @@
-const postcssPrefixSelector = require('postcss-prefix-selector');
 module.exports = {
 	plugins: [
 		[
 			// This rule handles legacy CSS for the digital platform and adds a prefix
 			// to all *-legacy.scss files to ensure that the styles are only applied to
 			// elements within a .cgdpl container.
-			postcssPrefixSelector({
-				prefix: '.cgdpl',
-				transform(prefix, selector, prefixedSelector, filepath) {
-					if (filepath.match(/-legacy\.scss$/)) {
-						return prefixedSelector;
-					} else if (filepath.match(/r4r-app\/.*\.s?css$/)) {
-						// Hack for legacy R4R app
-						return prefixedSelector;
-					}
-					return selector;
-				},
-			}),
-			// This rule handles CKEditor styles and adds a prefix to all selectors
+			//
+			// Additionally, as it seems you can only use this plugin once, we also
+			// have to handle the CKEditor styles here.  It adds a prefix to all selectors
 			// for the WYSIWYG editor to ensure that the styles are only applied to
 			// elements within a .ck-content container. This is necessary to prevent
 			// CKEditor styles from leaking into the rest of the site after the
 			// CKEditor 5 upgrade, which no longer uses an iframe.
-			postcssPrefixSelector({
-				prefix: '.ck-content',
+			'postcss-prefix-selector',
+			{
+				prefix: '.cgdpl',
 				transform(prefix, selector, prefixedSelector, filepath) {
-					if (filepath.match(/ckeditor\.scss$/)) {
+					if (filepath.match(/-legacy\.scss$/)) {
+						// Handle legacy CSS for the front-end
+						return prefixedSelector;
+					} else if (filepath.match(/r4r-app\/.*\.s?css$/)) {
+						// Hack for legacy R4R app
+						return prefixedSelector;
+					} else if (filepath.match(/ckeditor\.scss$/)) {
+						// Hack for ckeditor.
 						if (selector.startsWith('.ck-content')) {
 							return selector;
 						} else if (selector.startsWith('.contentzone ')) {
@@ -44,9 +41,10 @@ module.exports = {
 							return prefixedSelector;
 						}
 					}
+
 					return selector;
 				},
-			}),
+			},
 		],
 		['autoprefixer'],
 	],
