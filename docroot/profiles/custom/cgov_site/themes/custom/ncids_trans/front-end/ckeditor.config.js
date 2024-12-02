bda225ea58f87aadea91cf15eff52f05237f5980
Dev Rana <dev-rana-publicis@users.noreply.github.com>
2024-12-02 00:50:53 -0500

    postcss.config.js change

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/ckeditor.config.js
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/ckeditor.config.js
@@ -13,15 +13,29 @@ module.exports = {
 			// CKEditor 5 upgrade, which no longer uses an iframe.
 			'postcss-prefix-selector',
 			{
-				prefix: '.cgdpl',
+				prefix: '.ck-content',
 				transform(prefix, selector, prefixedSelector, filepath) {
-					if (filepath.match(/-legacy\.scss$/)) {
-						// Handle legacy CSS for the front-end
-						return prefixedSelector;
-					} else if (filepath.match(/r4r-app\/.*\.s?css$/)) {
-						// Hack for legacy R4R app
-						return prefixedSelector;
+					if (filepath.match(/ckeditor\.scss$/)) {
+						// Hack for ckeditor.
+						if (selector.startsWith('.ck-content')) {
+							return selector;
+						} else if (selector.startsWith('.contentzone ')) {
+							return selector.replace(/^\.contentzone/, '.ck-content');
+						} else if (selector.startsWith('table.table-default')) {
+							return selector.replace(
+								/^table\.table-default/,
+								'.ck-content .table table.table-default'
+							);
+						} else if (selector.startsWith('table[data-sortable]')) {
+							return selector.replace(
+								/table\[data-sortable\]/,
+								'.ck-content .table table[data-sortable]'
+							);
+						} else {
+							return prefixedSelector;
+						}
 					}
+
 					return selector;
 				},
 			},
