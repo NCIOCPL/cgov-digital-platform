9d89797c53ddb757fc17ebd6b2f3285aecd1666c
Dev Rana <dev-rana-publicis@users.noreply.github.com>
2025-02-10 16:43:16 -0500

    Press Release
    - Created entrypoint for press release
    - Edited field citation template for press release

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/webpack.entries.js
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/webpack.entries.js
@@ -53,6 +53,10 @@ module.exports = {
 		__dirname,
 		'src/entrypoints/cancer-center/cancer-center.ts'
 	),
+	'press-release': path.resolve(
+		__dirname,
+		'src/entrypoints/press-release/press-release.ts'
+	),
 	blogs: path.resolve(__dirname, 'src/entrypoints/blogs/blogs.ts'),
 	biography: path.resolve(__dirname, 'src/entrypoints/biography/biography.ts'),
 	cthp: path.resolve(__dirname, 'src/entrypoints/cthp/cthp.ts'),
