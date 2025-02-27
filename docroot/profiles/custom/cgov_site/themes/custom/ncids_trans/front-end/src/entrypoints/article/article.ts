816ab9a37e0ad25680ce2e9dda035094268c54be
Derek Lescarbeau <derek.lescarbeau@publicissapient.com>
2025-02-27 15:43:33 -0500

    (#4594) accordion styles update and js implementation

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/article/article.ts
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/article/article.ts
@@ -1,2 +1,13 @@
 import './article.scss';
 import './article-legacy.scss';
+
+import cgdpArticleBodyInit from '../../lib/components/cgdp-article-body';
+import cgdpRelatedResourcesInit from '../../lib/components/cgdp-related-resources';
+
+//DOM Ready event
+const onDOMContentLoaded = () => {
+	cgdpArticleBodyInit();
+	cgdpRelatedResourcesInit();
+};
+
+document.addEventListener('DOMContentLoaded', onDOMContentLoaded);
--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/press-release/press-release.ts
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/press-release/press-release.ts
@@ -1,2 +1,11 @@
 import './press-release.scss';
 import './press-release-legacy.scss';
+
+import cgdpRelatedResourcesInit from '../../lib/components/cgdp-related-resources';
+
+//DOM Ready event
+const onDOMContentLoaded = () => {
+	cgdpRelatedResourcesInit();
+};
+
+document.addEventListener('DOMContentLoaded', onDOMContentLoaded);
