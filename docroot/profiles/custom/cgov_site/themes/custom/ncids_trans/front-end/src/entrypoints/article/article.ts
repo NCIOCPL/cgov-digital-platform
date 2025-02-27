114802a824ff68b0527005f42455d5478d214550
Derek Lescarbeau <derek.lescarbeau@publicissapient.com>
2025-02-27 15:43:33 -0500

    (#4594) accordion styles update and js implementation

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/article/article.ts
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/article/article.ts
@@ -1,8 +1,10 @@
 import './article.scss';
 import './article-legacy.scss';
 
+import cgdpArticleBodyInit from '../../lib/components/cgdp-article-body';
 import cgdpRelatedResourcesInit from '../../lib/components/cgdp-related-resources';
 
 document.addEventListener('DOMContentLoaded', () => {
+	cgdpArticleBodyInit();
 	cgdpRelatedResourcesInit();
 });
---
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/lib/components/cgdp-article-body/index.ts
@@ -0,0 +1,3 @@
+// Passthrough for cleaner imports.
+import initialize from './cgdp-article-body';
+export default initialize;
