a611cd4868697a05cdecadf99ff898a3559c402c
Dev Rana <dev-rana-publicis@users.noreply.github.com>
2025-03-24 10:32:51 -0400

    (#4595) Convert article-footer for inner pages
      - make ncids-trans the only css and js we pull in
      - Fixes error in PDQ found from QA
      - refactoring of ncids-trans-common
      - Added theme suggestion for new page--ncids-inner-page.html.twig template to isolate NCIDS
        changes from other routes (e.g., /user/login) that use page.html.twig that need more work.
      - updates cgov_article, cgov_biography, cgov_cancer_center, cgov_cancer_research, cgov_event,
        cgov_press_release, cgov_infographic, cgov_video to use new ncids-inner-page page template.
      - adds display mode for internal, external, and media links
      - adds internal, external, media list items for related resources, updates imports
      - updates view displays on appropriate full inner pages
      - Fix bad yaml where the title was an array. These cause the yaml_content module to replace content instead of loading new pages. It only happened for a couple, but it was nasty when it did.
      - Add content type specific templates for PDQ types that use ncids-common.
      - Adds block--public-use template to get rid of extraneous block element in public use
      - Removed stupid exit notification links put into YAML content that made people thing there were double exit links.
      - Fixed issue that seems transitive, but News & Events and Research were missing mega menu content. This was because there are multiple taxonomy_terms with the same name, and the mega menu content was getting attached to the wrong navigation item.
      - Backport new block styles for public use and syndication to PDQ.
      - Enabled dates on a sample video to allow for better testing of videos.
    
    Co-authored-by: Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
    Co-authored-by: Derek Lescarbeau <dlescarbeau@users.noreply.github.com>
    
    Closes #4595

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/article/article.ts
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/article/article.ts
@@ -1,2 +1,8 @@
 import './article.scss';
 import './article-legacy.scss';
+
+import cgdpRelatedResourcesInit from '../../lib/components/cgdp-related-resources';
+
+document.addEventListener('DOMContentLoaded', () => {
+	cgdpRelatedResourcesInit();
+});
