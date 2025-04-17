69993425175a0ae87eff1fa5da763b9985555a52
Blair Learn <blairlearn@users.noreply.github.com>
2025-04-17 11:46:05 -0400

    (#4634) Remove cgov_common from front-end build
    - Replace use of cgov_common in tests
      - Use stark where possible
      - Use ncids_trans when a front-end theme is required.
    
    Closes #4634

--- scripts/blt/custom/frontend-assets.sh
+++ scripts/blt/custom/frontend-assets.sh
@@ -9,11 +9,6 @@ pushd themes/custom/ncids_trans/front-end
 npm run build:prod
 popd
 
-## Install cgov_common packages
-pushd themes/custom/cgov/cgov_common
-npm run build
-popd
-
 ## Build pdq_glossifier packages
 pushd modules/custom/pdq_glossifier
 npm run build
