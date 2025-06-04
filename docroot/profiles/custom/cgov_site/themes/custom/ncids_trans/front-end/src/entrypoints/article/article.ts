f21f5968cd2c5d9ed5071ba30612aac545dcfe40
Derek Lescarbeau <derek.lescarbeau@publicissapient.com>
2025-06-04 10:28:32 -0400

    (#4603) Updates inner page article On This Page component to NCIDS
    Closes #4603

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/article/article.ts
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/article/article.ts
@@ -2,7 +2,9 @@ import './article.scss';
 import './article-legacy.scss';
 
 import cgdpRelatedResourcesInit from '../../lib/components/cgdp-related-resources';
+import cgdpOnThisPageInit from '../../lib/components/cgdp-on-this-page';
 
 document.addEventListener('DOMContentLoaded', () => {
 	cgdpRelatedResourcesInit();
+	cgdpOnThisPageInit();
 });
