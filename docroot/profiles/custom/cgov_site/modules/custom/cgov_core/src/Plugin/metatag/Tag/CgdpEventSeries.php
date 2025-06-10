e0666ff91856e2cf596eb4de432c8adf09c5d482
erikscottmeyer <eriksmeyer@gmail.com>
2025-06-10 14:23:58 -0400

    (#4757) Update Events Dynamic List for MLPS Structure
    Closes #4757

---
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_core/src/Plugin/metatag/Tag/CgdpEventSeries.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\cgov_core\Plugin\metatag\Tag;
+
+use Drupal\metatag\Plugin\metatag\Tag\MetaNameBase;
+
+/**
+ * Cgdp eventSeries.
+ *
+ * @MetatagTag(
+ *   id = "cgdp_event_series",
+ *   label = @Translation("Cgdp Event Series"),
+ *   description = @Translation("Cgdp Event Series Meta Properties."),
+ *   name = "cgdp.eventSeries",
+ *   group = "cgdpAdvanced_advanced",
+ *   weight = 6,
+ *   type = "label",
+ *   secure = FALSE,
+ *   multiple = FALSE
+ * )
+ */
+class CgdpEventSeries extends MetaNameBase {
+  // Just a placeholder class for a plugin.
+}
