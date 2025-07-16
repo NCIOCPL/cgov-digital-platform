4a7b314a6deff1c77e8784ff681e2322f42a0a82
Dev Rana <dev-rana-publicis@users.noreply.github.com>
2025-07-16 12:06:21 -0400

    (#4649) Convert Embedded Infographic on Inner Pages
    Closes #4649

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/press-release/press-release.ts
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/press-release/press-release.ts
@@ -3,8 +3,10 @@ import './press-release-legacy.scss';
 
 import cgdpRelatedResourcesInit from '../../lib/components/cgdp-related-resources';
 import cgdpCitationInit from '../../lib/components/cgdp-article-footer-citation';
+import cgdpInfographicInit from '../../lib/components/cgdp-infographic';
 
 document.addEventListener('DOMContentLoaded', () => {
 	cgdpRelatedResourcesInit();
 	cgdpCitationInit();
+	cgdpInfographicInit();
 });
