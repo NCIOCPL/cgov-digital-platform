c16db7845d48ba3e462c9692043bfad0c7e95c4f
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2025-11-25 13:38:13 -0500

    (#5058) Add post processing step to transformer
    Closes #5058

--- docroot/modules/custom/ncids_html_transformer/tests/src/Unit/BasicMigrationTransformerTest.php
+++ docroot/modules/custom/ncids_html_transformer/tests/src/Unit/BasicMigrationTransformerTest.php
@@ -59,7 +59,7 @@ class BasicMigrationTransformerTest extends UnitTestCase {
   public function testCalloutBoxWithInvalidClass(): void {
     $input = '<div class="callout-box invalid-class">Content</div>';
     $output = $this->transformerManager->transformAll($input);
-    $expected = '<div class="callout-box invalid-class" data-html-transformer="callout-box">Content</div>';
+    $expected = '<div class="callout-box invalid-class">Content</div>';
     $this->assertEquals($expected, $output, 'Should add data tag and NOT transform callout-box content');
   }
 
@@ -71,7 +71,7 @@ class BasicMigrationTransformerTest extends UnitTestCase {
   public function testNestedInvalidClasses(): void {
     $input = '<div class="callout-box"><div class="invalid1"><p class="invalid2">Text</p></div></div>';
     $output = $this->transformerManager->transformAll($input);
-    $expected = '<div class="callout-box" data-html-transformer="callout-box"><div class="invalid1"><p class="invalid2">Text</p></div></div>';
+    $expected = '<div class="callout-box"><div class="invalid1"><p class="invalid2">Text</p></div></div>';
     $this->assertEquals($expected, $output, 'Should add data tag and NOT transform nested content within callout-box');
   }
 
@@ -83,7 +83,7 @@ class BasicMigrationTransformerTest extends UnitTestCase {
   public function testNoBulletsTransformation(): void {
     $input = '<ul class="no-bullets extra-class">List items</ul>';
     $output = $this->transformerManager->transformAll($input);
-    $expected = '<ul class="no-bullets extra-class" data-html-transformer="no-bullets">List items</ul>';
+    $expected = '<ul class="no-bullets extra-class">List items</ul>';
     $this->assertEquals($expected, $output, 'Should add data tag and NOT transform no-bullets content');
   }
 
@@ -95,7 +95,7 @@ class BasicMigrationTransformerTest extends UnitTestCase {
   public function testTableWithAllowedStyles(): void {
     $input = '<table class="complex-table" style="width: 100%; background-color: #fff; margin: 10px; invalid-prop: value;"><tr style="text-align: center; invalid: prop;"><td>Content</td></tr></table>';
     $output = $this->transformerManager->transformAll($input);
-    $expected = '<table class="complex-table" style="width: 100%; background-color: #fff; margin: 10px; invalid-prop: value;" data-html-transformer="table"><tr style="text-align: center; invalid: prop;"><td>Content</td></tr></table>';
+    $expected = '<table class="complex-table" style="width: 100%; background-color: #fff; margin: 10px; invalid-prop: value;"><tr style="text-align: center; invalid: prop;"><td>Content</td></tr></table>';
     $this->assertEquals($expected, $output, 'Should add data tag and NOT transform complex-table content');
   }
 
@@ -143,7 +143,7 @@ class BasicMigrationTransformerTest extends UnitTestCase {
   public function testCalloutBoxVariations(): void {
     $input = '<div class="callout-box-center invalid"><aside class="callout-box-right wrong">Right Box</aside></div>';
     $output = $this->transformerManager->transformAll($input);
-    $expected = '<div class="callout-box-center invalid" data-html-transformer="callout-box-center"><aside class="callout-box-right wrong" data-html-transformer="callout-box-right">Right Box</aside></div>';
+    $expected = '<div class="callout-box-center invalid"><aside class="callout-box-right wrong">Right Box</aside></div>';
     $this->assertEquals($expected, $output, 'Should preserve allowed callout box variations and remove invalid classes (no data tag added)');
   }
 
@@ -155,7 +155,7 @@ class BasicMigrationTransformerTest extends UnitTestCase {
   public function testMixedContentWithTextNodes(): void {
     $input = '<div class="callout-box">Text <span class="invalid">More</span> <p class="wrong">Text</p></div>';
     $output = $this->transformerManager->transformAll($input);
-    $expected = '<div class="callout-box" data-html-transformer="callout-box">Text <span class="invalid">More</span> <p class="wrong">Text</p></div>';
+    $expected = '<div class="callout-box">Text <span class="invalid">More</span> <p class="wrong">Text</p></div>';
     $this->assertEquals($expected, $output, 'Should add data tag and NOT transform callout-box content, preserving all child elements');
   }
 
@@ -167,7 +167,7 @@ class BasicMigrationTransformerTest extends UnitTestCase {
   public function testTableCellAlignment(): void {
     $input = '<table class="table-default"><tr><td class="text-left invalid">Content</td><td class="wrong text-left">More</td></tr></table>';
     $output = $this->transformerManager->transformAll($input);
-    $expected = '<table class="table-default" data-html-transformer="table"><tr><td class="text-left invalid">Content</td><td class="wrong text-left">More</td></tr></table>';
+    $expected = '<table class="table-default"><tr><td class="text-left invalid">Content</td><td class="wrong text-left">More</td></tr></table>';
     $this->assertEquals($expected, $output, 'Should skip over table cells but add data tag to table element');
   }
 
@@ -179,7 +179,7 @@ class BasicMigrationTransformerTest extends UnitTestCase {
   public function testComplexNestedStructure(): void {
     $input = '<div class="grid-container invalid"><div class="grid-row wrong"><div class="grid-col-6 bad"><table class="complex-table invalid"><tr class="wrong"><td class="text-left bad" style="text-align: center; invalid: prop;">Content</td></tr></table></div></div></div>';
     $output = $this->transformerManager->transformAll($input);
-    $expected = '<div class="grid-container"><div class="grid-row"><div class="grid-col-6"><table class="complex-table invalid" data-html-transformer="table"><tr class="wrong"><td class="text-left bad" style="text-align: center; invalid: prop;">Content</td></tr></table></div></div></div>';
+    $expected = '<div class="grid-container"><div class="grid-row"><div class="grid-col-6"><table class="complex-table invalid"><tr class="wrong"><td class="text-left bad" style="text-align: center; invalid: prop;">Content</td></tr></table></div></div></div>';
     $this->assertEquals($expected, $output, 'Should transform outer elements but preserve complex-table and its children with data tag');
   }
 
@@ -189,7 +189,7 @@ class BasicMigrationTransformerTest extends UnitTestCase {
   public function testPullquotePreservation(): void {
     $input = '<blockquote class="pullquote invalid-class">Quote content</blockquote>';
     $output = $this->transformerManager->transformAll($input);
-    $expected = '<blockquote class="pullquote invalid-class" data-html-transformer="pullquote">Quote content</blockquote>';
+    $expected = '<blockquote class="pullquote invalid-class">Quote content</blockquote>';
     $this->assertEquals($expected, $output, 'Should add data tag and NOT transform pullquote content');
   }
 
@@ -199,7 +199,7 @@ class BasicMigrationTransformerTest extends UnitTestCase {
   public function testNoDescriptionPreservation(): void {
     $input = '<div class="no-description invalid-class">Description content</div>';
     $output = $this->transformerManager->transformAll($input);
-    $expected = '<div class="no-description invalid-class" data-html-transformer="no-description">Description content</div>';
+    $expected = '<div class="no-description invalid-class">Description content</div>';
     $this->assertEquals($expected, $output, 'Should add data tag and NOT transform no-description content');
   }
 
@@ -211,7 +211,7 @@ class BasicMigrationTransformerTest extends UnitTestCase {
   public function testStyleTransformation(): void {
     $input = '<div style="color: red; margin: 10px;">Container<span style="font-size: small; color: blue;">Text</span><table style="width: 100%; background-color: #fff; margin: 10px; invalid-prop: value;"><tr style="text-align: center; color: red;"><td style="padding: 5px; font-size: large;">Cell Content</td></tr></table></div>';
     $output = $this->transformerManager->transformAll($input);
-    $expected = '<div>Container<span>Text</span><table style="width: 100%; background-color: #fff; margin: 10px; invalid-prop: value;" data-html-transformer="table"><tr style="text-align: center; color: red;"><td style="padding: 5px; font-size: large;">Cell Content</td></tr></table></div>';
+    $expected = '<div>Container<span>Text</span><table style="width: 100%; background-color: #fff; margin: 10px; invalid-prop: value;"><tr style="text-align: center; color: red;"><td style="padding: 5px; font-size: large;">Cell Content</td></tr></table></div>';
     $this->assertEquals($expected, $output, 'Should remove all styles from non-table elements and keep only allowed styles on table elements');
   }
 
@@ -223,7 +223,7 @@ class BasicMigrationTransformerTest extends UnitTestCase {
   public function testTableStyleCompleteRemoval(): void {
     $input = '<table style="color: red; margin: 10px; font-size: large;"><tbody style="display: block;"><tr style="cursor: pointer;"><th style="color: blue;">Header</th><td style="font-family: Arial;">Data</td></tr></tbody></table>';
     $output = $this->transformerManager->transformAll($input);
-    $expected = '<table style="color: red; margin: 10px; font-size: large;" data-html-transformer="table"><tbody style="display: block;"><tr style="cursor: pointer;"><th style="color: blue;">Header</th><td style="font-family: Arial;">Data</td></tr></tbody></table>';
+    $expected = '<table style="color: red; margin: 10px; font-size: large;"><tbody style="display: block;"><tr style="cursor: pointer;"><th style="color: blue;">Header</th><td style="font-family: Arial;">Data</td></tr></tbody></table>';
     $this->assertEquals($expected, $output, 'Should remove style attributes entirely from table elements when no allowed styles are present');
   }
 
@@ -235,7 +235,7 @@ class BasicMigrationTransformerTest extends UnitTestCase {
   public function testTableMixedStyles(): void {
     $input = '<table style="width: 100%; color: red; border: 1px solid black; font-size: 12px;"><tr style="height: 50px; background-color: #f0f0f0; display: flex;"><td style="text-align: left; color: blue; padding: 10px; margin: 5px;">Content</td></tr></table>';
     $output = $this->transformerManager->transformAll($input);
-    $expected = '<table style="width: 100%; color: red; border: 1px solid black; font-size: 12px;" data-html-transformer="table"><tr style="height: 50px; background-color: #f0f0f0; display: flex;"><td style="text-align: left; color: blue; padding: 10px; margin: 5px;">Content</td></tr></table>';
+    $expected = '<table style="width: 100%; color: red; border: 1px solid black; font-size: 12px;"><tr style="height: 50px; background-color: #f0f0f0; display: flex;"><td style="text-align: left; color: blue; padding: 10px; margin: 5px;">Content</td></tr></table>';
     $this->assertEquals($expected, $output, 'Should preserve only allowed styles on table elements and remove disallowed ones');
   }
 
