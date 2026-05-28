f85d1b5f1f911acc5c54e464d9a6f97cea3305d0
dlescarbeau <lescarbeaudw@nih.gov>
2026-05-28 11:34:57 -0400

    (#5235) Updates press release body to ncids full html
    - updates yaml files with ncids_full_html
    
    - updates twig template with new html
    
    - adds analytics tracking for links in the body field
    
    Closes #5235

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/press-release/press-release.ts
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/press-release/press-release.ts
@@ -1,16 +1,41 @@
 import './press-release.scss';
-import './press-release-legacy.scss';
 
+import cgdpDefinitionInit from '../../lib/components/cgdp-definition';
 import cgdpRelatedResourcesInit from '../../lib/components/cgdp-related-resources';
 import cgdpCitationInit from '../../lib/components/cgdp-article-footer-citation';
 import cgdpInfographicInit from '../../lib/components/cgdp-infographic';
 import cgdpEmbedVideoInit from '../../lib/components/wysiwyg/common/cgdp-embed-video';
 import cgdpEmbedCardInit from '../../lib/components/wysiwyg/common/cgdp-embed-card';
 
+import { bodyLinkAnalyticsHelper } from '../../lib/core/analytics/inner-page-analytics-tracker';
+
+let pressReleaseBodyAnalyticsInit = false;
+
+/**
+ * Body Analytics
+ */
+const cgdpBodyAnalyticsInit = (): void => {
+	if (!pressReleaseBodyAnalyticsInit) {
+		const pressReleaseBodySection = document.querySelector(
+			'.usa-prose--ncids-full-html'
+		);
+		const pressReleaseBodyParent = pressReleaseBodySection?.parentElement;
+
+		// Verify both elements exist before running helper
+		if (pressReleaseBodySection && pressReleaseBodyParent) {
+			// NOTE: Verify if '0' is correct, or if this index needs to be dynamic
+			bodyLinkAnalyticsHelper(pressReleaseBodyParent as HTMLElement, 0);
+			pressReleaseBodyAnalyticsInit = true;
+		}
+	}
+};
+
 document.addEventListener('DOMContentLoaded', () => {
+	cgdpDefinitionInit(true);
 	cgdpRelatedResourcesInit();
 	cgdpCitationInit();
 	cgdpInfographicInit();
 	cgdpEmbedVideoInit();
 	cgdpEmbedCardInit();
+	cgdpBodyAnalyticsInit();
 });
