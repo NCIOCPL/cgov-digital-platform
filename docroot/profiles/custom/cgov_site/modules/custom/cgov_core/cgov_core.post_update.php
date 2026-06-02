61b7e23e8fed6ed48c5d4bd6adc625f2a9211c40
dlescarbeau <lescarbeaudw@nih.gov>
2026-06-02 11:32:37 -0400

    (#5205) Convert citations to NCIDS Streamlined
    Closes #5205

--- docroot/profiles/custom/cgov_site/modules/custom/cgov_core/cgov_core.post_update.php
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_core/cgov_core.post_update.php
@@ -28,3 +28,27 @@ function cgov_core_post_update_uninstall_stable() {
     $theme_installer->uninstall(['stable']);
   }
 }
+
+/**
+ * Updates citation content to use ncids_streamlined_no_headings.
+ */
+function cgov_core_post_update_citation_content_streamlined_format(): string {
+  $db = \Drupal::database();
+  $bundle = 'cgov_citation';
+  $old_format = 'streamlined';
+  $new_format = 'ncids_streamlined_no_headings';
+
+  $base_count = $db->update('paragraph__field_citation_content')
+    ->fields(['field_citation_content_format' => $new_format])
+    ->condition('bundle', $bundle)
+    ->condition('field_citation_content_format', $old_format)
+    ->execute();
+
+  $revision_count = $db->update('paragraph_revision__field_citation_content')
+    ->fields(['field_citation_content_format' => $new_format])
+    ->condition('bundle', $bundle)
+    ->condition('field_citation_content_format', $old_format)
+    ->execute();
+
+  return "Updated text formats on $base_count paragraphs and $revision_count paragraph revisions.";
+}
