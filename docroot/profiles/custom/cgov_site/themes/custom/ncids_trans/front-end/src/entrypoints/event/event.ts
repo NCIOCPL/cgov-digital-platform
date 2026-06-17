8e990addbb557a5c6338d5db84b98b8f522aa633
dlescarbeau <lescarbeaudw@nih.gov>
2026-06-17 10:24:49 -0400

    (#5257) Adds analytics to body section of events pages
    Closes #5257

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/event/event.ts
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/event/event.ts
@@ -5,6 +5,29 @@ import cgdpRelatedResourcesInit from '../../lib/components/cgdp-related-resource
 import cgdpInfographicInit from '../../lib/components/cgdp-infographic';
 import cgdpEmbedVideoInit from '../../lib/components/wysiwyg/common/cgdp-embed-video';
 import cgdpEmbedCardInit from '../../lib/components/wysiwyg/common/cgdp-embed-card';
+import { bodyLinkAnalyticsHelper } from '../../lib/core/analytics/inner-page-analytics-tracker';
+
+let eventsBodyAnalyticsInit = false;
+
+/**
+ * Body Analytics
+ */
+const cgdpBodyAnalyticsInit = (): void => {
+	if (!eventsBodyAnalyticsInit) {
+		const eventsBodySection = document.querySelector(
+			'.usa-prose--ncids-full-html'
+		);
+		const eventsBodyParent = eventsBodySection?.parentElement;
+
+		// Verify both elements exist before running helper
+		if (eventsBodySection && eventsBodyParent) {
+			// NOTE: There should only ever be one instance of the
+			// body section on the page, so we can hardcode the section index to 0
+			bodyLinkAnalyticsHelper(eventsBodyParent as HTMLElement, 0);
+			eventsBodyAnalyticsInit = true;
+		}
+	}
+};
 
 document.addEventListener('DOMContentLoaded', () => {
 	cgdpDefinitionInit(true);
@@ -12,4 +35,5 @@ document.addEventListener('DOMContentLoaded', () => {
 	cgdpInfographicInit();
 	cgdpEmbedVideoInit();
 	cgdpEmbedCardInit();
+	cgdpBodyAnalyticsInit();
 });
