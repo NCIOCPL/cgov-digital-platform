da61b9081bfdfe0068cd2900e8369d50f938333d
Derek Lescarbeau <lescarbeaudw@nih.gov>
2026-08-11 14:41:13 -0400

    (#5183) Migrates biography body to ncids full wysiwyg
    Closes #5183

--- docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/biography/biography.ts
+++ docroot/profiles/custom/cgov_site/themes/custom/ncids_trans/front-end/src/entrypoints/biography/biography.ts
@@ -1,5 +1,4 @@
 import './biography.scss';
-import './biography-legacy.scss';
 
 import cgdpRelatedResourcesInit from '../../lib/components/cgdp-related-resources';
 import cgdpCitationInit from '../../lib/components/cgdp-article-footer-citation';
@@ -7,6 +6,30 @@ import cgdpInfographicInit from '../../lib/components/cgdp-infographic';
 import cgdpEmbedVideoInit from '../../lib/components/wysiwyg/common/cgdp-embed-video';
 import cgdpEmbedCardInit from '../../lib/components/wysiwyg/common/cgdp-embed-card';
 import cgdpProfileBox from '../../lib/components/cgdp-profile-box';
+import cgdpDefinitionInit from '../../lib/components/cgdp-definition';
+
+import { bodyLinkAnalyticsHelper } from '../../lib/core/analytics/inner-page-analytics-tracker';
+
+let biographyBodyAnalyticsInit = false;
+
+/**
+ * Body Analytics
+ */
+const cgdpBodyAnalyticsInit = (): void => {
+	if (!biographyBodyAnalyticsInit) {
+		const biographyBodySection = document.querySelector(
+			'.usa-prose--ncids-full-html'
+		);
+		const biographyBodyParent = biographyBodySection?.parentElement;
+
+		// Verify both elements exist before running helper
+		if (biographyBodySection && biographyBodyParent) {
+			// NOTE: Verify if '0' is correct, or if this index needs to be dynamic
+			bodyLinkAnalyticsHelper(biographyBodyParent as HTMLElement, 0);
+			biographyBodyAnalyticsInit = true;
+		}
+	}
+};
 
 document.addEventListener('DOMContentLoaded', () => {
 	cgdpRelatedResourcesInit();
@@ -15,4 +38,6 @@ document.addEventListener('DOMContentLoaded', () => {
 	cgdpEmbedVideoInit();
 	cgdpEmbedCardInit();
 	cgdpProfileBox();
+	cgdpDefinitionInit();
+	cgdpBodyAnalyticsInit();
 });
